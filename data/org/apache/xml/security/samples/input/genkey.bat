804592e273146defc77ac7f281b0cc64e98556ca
Christian Geuer-Pollmann <geuerp@apache.org>
2001-10-01 08:25:05 +0000

    Initial commit
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@350318 13f79535-47bb-0310-9956-ffa450edef68

---
+++ data/org/apache/xml/security/samples/input/genkey.bat
@@ -0,0 +1,1 @@
+keytool -genkey -alias test -keyalg DSA -dname "CN=Christian Geuer-Pollmann, OU=FB12NUE, O=University of Siegen, C=DE" -keypass xmlsecurity -storepass xmlsecurity -keystore keystore.jks -validity 2000
