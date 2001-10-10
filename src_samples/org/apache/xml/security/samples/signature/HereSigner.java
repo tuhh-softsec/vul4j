41a3c10d1ed6f304ac060841e90621186ae8564d
Christian Geuer-Pollmann <geuerp@apache.org>
2001-10-10 10:46:16 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@350341 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src_samples/org/apache/xml/security/samples/signature/HereSigner.java
@@ -0,0 +1,195 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "<WebSig>" and "Apache Software Foundation" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    nor may "Apache" appear in their name, without prior written
+ *    permission of the Apache Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation and was
+ * originally based on software copyright (c) 2001, Institute for
+ * Data Communications Systems, <http://www.nue.et-inf.uni-siegen.de/>.
+ * The development of this software was partly funded by the European
+ * Commission in the <WebSig> project in the ISIS Programme.
+ * For more information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+package org.apache.xml.security.samples.signature;
+
+
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.security.*;
+import java.security.cert.*;
+import java.util.*;
+import javax.xml.transform.TransformerException;
+import org.apache.xpath.XPathAPI;
+import org.w3c.dom.*;
+import org.apache.xml.security.algorithms.MessageDigestAlgorithm;
+import org.apache.xml.security.c14n.helper.XPathContainer;
+import org.apache.xml.security.c14n.*;
+import org.apache.xml.security.exceptions.XMLSecurityException;
+import org.apache.xml.security.signature.*;
+import org.apache.xml.security.keys.*;
+import org.apache.xml.security.keys.content.*;
+import org.apache.xml.security.keys.content.x509.*;
+import org.apache.xml.security.keys.keyresolver.*;
+import org.apache.xml.security.keys.storage.*;
+import org.apache.xml.security.keys.storage.implementations.*;
+import org.apache.xml.security.utils.*;
+import org.apache.xml.security.transforms.*;
+import org.apache.xml.security.Init;
+import org.apache.xml.security.samples.utils.resolver.OfflineResolver;
+import  org.apache.xml.serialize.*;
+
+
+/**
+ *
+ *
+ * @author $Author$
+ */
+public class HereSigner {
+
+   /** {@link org.apache.log4j} logging facility */
+   static org.apache.log4j.Category cat =
+      org.apache.log4j.Category.getInstance(HereSigner.class.getName());
+
+   /**
+    * Method main
+    *
+    * @param unused
+    * @throws Exception
+    */
+   public static void main(String unused[]) throws Exception {
+      //J-
+      String keystoreType = "JKS";
+      String keystoreFile = "data/org/apache/xml/security/samples/input/keystore.jks";
+      String keystorePass = "xmlsecurity";
+      String privateKeyAlias = "test";
+      String privateKeyPass = "xmlsecurity";
+      String certificateAlias = "test";
+      File signatureFile = new File("hereSignature.xml");
+      //J+
+      KeyStore ks = KeyStore.getInstance(keystoreType);
+      FileInputStream fis = new FileInputStream(keystoreFile);
+
+      ks.load(fis, keystorePass.toCharArray());
+
+      PrivateKey privateKey = (PrivateKey) ks.getKey(privateKeyAlias,
+                                             privateKeyPass.toCharArray());
+      javax.xml.parsers.DocumentBuilderFactory dbf =
+         javax.xml.parsers.DocumentBuilderFactory.newInstance();
+
+      dbf.setNamespaceAware(true);
+
+      javax.xml.parsers.DocumentBuilder db = dbf.newDocumentBuilder();
+      org.w3c.dom.Document doc = db.newDocument();
+
+
+      String BaseURI = signatureFile.toURL().toString();
+      Constants.setSignatureSpecNSprefix("prof");
+      XMLSignature sig = new XMLSignature(doc, BaseURI,
+                                          XMLSignature.ALGO_ID_SIGNATURE_DSA);
+
+      doc.appendChild(sig.getElement());
+      sig.getSignedInfo()
+         .addResourceResolver(new org.apache.xml.security.samples.utils.resolver
+            .OfflineResolver());
+
+      {
+         ObjectContainer ob1 = new ObjectContainer(doc);
+         ob1.setId("object-1");
+         ob1.appendChild(doc.createTextNode("\nSigned Text\n"));
+         sig.appendObject(ob1);
+
+         Transforms transforms = new Transforms(doc);
+         XPathContainer xc = new XPathContainer(doc);
+         xc.setXPathNamespaceContext("prof", Constants.SignatureSpecNS);
+
+         //J-
+         String xpath = "\n\n"
+
+          + " ancestor-or-self::node()                                 " + "\n"
+          + "           =                                              " + "\n"
+          + " here()/ancestor::prof:Signature[1]/child::prof:Object[@Id='object-1'] " + "\n"
+
+          + "\n";
+         //J+
+
+         xc.setXPath(xpath);
+         HelperNodeList nl = new HelperNodeList();
+         nl.appendChild(doc.createTextNode("\n"));
+         nl.appendChild(xc.getElement());
+         nl.appendChild(doc.createTextNode("\n"));
+
+         transforms.addTransform(Transforms.TRANSFORM_XPATH, nl);
+         transforms.addTransform(Transforms.TRANSFORM_C14N_WITH_COMMENTS);
+         sig.addDocument("", transforms, Constants.ALGO_ID_DIGEST_SHA1);
+      }
+
+      {
+         X509Certificate cert =
+            (X509Certificate) ks.getCertificate(certificateAlias);
+
+         sig.addKeyInfo(cert);
+         sig.addKeyInfo(cert.getPublicKey());
+         System.out.println("Start signing");
+         sig.sign(privateKey);
+         System.out.println("Finished signing");
+      }
+
+      FileOutputStream f = new FileOutputStream(signatureFile);
+
+      XMLUtils.outputDOMc14nWithComments(doc, f);
+
+      f.close();
+      System.out.println("Wrote signature to " + BaseURI);
+   }
+
+   static {
+      org.apache.xml.security.Init.init();
+   }
+}
