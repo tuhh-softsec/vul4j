ab15eb508c0b87355d7e23f9be9c9a3309d78e69
Christian Geuer-Pollmann <geuerp@apache.org>
2001-12-11 14:21:23 +0000

    Changed protected to public cause now, Transform is used not only by ds:Transforms but also by xenc:Transforms which is in another package
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@350384 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/security/transforms/Transform.java
+++ src/org/apache/xml/security/transforms/Transform.java
@@ -2,7 +2,7 @@
  * The Apache Software License, Version 1.1
  *
  *
- * Copyright (c) 1999 The Apache Software Foundation.  All rights 
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -10,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -18,7 +18,7 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
@@ -26,7 +26,7 @@
  *
  * 4. The names "<WebSig>" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache@apache.org.
  *
  * 5. Products derived from this software may not be called "Apache",
@@ -51,8 +51,8 @@
  * individuals on behalf of the Apache Software Foundation and was
  * originally based on software copyright (c) 2001, Institute for
  * Data Communications Systems, <http://www.nue.et-inf.uni-siegen.de/>.
- * The development of this software was partly funded by the European 
- * Commission in the <WebSig> project in the ISIS Programme. 
+ * The development of this software was partly funded by the European
+ * Commission in the <WebSig> project in the ISIS Programme.
  * For more information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
@@ -165,7 +165,7 @@ public class Transform extends SignatureElementProxy {
     * @throws TransformationException
     * @throws XMLSecurityException
     */
-   protected Transform(Element element, String BaseURI)
+   public Transform(Element element, String BaseURI)
            throws InvalidTransformException, TransformationException,
                   XMLSecurityException {
 
@@ -214,7 +214,7 @@ public class Transform extends SignatureElementProxy {
     * @return <code>{@link Transfrom}</code> object
     * @throws InvalidTransformException
     */
-   protected static final Transform getInstance(
+   public static final Transform getInstance(
            Document doc, String algorithmURI) throws InvalidTransformException {
       return Transform.getInstance(doc, algorithmURI, (NodeList) null);
    }
@@ -228,7 +228,7 @@ public class Transform extends SignatureElementProxy {
     * @return <code>{@link Transfrom}</code> object
     * @throws InvalidTransformException
     */
-   protected static final Transform getInstance(
+   public static final Transform getInstance(
            Document doc, String algorithmURI, Element contextChild)
               throws InvalidTransformException {
 
@@ -248,7 +248,7 @@ public class Transform extends SignatureElementProxy {
     * @return <code>{@link Transfrom}</code> object
     * @throws InvalidTransformException
     */
-   protected static final Transform getInstance(
+   public static final Transform getInstance(
            Document doc, String algorithmURI, NodeList contextNodes)
               throws InvalidTransformException {
       return new Transform(doc, algorithmURI, contextNodes);
@@ -311,7 +311,7 @@ public class Transform extends SignatureElementProxy {
     * @throws InvalidCanonicalizerException
     * @throws TransformationException
     */
-   protected XMLSignatureInput performTransform(XMLSignatureInput input)
+   public XMLSignatureInput performTransform(XMLSignatureInput input)
            throws IOException, CanonicalizationException,
                   InvalidCanonicalizerException, TransformationException {
 
