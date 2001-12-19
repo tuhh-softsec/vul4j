c75d35c0e27f73ed9454137335c2bd1580dc6418
Christian Geuer-Pollmann <geuerp@apache.org>
2001-12-19 12:56:39 +0000

    Changed the printed stack trace
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@350390 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/security/exceptions/XMLSecurityException.java
+++ src/org/apache/xml/security/exceptions/XMLSecurityException.java
@@ -1,9 +1,8 @@
-
 /*
  * The Apache Software License, Version 1.1
  *
  *
- * Copyright (c) 1999 The Apache Software Foundation.  All rights 
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -11,7 +10,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -19,7 +18,7 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution,
- *    if any, must include the following acknowledgment:  
+ *    if any, must include the following acknowledgment:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
  *    Alternately, this acknowledgment may appear in the software itself,
@@ -27,7 +26,7 @@
  *
  * 4. The names "<WebSig>" and "Apache Software Foundation" must
  *    not be used to endorse or promote products derived from this
- *    software without prior written permission. For written 
+ *    software without prior written permission. For written
  *    permission, please contact apache@apache.org.
  *
  * 5. Products derived from this software may not be called "Apache",
@@ -52,8 +51,8 @@
  * individuals on behalf of the Apache Software Foundation and was
  * originally based on software copyright (c) 2001, Institute for
  * Data Communications Systems, <http://www.nue.et-inf.uni-siegen.de/>.
- * The development of this software was partly funded by the European 
- * Commission in the <WebSig> project in the ISIS Programme. 
+ * The development of this software was partly funded by the European
+ * Commission in the <WebSig> project in the ISIS Programme.
  * For more information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  */
@@ -229,6 +228,43 @@ public class XMLSecurityException extends Exception {
       }
 
       return message;
+   }
+
+   /**
+    * Method printStackTrace
+    *
+    */
+   public void printStackTrace() {
+
+      synchronized (System.err) {
+         System.err.println(this.getClass().getName() + ": "
+                            + this.getLocalizedMessage());
+         this.originalException.printStackTrace(System.err);
+      }
+   }
+
+   /**
+    * Method printStackTrace
+    *
+    * @param printwriter
+    */
+   public void printStackTrace(PrintWriter printwriter) {
+
+      printwriter.println(this.getClass().getName() + ": "
+                          + this.getLocalizedMessage());
+      this.originalException.printStackTrace(printwriter);
+   }
+
+   /**
+    * Method printStackTrace
+    *
+    * @param printstream
+    */
+   public void printStackTrace(PrintStream printstream) {
+
+      printstream.println(this.getClass().getName() + ": "
+                          + this.getLocalizedMessage());
+      this.originalException.printStackTrace(printstream);
    }
 
    /**
