54e3659529e1907cbc5fe2ca8ac7d7302235b100
Rodney Waldhoff <rwaldhoff@apache.org>
2003-03-12 21:08:16 +0000

    some doc enhancements
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/functor/trunk@144422 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/functor/example/QuicksortExample.java
+++ src/test/org/apache/commons/functor/example/QuicksortExample.java
@@ -1,5 +1,5 @@
 /* 
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons-sandbox//functor/src/test/org/apache/commons/functor/example/QuicksortExample.java,v 1.1 2003/03/12 00:12:44 rwaldhoff Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons-sandbox//functor/src/test/org/apache/commons/functor/example/QuicksortExample.java,v 1.2 2003/03/12 21:08:16 rwaldhoff Exp $
  * ====================================================================
  * The Apache Software License, Version 1.1
  *
@@ -82,14 +82,11 @@ import org.apache.commons.functor.core.composite.ConditionalUnaryFunction;
  */
 
 /* 
- * Here's an example of the QuicksortExample sorting algorithm, implemented using
- * commons-functor.
+ * Here's an example of the Quicksort sorting algorithm, implemented using
+ * Commons Functor.
  * 
- * We'll write this quicksort implementation in a literate programming style,
- * (in other words, with descriptive prose mixed right in with the source).
- * 
- * For convenience, and to make sure this example stays up to date, 
- * we'll implement our quicksort example as a JUnit TestCase.
+ * See http://jakarta.apache.org/commons/sandbox/functor/examples.html 
+ * for additional examples.
  */
 
 /**
@@ -98,7 +95,7 @@ import org.apache.commons.functor.core.composite.ConditionalUnaryFunction;
  * <p> 
  * See the extensive in line comments for details.
  * 
- * @version $Revision: 1.1 $ $Date: 2003/03/12 00:12:44 $
+ * @version $Revision: 1.2 $ $Date: 2003/03/12 21:08:16 $
  * @author Rodney Waldhoff
  */
 public class QuicksortExample extends TestCase {
@@ -117,28 +114,15 @@ public class QuicksortExample extends TestCase {
         return new TestSuite(QuicksortExample.class);
     }
 
- 
-/*
- * If you're not familiar with JUnit, don't worry. An understanding of JUnit 
- * isn't important for an understanding of this example, and we'll walk you 
- * through the relevant bits anyway.
- * 
- * Two things you'll want to know about JUnit are (a) all the methods
- * whose names start with "test" will be executed automatically by the 
- * test suite and (b) there are various "assert" methods that can be used
- * to make assertions about the Objects being tested.  If any assertion
- * fails, the JUnit framework will count this as a test failure.
- */
-
 /*
  * ----------------------------------------------------------------------------
  * UNIT TESTS:
  * ----------------------------------------------------------------------------
  */
- 
+
 /*
  * In "test first" style, let's start with the some functional descriptions
- * of what'd we'd like our QuicksortExample to do, expressed as unit tests.
+ * of what'd we'd like our Quicksort to do, expressed as unit tests.
  * 
  * In our tests, we'll use a "quicksort" method which takes a List and
  * returns a (new) sorted List.  We'll define that method a bit later.
@@ -152,11 +136,8 @@ public class QuicksortExample extends TestCase {
     public void testSortEmpty() {
         List empty = Collections.EMPTY_LIST;
         List result = quicksort(empty);
-        assertTrue(
-            "Sorting an empty list should produce an empty list.",
-            result.isEmpty()
-        );
-    }    
+        assertTrue("Sorting an empty list should produce an empty list.", result.isEmpty());
+    }
 
 /*
  * Similarly, sorting a List composed of a single element
@@ -166,20 +147,18 @@ public class QuicksortExample extends TestCase {
     public void testSortSingleElementList() {
         List list = new ArrayList();
         list.add("element");
-        
+
         List sorted = quicksort(list);
-        
+
         assertTrue(
-            "The quicksort() method should return a distinct list.",
-            list != sorted
-        );
-        
+            "The quicksort() method should return a distinct list.", 
+            list != sorted);
+
         assertEquals(
-            "Sorting a single-element list should produce an equivalent list",
+            "Sorting a single-element list should produce an equivalent list", 
             list, 
-            sorted
-        );
-    }    
+            sorted);
+    }
 
 /*
  * Finally, sorting a List composed of multiple copies
@@ -188,19 +167,18 @@ public class QuicksortExample extends TestCase {
 
     public void testSortSingleValueList() {
         List list = new ArrayList();
-        for(int i=0;i<10;i++) {
+        for(int i = 0; i < 10; i++) {
             list.add("element");
         }
         List sorted = quicksort(list);
-        
+
         assertTrue(
-            "The quicksort() method should return a distinct list.",
-            list != sorted
-        );
-        
+            "The quicksort() method should return a distinct list.", 
+            list != sorted);
+
         assertEquals(list, sorted);
-    }    
- 
+    }
+
 /*
  * So far so good.
  * 
@@ -210,23 +188,21 @@ public class QuicksortExample extends TestCase {
  */
     public void testSortSorted() {
         List list = new ArrayList();
-        for(int i=0;i<10;i++) {
+        for(int i = 0; i < 10; i++) {
             list.add(new Integer(i));
         }
-        
+
         List sorted = quicksort(list);
-        
+
         assertTrue(
-            "The quicksort() method should return a distinct list.",
-            list != sorted
-        );
-        
+            "The quicksort() method should return a distinct list.", 
+            list != sorted);
+
         assertEquals(
-            "Sorting an already sorted list should produce an equivalent list",
+            "Sorting an already sorted list should produce an equivalent list", 
             list, 
-            sorted
-        );
-    }    
+            sorted);
+    }
 
 /*
  * Sorting a reverse-order list (finally, a test case that requires something 
@@ -235,7 +211,7 @@ public class QuicksortExample extends TestCase {
     public void testSortReversed() {
         List expected = new ArrayList();
         List tosort = new ArrayList();
-        for(int i=0;i<10;i++) {
+        for(int i = 0; i < 10; i++) {
             /*
              * The "expected" list contains the integers in order.
              */
@@ -245,25 +221,24 @@ public class QuicksortExample extends TestCase {
              */
             tosort.add(new Integer(9 - i));
         }
-        
-        assertEquals(expected, quicksort(tosort));
-    }    
 
+        assertEquals(expected, quicksort(tosort));
+    }
 
 /*
  * Just for fun, let's add some randomness to the tests, first by shuffling:
  */
     public void testSortShuffled() {
         List expected = new ArrayList();
-        for(int i=0;i<10;i++) {
+        for(int i = 0; i < 10; i++) {
             expected.add(new Integer(i));
         }
         List tosort = new ArrayList(expected);
         Collections.shuffle(tosort);
-        
+
         assertEquals(expected, quicksort(tosort));
-    }    
-    
+    }
+
 /*
  * and then using random values:
  */
@@ -271,9 +246,9 @@ public class QuicksortExample extends TestCase {
         Random random = new Random();
         /*
          * populate a list with random integers
-         */        
+         */
         List tosort = new ArrayList();
-        for(int i=0;i<10;i++) {
+        for(int i = 0; i < 10; i++) {
             tosort.add(new Integer(random.nextInt(10)));
         }
         /*
@@ -284,7 +259,7 @@ public class QuicksortExample extends TestCase {
         Collections.sort(expected);
 
         assertEquals(expected, quicksort(tosort));
-    }    
+    }
 
 /*
  * Finally, while this quicksort implementation is intended to 
@@ -292,78 +267,81 @@ public class QuicksortExample extends TestCase {
  * let's output some timings just to demonstrate that the 
  * performance is adequate.
  */
- 
+
     private static final int SIZE = 1000;
     private static final int COUNT = 100;
-    
+
     public void testTimings() {
         /*
          * We'll need the total elapsed time:
          */
         long elapsed = 0L;
-        
+
         /*
          * and a source for random integers:
          */
         Random random = new Random();
-        
+
         /*
          * Repeat this COUNT times:
          */
-        for(int i=0;i<COUNT;i++) {
+        for(int i = 0; i < COUNT; i++) {
             /*
              * Create a List of size SIZE, and
              * populate it with random integers:
              */
             List tosort = new ArrayList(SIZE);
-            for(int j=0;j<SIZE;j++) {
+            for(int j = 0; j < SIZE; j++) {
                 tosort.add(new Integer(random.nextInt(SIZE)));
             }
-            
+
             /*
              * Start the timer.
              */
-            long start = System.currentTimeMillis();        
-            
+            long start = System.currentTimeMillis();
+
             /*
              * Sort the list.
              * (We'll ignore the returned value here.)
              */
             quicksort(tosort);
-            
+
             /*
              * Stop the timer.
              */
-             long stop = System.currentTimeMillis();
-             
-             /*
-              * Add the elapsed time to our total.
-              */
-              elapsed += stop - start;
+            long stop = System.currentTimeMillis();
+
+            /*
+             * Add the elapsed time to our total.
+             */
+            elapsed += stop - start;
         }
-        
 
         /*
          * Whew, that was a lot of processing.  Now figure out
          * how long it took on average (per list):
          */
-        double avgmillis = ((double)elapsed)/((double)COUNT);
-         
+        double avgmillis = ((double)elapsed) / ((double)COUNT);
+
         /* 
          * and print a simple summary.
          */
-        System.out.println();        
+        System.out.println();
         System.out.println(
-            "QuicksortExample Example: Sorted " + COUNT + 
-            " lists of " + SIZE + 
-            " elements in " + elapsed + " millis (" + 
-            avgmillis + 
-            " millis, or " +
-            (avgmillis/1000D) + 
-            " seconds on average).");
-        System.out.println();        
-    }    
-     
+            "Quicksort Example: Sorted "
+                + COUNT
+                + " lists of "
+                + SIZE
+                + " elements in "
+                + elapsed
+                + " millis ("
+                + avgmillis
+                + " millis, or "
+                + (avgmillis / 1000D)
+                + " seconds on average).");
+        System.out.println();
+    }
+
 /*
  * BUILDING BLOCKS:
  * ----------------------------------------------------------------------------
@@ -371,21 +349,44 @@ public class QuicksortExample extends TestCase {
  * Let's save ourselves some casting and error checking by defining
  * functor subclasses that deal with java.util.List.
  *
- * Let ListFunction be a UnaryFunction that operates on Lists :
- */ 
- 
+ * Let ListFunction be a UnaryFunction that operates on Lists:
+ */
+
     public abstract class ListFunction implements UnaryFunction {
         public abstract Object evaluate(List list);
-        
+
         public Object evaluate(Object obj) {
-            if(null == obj) {
+            if(obj instanceof List) {
+                return evaluate((List)obj);
+            } else if(null == obj) {
                 throw new NullPointerException("The argument must not be null.");
-            } else if(!(obj instanceof List)) {
+            } else {
                 throw new ClassCastException(
                     "The argument must be a List, found " + 
                     obj.getClass().getName());
-            } else { 
-                return evaluate((List)obj);
+            }
+        }
+    }
+
+/*
+ * Let ObjectListFunction be a BinaryFunction that operates on 
+ * an Object, List pair:
+ */
+
+    public abstract class ObjectListFunction implements BinaryFunction {
+        public abstract Object evaluate(Object head, List tail);
+
+        public Object evaluate(Object left, Object right) {
+            if(left != null && right instanceof List) {
+                return evaluate(left, (List)right);
+            } else if(null == left) {
+                throw new NullPointerException("The left argument must not be null.");
+            } else if(null == right) {
+                throw new NullPointerException("The right argument must not be null.");
+            } else {
+                throw new ClassCastException(
+                    "The right argument must be a List, found " + 
+                    right.getClass().getName());
             }
         }
     }
@@ -415,13 +416,13 @@ public class QuicksortExample extends TestCase {
 /* 
  * Let's define functors for the operations we'll need.
  * 
- * Given a List, we want to be able to break it into its head:
+ * Given a List, we need to be able to break it into its head:
  */
 
     private UnaryFunction head = new ListFunction() {
         public Object evaluate(List list) {
             return list.get(0);
-        }        
+        }
     };
 
 /* 
@@ -429,59 +430,65 @@ public class QuicksortExample extends TestCase {
  */
     private UnaryFunction tail = new ListFunction() {
         public Object evaluate(List list) {
-            return list.size() < 2 ? Collections.EMPTY_LIST : list.subList(1,list.size());
-        }        
-    };
-            
-/* 
- * Given a List in head/tail form, we should be able to find
- * the List of elements in the tail less than the head:
- */
-    private BinaryFunction lesserTail = new BinaryFunction() {
-        public Object evaluate(Object head, Object tail) {
-            return CollectionAlgorithms.select(
-                ((List)tail).iterator(),
-                RightBoundPredicate.bind(
-                    IsLessThan.getIsLessThan(),
-                    head));            
+            return list.size() < 2 ? 
+                Collections.EMPTY_LIST : 
+                list.subList(1, list.size());
         }
     };
 
 /* 
- * and we should be able to find the List of elements in 
- * the tail greater than the head:
+ * Given a List in head/tail form, we should be able to find
+ * the list of elements in the tail less than the head.
+ * We can simply apply the select algorithm here, using 
+ * a predicate identifying elements less than the head.
+ */
+    private BinaryFunction lesserTail = new ObjectListFunction() {
+        public Object evaluate(Object head, List tail) {
+            return CollectionAlgorithms.select(
+                tail.iterator(),
+                RightBoundPredicate.bind(
+                    IsLessThan.getIsLessThan(), 
+                    head));
+        }
+    };
+
+/* 
+ * We must also be able to find the List of elements in 
+ * the tail greater than (or equal to) the head. This 
+ * is similar to the lesserTail approach.
  */
     private BinaryFunction greaterTail = new BinaryFunction() {
         public Object evaluate(Object head, Object tail) {
             return CollectionAlgorithms.select(
                 ((List)tail).iterator(),
                 RightBoundPredicate.bind(
-                    IsGreaterThanOrEqual.getIsGreaterThanOrEqual(),
-                    head));            
+                    IsGreaterThanOrEqual.getIsGreaterThanOrEqual(), 
+                    head));
         }
     };
 
 /* 
  * With these building blocks, our quicksort function is a 
- * straightfoward application of the description above:
+ * straightfoward application of the description above.
  */
-    private UnaryFunction quicksort = new ConditionalUnaryFunction(        
+    private UnaryFunction quicksort = new ConditionalUnaryFunction(
         IsEmpty.getIsEmpty(),                         /* If the list is empty, */
         new ConstantFunction(Collections.EMPTY_LIST), /*  then return an empty list, */
         new ListFunction() {                          /*  else, split and recurse */
-            public Object evaluate(List list) {
-                List result = new ArrayList(list.size());
-                Object h = head.evaluate(list);
-                Object t = tail.evaluate(list);
-                result.addAll((List)quicksort.evaluate(lesserTail.evaluate(h,t)));
-                result.add(h);
-                result.addAll((List)quicksort.evaluate(greaterTail.evaluate(h,t)));
-                return result;
-            }
+        public Object evaluate(List list) {
+            List result = new ArrayList(list.size());
+            Object h = head.evaluate(list);
+            Object t = tail.evaluate(list);
+            result.addAll((List)quicksort.evaluate(lesserTail.evaluate(h, t)));
+            result.add(h);
+            result.addAll((List)quicksort.evaluate(greaterTail.evaluate(h, t)));
+            return result;
         }
-    );
-    
+    });
 
+/*
+ * Finally, our quicksort method simply invokes the UnaryFunction:
+ */
     public List quicksort(List list) {
         return (List)(quicksort.evaluate(list));
     }
