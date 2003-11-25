6761b354f57fef7b778fb2de895f1cb7472cb9d1
Rodney Waldhoff <rwaldhoff@apache.org>
2003-11-25 19:24:37 +0000

    delete NumberRange, functionality moved to IntegerRange and LongRange
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/sandbox/functor/trunk@144473 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/functor/generator/util/TestAll.java
+++ src/test/org/apache/commons/functor/generator/util/TestAll.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons-sandbox//functor/src/test/org/apache/commons/functor/generator/util/TestAll.java,v 1.3 2003/11/24 23:59:21 rwaldhoff Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons-sandbox//functor/src/test/org/apache/commons/functor/generator/util/TestAll.java,v 1.4 2003/11/25 19:24:37 rwaldhoff Exp $
  * ====================================================================
  * The Apache Software License, Version 1.1
  *
@@ -61,7 +61,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * @version $Revision: 1.3 $ $Date: 2003/11/24 23:59:21 $
+ * @version $Revision: 1.4 $ $Date: 2003/11/25 19:24:37 $
  * @author Rodney Waldhoff
  */
 public class TestAll extends TestCase {
@@ -72,7 +72,6 @@ public class TestAll extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite();
         suite.addTest(TestEachElement.suite());
-        suite.addTest(TestNumberRange.suite());
         suite.addTest(TestIntegerRange.suite());
         suite.addTest(TestLongRange.suite());
         return suite;
