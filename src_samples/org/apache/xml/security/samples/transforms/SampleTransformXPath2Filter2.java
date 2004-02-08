ee78201331bb23c4341bfdc0534eebe1b96275ae
Berin Lautenbach <blautenb@apache.org>
2004-02-08 06:12:21 +0000

    Update to Apache 2.0 license
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@350955 13f79535-47bb-0310-9956-ffa450edef68

--- src_samples/org/apache/xml/security/samples/transforms/SampleTransformXPath2Filter2.java
+++ src_samples/org/apache/xml/security/samples/transforms/SampleTransformXPath2Filter2.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright  2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.apache.xml.security.samples.transforms;
 
 
@@ -21,14 +38,17 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-
 /**
  * Sample for the <I>XML Signature XPath Filter v2.0</I>
  *
  * @author $Author$
- * @see <A HREF="http://www.w3.org/TR/xmldsig-filter2/">XPath Filter v2.0 (TR)</A>
- * @see <A HREF=http://www.w3.org/Signature/Drafts/xmldsig-xfilter2/">XPath Filter v2.0 (editors copy)</A>
+ * @see <A HREF="http://www.w3.org/TR/xmldsig-filter2/">XPath Filter v2.0 (TR)</
+A>
+ * @see <A HREF=http://www.w3.org/Signature/Drafts/xmldsig-xfilter2/">XPath Filt
+er v2.0 (editors copy)</A>
  */
+
+
 public class SampleTransformXPath2Filter2 {
 
    /**
--- src_unitTests/org/apache/xml/security/test/interop/InteropTest.java
+++ src_unitTests/org/apache/xml/security/test/interop/InteropTest.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.apache.xml.security.test.interop;
 
 
@@ -15,11 +32,12 @@ import org.apache.xml.security.utils.resolver.ResourceResolverSpi;
 import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Element;
 
-
 /**
  *
  * @author $Author$
  */
+
+
 public class InteropTest extends TestCase {
 
    /** {@link org.apache.commons.logging} logging facility */
