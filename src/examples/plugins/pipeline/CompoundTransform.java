fb3e243d44ee0ae6a3b884d9663edb3fd97590c7
Simon Kitching <skitching@apache.org>
2004-02-29 05:06:57 +0000

    Plugins example
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/digester/trunk@132570 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/examples/plugins/pipeline/CompoundTransform.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+
+import java.util.LinkedList;
+import java.util.Iterator;
+
+import org.apache.commons.digester.Digester;
+import org.apache.commons.digester.plugins.PluginCreateRule;
+
+/**
+ * An implementation of the Transform interface which is configured with
+ * a sequence of "subtransforms", and applies them one by one to the input
+ * data.
+ * <p>
+ * This demonstrates that a plugged-in class can itself define plugin-points 
+ * for user-defined classes if it wishes.
+ */
+
+public class CompoundTransform implements Transform {
+    private LinkedList transforms = new LinkedList();
+    
+    public void addTransform(Transform transform) {
+        transforms.add(transform);
+    }
+    
+    public String transform(String s) {
+        for(Iterator i = transforms.iterator(); i.hasNext(); ) {
+            Transform t = (Transform) i.next();
+            s = t.transform(s);
+        }
+        return s;
+    }
+    
+    public static void addRules(Digester d, String pattern) {
+        PluginCreateRule pcr = new PluginCreateRule(Transform.class);
+        d.addRule(pattern+"/subtransform", pcr);
+        d.addSetNext(pattern+"/subtransform", "addTransform");
+    }
+}
---
+++ src/examples/plugins/pipeline/SubstituteTransform.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+
+import org.apache.commons.digester.Digester;
+
+/**
+ * An implementation of the Transform interface which replaces all occurrences
+ * of a specified string with a different string.
+ * <p>
+ * Because this class wishes to configure instances via nested "from" and
+ * "to" tags, it needs to define an addRules method to add rules to the
+ * Digester dynamically. Note that there are different ways of defining the
+ * rules though; for example they can be defined in a separate
+ * SubstituteTransformRuleInfo class.
+ */
+ 
+public class SubstituteTransform implements Transform {
+    private String from;
+    private String to;
+    
+    public void setFrom(String from) {
+        this.from = from;
+    }
+    
+    public void setTo(String to) {
+        this.to = to;
+    }
+    
+    public String transform(String s) {
+        StringBuffer buf = new StringBuffer(s);
+        while (true) {
+            int idx = buf.indexOf(from);
+            if (idx == -1)
+                break;
+            
+            StringBuffer buf2 = buf.replace(idx, idx+from.length(), to);
+        }
+        return buf.toString();
+    }
+    
+    public static void addRules(Digester d, String pattern) {
+        d.addCallMethod(pattern+"/from", "setFrom", 0);
+        d.addCallMethod(pattern+"/to", "setTo", 0);
+    }
+}
