a0c322cd5a899c8b2defd33f6a2a9441a5d6326f
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-14 14:25:26 +0000

    o Adding some cli utilities.

---
+++ src/main/java/org/codehaus/plexus/util/cli/CommandLineUtils.java
@@ -0,0 +1,81 @@
+package org.codehaus.plexus.util.cli;
+
+/*
+ * LICENSE
+ */
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l </a>
+ * @version $Id$
+ */
+public abstract class CommandLineUtils
+{
+    public static class StringStreamConsumer
+        implements StreamConsumer
+    {
+        private StringBuffer string = new StringBuffer();
+
+        private String ls = System.getProperty( "line.separator" );
+
+        public void consumeLine( String line )
+        {
+            string.append( line + ls );
+        }
+
+        public String getOutput()
+        {
+            return string.toString();
+        }
+    }
+
+    public static int executeCommandLine( Commandline cl, StreamConsumer systemOut, StreamConsumer systemErr )
+    	throws Exception
+    {
+        if ( cl == null )
+        {
+            throw new IllegalArgumentException( "cl cannot be null." );
+        }
+
+        Process p;
+
+//        System.out.println( "Executing: " + cl );
+//        System.out.println( "pwd: " + cl.getWorkingDirectory().getAbsolutePath() );
+
+        p = cl.execute();
+
+        StreamPumper inputPumper = new StreamPumper( p.getInputStream(), systemOut );
+
+        StreamPumper errorPumper = new StreamPumper( p.getErrorStream(), systemErr );
+
+        inputPumper.start();
+
+        errorPumper.start();
+
+        try
+        {
+            int returnValue = p.waitFor();
+
+            while( !inputPumper.isDone() )
+            {
+                Thread.sleep( 0 );
+            }
+
+            while( !errorPumper.isDone() )
+            {
+                Thread.sleep( 0 );
+            }
+
+            return returnValue;
+        }
+        catch( InterruptedException ex )
+        {
+            throw new Exception( "Error while executing external command.", ex );
+        }
+        finally
+        {
+            inputPumper.close();
+
+            errorPumper.close();
+        }
+    }
+}
