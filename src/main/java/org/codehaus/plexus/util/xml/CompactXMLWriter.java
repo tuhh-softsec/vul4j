1f3e3452fed4598fbcc689788ead7e3e29ae0dba
jvanzyl <jvanzyl@30dfa635-7bf8-0310-9608-e41f2d776689>
2004-10-23 16:34:54 +0000

    o moving all xstream XML code I've been using everywhere into one location so I can remove my cut & paste jobs everywhere.

---
+++ src/main/java/org/codehaus/plexus/util/xml/CompactXMLWriter.java
@@ -0,0 +1,23 @@
+package org.codehaus.plexus.util.xml;
+
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class CompactXMLWriter extends PrettyPrintXMLWriter
+{
+
+    public CompactXMLWriter( PrintWriter writer )
+    {
+        super( writer );
+    }
+
+    public CompactXMLWriter( Writer writer )
+    {
+        super( writer );
+    }
+
+    protected void endOfLine()
+    {
+        // override parent: don't write anything at end of line
+    }
+}
---
+++ src/main/java/org/codehaus/plexus/util/xml/XMLWriter.java
@@ -0,0 +1,14 @@
+package org.codehaus.plexus.util.xml;
+
+public interface XMLWriter
+{
+
+    void startElement( String name );
+
+    void addAttribute( String key, String value );
+
+    void writeText( String text );
+
+    void endElement();
+
+}
