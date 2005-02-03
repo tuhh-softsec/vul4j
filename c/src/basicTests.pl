b408a22b5f9e6817318ec9d9faedca335809e1d4
Milan Tomic <milan@apache.org>
2005-02-03 10:09:57 +0000

    Apache licence fix.
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@351204 13f79535-47bb-0310-9956-ffa450edef68

--- c/src/basicTests.pl
+++ c/src/basicTests.pl
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# imitations under the License.
+# limitations under the License.
 
 # All these setting come from the configure script
 # We use configure to do most of the hard work as it is more
