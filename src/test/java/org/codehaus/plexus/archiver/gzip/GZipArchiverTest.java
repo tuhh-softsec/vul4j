a59176c4deb4efd687e967ab398a21437130091a
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-03-12 00:33:30 +0000

    o Fixing a test that wouldn't run from the reactor.

--- src/test/java/org/codehaus/plexus/archiver/gzip/GZipArchiverTest.java
+++ src/test/java/org/codehaus/plexus/archiver/gzip/GZipArchiverTest.java
@@ -1,29 +1,27 @@
 package org.codehaus.plexus.archiver.gzip;
 
-import java.io.File;
-
 import org.codehaus.plexus.PlexusTestCase;
 import org.codehaus.plexus.archiver.Archiver;
 import org.codehaus.plexus.archiver.zip.ZipArchiver;
 
-public class GZipArchiverTest extends PlexusTestCase
+/**
+ * @author Emmanuel Venisse
+ * @version $Id$
+ */
+public class GZipArchiverTest
+    extends PlexusTestCase
 {
-    public GZipArchiverTest( String testName )
-    {
-        super( testName );
-    }
-    
     public void testCreateArchive() throws Exception
     {
         ZipArchiver zipArchiver = (ZipArchiver) lookup( Archiver.ROLE, "zip" );
-        zipArchiver.addDirectory( new File( "src" ) );
-        zipArchiver.setDestFile( new File( "target/output/archiveForGzip.zip" ) );
+        zipArchiver.addDirectory( getTestFile( "src" ) );
+        zipArchiver.setDestFile( getTestFile( "target/output/archiveForGzip.zip" ) );
         zipArchiver.createArchive();
         GZipArchiver archiver = (GZipArchiver) lookup( Archiver.ROLE, "gzip" );
         String[] inputFiles = new String[1];
         inputFiles[0] = "archiveForGzip.zip";
-        archiver.addDirectory( new File( "target/output") , inputFiles, null );
-        archiver.setDestFile( new File( "target/output/archive.gzip" ) );
+        archiver.addDirectory( getTestFile( "target/output") , inputFiles, null );
+        archiver.setDestFile( getTestFile( "target/output/archive.gzip" ) );
         archiver.createArchive();
     }
 }
