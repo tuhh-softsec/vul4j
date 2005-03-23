103796dcb03a8d0f4dfbfb72591be0f759553007
Raul Benito Garcia <raul@apache.org>
2005-03-23 18:11:03 +0000

    Added serialVersionUID suggested by eclipse
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@351277 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/security/transforms/implementations/FuncHere.java
+++ src/org/apache/xml/security/transforms/implementations/FuncHere.java
@@ -53,6 +53,11 @@ import org.w3c.dom.Node;
 public class FuncHere extends Function {
 
    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+   /**
     * The here function returns a node-set containing the attribute or
     * processing instruction node or the parent element of the text node
     * that directly bears the XPath expression.  This expression results
