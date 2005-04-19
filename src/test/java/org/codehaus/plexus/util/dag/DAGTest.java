1a5643a040b9e10fd4b3ff92eb9eb169cf207cc9
brett <brett@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-04-19 05:38:54 +0000

    start cleaning up exception handling - traded swallowing exception for throwing it (lesser of two evils, will come back at it...) - let tests throw exceptions on error rather than catch, fail, printstacktrace so that tools properly handle them - assertTrue( true ) in empty catches in junit to make IDEA happy - still is clear that is the expected path.

--- src/test/java/org/codehaus/plexus/util/dag/DAGTest.java
+++ src/test/java/org/codehaus/plexus/util/dag/DAGTest.java
@@ -34,117 +34,109 @@ import java.util.Set;
  * @author <a href="michal.maczka@dimatics.com">Michal Maczka</a>
  * @version $Id$
  */
-public class DAGTest extends TestCase
+public class DAGTest
+    extends TestCase
 {
     public void testDAG()
+        throws CycleDetectedException
     {
-        try
-        {
-            final DAG dag = new DAG();
+        final DAG dag = new DAG();
 
-            dag.addVertex( "a" );
+        dag.addVertex( "a" );
 
-            assertEquals( 1, dag.getVerticies().size() );
+        assertEquals( 1, dag.getVerticies().size() );
 
-            assertEquals( "a", dag.getVertex( "a" ).getLabel() );
+        assertEquals( "a", dag.getVertex( "a" ).getLabel() );
 
-            dag.addVertex( "a" );
+        dag.addVertex( "a" );
 
-            assertEquals( 1, dag.getVerticies().size() );
+        assertEquals( 1, dag.getVerticies().size() );
 
-            assertEquals( "a", dag.getVertex( "a" ).getLabel() );
+        assertEquals( "a", dag.getVertex( "a" ).getLabel() );
 
-            dag.addVertex( "b" );
+        dag.addVertex( "b" );
 
-            assertEquals( 2, dag.getVerticies().size() );
+        assertEquals( 2, dag.getVerticies().size() );
 
-            assertFalse( dag.hasEdge( "a", "b" ) );
+        assertFalse( dag.hasEdge( "a", "b" ) );
 
-            assertFalse( dag.hasEdge( "b", "a" ) );
+        assertFalse( dag.hasEdge( "b", "a" ) );
 
-            final Vertex a = dag.getVertex( "a" );
+        final Vertex a = dag.getVertex( "a" );
 
-            final Vertex b = dag.getVertex( "b" );
+        final Vertex b = dag.getVertex( "b" );
 
-            assertEquals( "a", a.getLabel() );
+        assertEquals( "a", a.getLabel() );
 
-            assertEquals( "b", b.getLabel() );
+        assertEquals( "b", b.getLabel() );
 
-            dag.addEdge( "a", "b" );
+        dag.addEdge( "a", "b" );
 
-            assertTrue( a.getChildren().contains( b ) );
+        assertTrue( a.getChildren().contains( b ) );
 
-            assertTrue( b.getParents().contains( a ) );
+        assertTrue( b.getParents().contains( a ) );
 
-            assertTrue( dag.hasEdge( "a", "b" ) );
+        assertTrue( dag.hasEdge( "a", "b" ) );
 
-            assertFalse( dag.hasEdge( "b", "a" ) );
+        assertFalse( dag.hasEdge( "b", "a" ) );
 
-            dag.addEdge( "c", "d" );
+        dag.addEdge( "c", "d" );
 
-            assertEquals( 4, dag.getVerticies().size() );
+        assertEquals( 4, dag.getVerticies().size() );
 
-            final Vertex c = dag.getVertex( "c" );
+        final Vertex c = dag.getVertex( "c" );
 
-            final Vertex d = dag.getVertex( "d" );
+        final Vertex d = dag.getVertex( "d" );
 
-            assertEquals( "a", a.getLabel() );
+        assertEquals( "a", a.getLabel() );
 
-            assertEquals( "b", b.getLabel() );
+        assertEquals( "b", b.getLabel() );
 
-            assertEquals( "c", c.getLabel() );
+        assertEquals( "c", c.getLabel() );
 
-            assertEquals( "d", d.getLabel() );
+        assertEquals( "d", d.getLabel() );
 
-            assertFalse( dag.hasEdge( "b", "a" ) );
+        assertFalse( dag.hasEdge( "b", "a" ) );
 
-            assertFalse( dag.hasEdge( "a", "c" ) );
+        assertFalse( dag.hasEdge( "a", "c" ) );
 
-            assertFalse( dag.hasEdge( "a", "d" ) );
+        assertFalse( dag.hasEdge( "a", "d" ) );
 
-            assertTrue( dag.hasEdge( "c", "d" ) );
+        assertTrue( dag.hasEdge( "c", "d" ) );
 
-            assertFalse( dag.hasEdge( "d", "c" ) );
+        assertFalse( dag.hasEdge( "d", "c" ) );
 
-            final Set labels = dag.getLabels();
+        final Set labels = dag.getLabels();
 
-            assertEquals( 4, labels.size() );
+        assertEquals( 4, labels.size() );
 
-            assertTrue( labels.contains( "a" ) );
+        assertTrue( labels.contains( "a" ) );
 
-            assertTrue( labels.contains( "b" ) );
+        assertTrue( labels.contains( "b" ) );
 
-            assertTrue( labels.contains( "c" ) );
+        assertTrue( labels.contains( "c" ) );
 
-            assertTrue( labels.contains( "d" ) );
+        assertTrue( labels.contains( "d" ) );
 
-            dag.addEdge( "a", "d" );
+        dag.addEdge( "a", "d" );
 
-            assertTrue( a.getChildren().contains( d ) );
+        assertTrue( a.getChildren().contains( d ) );
 
-            assertTrue( d.getParents().contains( a ) );
+        assertTrue( d.getParents().contains( a ) );
 
-            // "b" and "d" are children of "a"
-            assertEquals( 2, a.getChildren().size() );
+        // "b" and "d" are children of "a"
+        assertEquals( 2, a.getChildren().size() );
 
-            assertTrue( a.getChildLabels().contains( "b" ) );
+        assertTrue( a.getChildLabels().contains( "b" ) );
 
-            assertTrue( a.getChildLabels().contains( "d" ) );
+        assertTrue( a.getChildLabels().contains( "d" ) );
 
-            // "a" and "c" are parents of "d"
-            assertEquals( 2, d.getParents().size() );
+        // "a" and "c" are parents of "d"
+        assertEquals( 2, d.getParents().size() );
 
-            assertTrue( d.getParentLabels().contains( "a" ) );
+        assertTrue( d.getParentLabels().contains( "a" ) );
 
-            assertTrue( d.getParentLabels().contains( "c" ) );
-        }
-        catch ( CycleDetectedException e )
-        {
-            e.printStackTrace();
-
-            fail( e.getMessage() );
-        }
-
+        assertTrue( d.getParentLabels().contains( "c" ) );
     }
 
 
--- src/test/java/org/codehaus/plexus/util/dag/TopologicalSorterTest.java
+++ src/test/java/org/codehaus/plexus/util/dag/TopologicalSorterTest.java
@@ -33,177 +33,169 @@ import java.util.List;
  * @author <a href="michal.maczka@dimatics.com">Michal Maczka</a>
  * @version $Id$
  */
-public class TopologicalSorterTest extends TestCase
+public class TopologicalSorterTest
+    extends TestCase
 {
     public void testDfs()
+        throws CycleDetectedException
     {
-        try
-        {
-            // a --> b --->c
-            //
-            // result a,b,c
-            final DAG dag1 = new DAG();
+        // a --> b --->c
+        //
+        // result a,b,c
+        final DAG dag1 = new DAG();
 
-            dag1.addEdge( "a", "b" );
+        dag1.addEdge( "a", "b" );
 
-            dag1.addEdge( "b", "c" );
+        dag1.addEdge( "b", "c" );
 
-            final List expected1 = new ArrayList();
+        final List expected1 = new ArrayList();
 
+        expected1.add( "c" );
 
-            expected1.add( "c" );
+        expected1.add( "b" );
 
-            expected1.add( "b" );
+        expected1.add( "a" );
 
-            expected1.add( "a" );
+        final List actual1 = TopologicalSorter.sort( dag1 );
 
-            final List actual1 = TopologicalSorter.sort( dag1 );
+        assertEquals( "Order is different then expected", expected1, actual1 );
 
-            assertEquals( "Order is different then expected", expected1, actual1 );
+        //
+        //  a <-- b <---c
+        //
+        // result c, b, a
+        final DAG dag2 = new DAG();
 
-            //
-            //  a <-- b <---c
-            //
-            // result c, b, a
-            final DAG dag2 = new DAG();
+        dag2.addVertex( "a" );
 
-            dag2.addVertex( "a" );
+        dag2.addVertex( "b" );
 
-            dag2.addVertex( "b" );
+        dag2.addVertex( "c" );
 
-            dag2.addVertex( "c" );
+        dag2.addEdge( "b", "a" );
 
-            dag2.addEdge( "b", "a" );
+        dag2.addEdge( "c", "b" );
 
-            dag2.addEdge( "c", "b" );
+        final List expected2 = new ArrayList();
 
-            final List expected2 = new ArrayList();
+        expected2.add( "a" );
 
-            expected2.add( "a" );
+        expected2.add( "b" );
 
-            expected2.add( "b" );
+        expected2.add( "c" );
 
-            expected2.add( "c" );
+        final List actual2 = TopologicalSorter.sort( dag2 );
 
-            final List actual2 = TopologicalSorter.sort( dag2 );
+        assertEquals( "Order is different then expected", expected2, actual2 );
 
-            assertEquals( "Order is different then expected", expected2, actual2 );
+        //
+        //  a --> b --> c --> e
+        //        |     |     |
+        //        |     V     V
+        //          --> d <-- f  --> g
+        // result d, g, f, c, b, a
+        final DAG dag3 = new DAG();
 
-            //
-            //  a --> b --> c --> e
-            //        |     |     |
-            //        |     V     V
-            //          --> d <-- f  --> g
-            // result d, g, f, c, b, a
-            final DAG dag3 = new DAG();
+        // force order of nodes in the graph
+        dag3.addVertex( "a" );
 
-            // force order of nodes in the graph
-            dag3.addVertex( "a" );
+        dag3.addVertex( "b" );
 
-            dag3.addVertex( "b" );
+        dag3.addVertex( "c" );
 
-            dag3.addVertex( "c" );
+        dag3.addVertex( "d" );
 
-            dag3.addVertex( "d" );
+        dag3.addVertex( "e" );
 
-            dag3.addVertex( "e" );
+        dag3.addVertex( "f" );
 
-            dag3.addVertex( "f" );
+        dag3.addEdge( "a", "b" );
 
-            dag3.addEdge( "a", "b" );
+        dag3.addEdge( "b", "c" );
 
-            dag3.addEdge( "b", "c" );
+        dag3.addEdge( "b", "d" );
 
-            dag3.addEdge( "b", "d" );
+        dag3.addEdge( "c", "d" );
 
-            dag3.addEdge( "c", "d" );
+        dag3.addEdge( "c", "e" );
 
-            dag3.addEdge( "c", "e" );
+        dag3.addEdge( "f", "d" );
 
-            dag3.addEdge( "f", "d" );
+        dag3.addEdge( "e", "f" );
 
-            dag3.addEdge( "e", "f" );
+        dag3.addEdge( "f", "g" );
 
-            dag3.addEdge( "f", "g" );
+        final List expected3 = new ArrayList();
 
-            final List expected3 = new ArrayList();
+        expected3.add( "d" );
 
-            expected3.add( "d" );
+        expected3.add( "g" );
 
-            expected3.add( "g" );
+        expected3.add( "f" );
 
-            expected3.add( "f" );
+        expected3.add( "e" );
 
-            expected3.add( "e" );
+        expected3.add( "c" );
 
-            expected3.add( "c" );
+        expected3.add( "b" );
 
-            expected3.add( "b" );
+        expected3.add( "a" );
 
-            expected3.add( "a" );
+        final List actual3 = TopologicalSorter.sort( dag3 );
 
-            final List actual3 = TopologicalSorter.sort( dag3 );
+        assertEquals( "Order is different then expected", expected3, actual3 );
 
-            assertEquals( "Order is different then expected", expected3, actual3 );
+        //
+        //  a --> b --> c --> e
+        //        |     |     |
+        //        |     V     V
+        //          --> d <-- f
+        // result d, f, e, c, b, a
+        final DAG dag4 = new DAG();
+        // force order of nodes in the graph
 
-            //
-            //  a --> b --> c --> e
-            //        |     |     |
-            //        |     V     V
-            //          --> d <-- f
-            // result d, f, e, c, b, a
-            final DAG dag4 = new DAG();
-            // force order of nodes in the graph
+        dag4.addVertex( "f" );
 
-            dag4.addVertex( "f" );
+        dag4.addVertex( "e" );
 
-            dag4.addVertex( "e" );
+        dag4.addVertex( "d" );
 
-            dag4.addVertex( "d" );
+        dag4.addVertex( "c" );
 
-            dag4.addVertex( "c" );
+        dag4.addVertex( "a" );
 
-            dag4.addVertex( "a" );
+        dag4.addVertex( "b" );
 
-            dag4.addVertex( "b" );
+        dag4.addEdge( "a", "b" );
 
-            dag4.addEdge( "a", "b" );
+        dag4.addEdge( "b", "c" );
 
-            dag4.addEdge( "b", "c" );
+        dag4.addEdge( "b", "d" );
 
-            dag4.addEdge( "b", "d" );
+        dag4.addEdge( "c", "d" );
 
-            dag4.addEdge( "c", "d" );
+        dag4.addEdge( "c", "e" );
 
-            dag4.addEdge( "c", "e" );
+        dag4.addEdge( "f", "d" );
 
-            dag4.addEdge( "f", "d" );
+        dag4.addEdge( "e", "f" );
 
-            dag4.addEdge( "e", "f" );
+        final List expected4 = new ArrayList();
 
-            final List expected4 = new ArrayList();
+        expected4.add( "d" );
 
-            expected4.add( "d" );
+        expected4.add( "f" );
 
-            expected4.add( "f" );
+        expected4.add( "e" );
 
-            expected4.add( "e" );
+        expected4.add( "c" );
 
-            expected4.add( "c" );
+        expected4.add( "b" );
 
-            expected4.add( "b" );
+        expected4.add( "a" );
 
-            expected4.add( "a" );
+        final List actual4 = TopologicalSorter.sort( dag4 );
 
-            final List actual4 = TopologicalSorter.sort( dag4 );
-
-            assertEquals( "Order is different then expected", expected4, actual4 );
-        }
-        catch ( CycleDetectedException e )
-        {
-            e.printStackTrace();
-
-            fail( e.getMessage() );
-        }
+        assertEquals( "Order is different then expected", expected4, actual4 );
     }
 }
