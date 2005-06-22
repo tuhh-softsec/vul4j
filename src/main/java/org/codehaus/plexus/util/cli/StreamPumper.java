4a1454c5738ff510364fc3bb31119973f635710e
evenisse <evenisse@30dfa635-7bf8-0310-9608-e41f2d776689>
2005-06-22 16:00:12 +0000

    Clean imports

--- src/main/java/org/codehaus/plexus/util/cli/StreamPumper.java
+++ src/main/java/org/codehaus/plexus/util/cli/StreamPumper.java
@@ -79,7 +79,6 @@ package org.codehaus.plexus.util.cli;
  */
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
--- src/main/java/org/codehaus/plexus/util/reflection/Reflector.java
+++ src/main/java/org/codehaus/plexus/util/reflection/Reflector.java
@@ -41,7 +41,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
