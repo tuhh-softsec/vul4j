a05af70be5b626f90deeb5d539d8fac36b1ca936
Milan Tomic <milan@apache.org>
2005-08-21 14:25:00 +0000

    Initial commit of NSS files.
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@351432 13f79535-47bb-0310-9956-ffa450edef68

---
+++ c/src/enc/NSS/NSSCryptoKeyHMAC.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * XSEC
+ *
+ * NSSCryptoKeyHMAC := HMAC Keys
+ *
+ * Author(s): Milan Tomic
+ *
+ */
+
+#include <xsec/enc/NSS/NSSCryptoKeyHMAC.hpp>
+#include <xsec/framework/XSECError.hpp>
+#include <xsec/enc/XSECCryptoException.hpp>
+
+#if defined (HAVE_NSS)
+
+// --------------------------------------------------------------------------------
+//           Constructor
+// --------------------------------------------------------------------------------
+
+NSSCryptoKeyHMAC::NSSCryptoKeyHMAC() :m_keyBuf("") {
+
+	m_keyBuf.isSensitive();
+	m_keyLen = 0;
+
+};
+
+// --------------------------------------------------------------------------------
+//           Set key
+// --------------------------------------------------------------------------------
+
+void NSSCryptoKeyHMAC::setKey(unsigned char * inBuf, unsigned int inLength) {
+
+	m_keyBuf.sbMemcpyIn(inBuf, inLength);
+	m_keyLen = inLength;
+
+}
+
+// --------------------------------------------------------------------------------
+//           Get key
+// --------------------------------------------------------------------------------
+
+unsigned int NSSCryptoKeyHMAC::getKey(safeBuffer &outBuf) {
+
+	outBuf = m_keyBuf;
+	return m_keyLen;
+
+}
+
+// --------------------------------------------------------------------------------
+//           Replicate key
+// --------------------------------------------------------------------------------
+
+XSECCryptoKey * NSSCryptoKeyHMAC::clone() {
+
+	NSSCryptoKeyHMAC * ret;
+
+	XSECnew(ret, NSSCryptoKeyHMAC());
+
+	ret->m_keyBuf = m_keyBuf;
+	ret->m_keyLen = m_keyLen;
+
+	return ret;
+
+}
+
+#endif /* HAVE_NSS */
