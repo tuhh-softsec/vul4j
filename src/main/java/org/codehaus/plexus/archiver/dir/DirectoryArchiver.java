d6d5f0f3d1e7a90b6332535400c1f566b2ee9221
trygvis <trygvis@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-02-14 16:02:44 +0000

    o Adding chmod'ing files to the directory archiver.

--- src/main/java/org/codehaus/plexus/archiver/dir/DirectoryArchiver.java
+++ src/main/java/org/codehaus/plexus/archiver/dir/DirectoryArchiver.java
@@ -17,8 +17,6 @@ package org.codehaus.plexus.archiver.dir;
  */
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
@@ -26,15 +24,20 @@ import java.util.Map;
 import org.codehaus.plexus.archiver.AbstractArchiver;
 import org.codehaus.plexus.archiver.ArchiveEntry;
 import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.Os;
+import org.codehaus.plexus.util.cli.CommandLineUtils;
+import org.codehaus.plexus.util.cli.Commandline;
+import org.codehaus.plexus.util.cli.CommandLineException;
 
 /**
  * A plexus archiver implementation that stores the files to archive in a
  * directory.
  */
-public class DirectoryArchiver 
-	extends AbstractArchiver 
+public class DirectoryArchiver
+    extends AbstractArchiver
 {
-    public void resetArchiver() 
+    public void resetArchiver()
     {
         setIncludeEmptyDirs( false );
         getDirs().clear();
@@ -42,152 +45,169 @@ public class DirectoryArchiver
         setIncludeEmptyDirs( true );
     }
 
-	public void createArchive() 
-		throws ArchiverException, IOException 
-	{
-		//Most of this method was copied from org.codehaus.plexus.archiver.tar.TarArchiver
-		//and modified to store files in a directory, not a tar archive.
-		Map listFiles = getFiles();
-		if ( listFiles == null || listFiles.size() == 0 ) 
-		{
-			throw new ArchiverException( "You must set at least one file." );
-		}
+    public void createArchive()
+        throws ArchiverException, IOException
+    {
+        //Most of this method was copied from org.codehaus.plexus.archiver.tar.TarArchiver
+        //and modified to store files in a directory, not a tar archive.
+        Map listFiles = getFiles();
+        if ( listFiles == null || listFiles.size() == 0 )
+        {
+            throw new ArchiverException( "You must set at least one file." );
+        }
 
-		File destDirectory = getDestFile();
-		if ( destDirectory == null ) 
-		{
-			throw new ArchiverException( "You must set the destination directory." );
-		}
-		if ( destDirectory.exists() && !destDirectory.isDirectory() ) 
-		{
-			throw new ArchiverException( destDirectory + " is not a directory." );
-		}
-		if ( destDirectory.exists() && !destDirectory.canWrite() ) 
-		{
-			throw new ArchiverException( destDirectory + " is not writable." );
-		}
+        File destDirectory = getDestFile();
+        if ( destDirectory == null )
+        {
+            throw new ArchiverException( "You must set the destination directory." );
+        }
+        if ( destDirectory.exists() && !destDirectory.isDirectory() )
+        {
+            throw new ArchiverException( destDirectory + " is not a directory." );
+        }
+        if ( destDirectory.exists() && !destDirectory.canWrite() )
+        {
+            throw new ArchiverException( destDirectory + " is not writable." );
+        }
 
-		// Check if we don't add directory file in itself
-		for ( Iterator iter = getFiles().keySet().iterator(); iter.hasNext(); ) 
-		{
-			String fileName = ( String ) iter.next();
-			ArchiveEntry fileToAdd = ( ArchiveEntry ) getFiles().get( fileName );
-			if ( destDirectory.equals( fileToAdd.getFile() ) ) 
-			{
-				throw new ArchiverException(
-						"The destination directory cannot include itself.");
-			}
-		}
+        // Check if we don't add directory file in itself
+        for ( Iterator iter = getFiles().keySet().iterator(); iter.hasNext(); )
+        {
+            String fileName = (String) iter.next();
+            ArchiveEntry fileToAdd = (ArchiveEntry) getFiles().get( fileName );
+            if ( destDirectory.equals( fileToAdd.getFile() ) )
+            {
+                throw new ArchiverException( "The destination directory cannot include itself." );
+            }
+        }
 
-		getLogger().info( "Copying " + listFiles.size() + " files to " + destDirectory.getAbsolutePath() );
+        getLogger().info( "Copying " + listFiles.size() + " files to " + destDirectory.getAbsolutePath() );
 
-		try 
-		{
-			for ( Iterator iter = getFiles().keySet().iterator(); iter.hasNext(); ) 
-			{
-				String fileName = ( String ) iter.next();
-				ArchiveEntry f = ( ArchiveEntry ) getFiles().get( fileName );
-				String destDir = destDirectory.getCanonicalPath();
-				fileName = destDir + File.separator + fileName;
-				copyFile( f, fileName );
-			}
-		}
-		catch ( IOException ioe ) 
-		{
-			String message = "Problem copying files : " + ioe.getMessage();
-			throw new ArchiverException( message, ioe );
-		}
-	}
+        try
+        {
+            for ( Iterator iter = getFiles().keySet().iterator(); iter.hasNext(); )
+            {
+                String fileName = (String) iter.next();
+                ArchiveEntry f = (ArchiveEntry) getFiles().get( fileName );
+                String destDir = destDirectory.getCanonicalPath();
+                fileName = destDir + File.separator + fileName;
+                copyFile( f, fileName );
+            }
+        }
+        catch ( IOException ioe )
+        {
+            String message = "Problem copying files : " + ioe.getMessage();
+            throw new ArchiverException( message, ioe );
+        }
+    }
 
-	/**
-	 * Copies the specified file to the specified path, creating any ancestor directory
-	 * structure as necessary.
-	 * 
-	 * @param file The file to copy (IOException will be thrown if this does not exist)
-	 * @param vPath The fully qualified path to copy the file to.
-	 * @throws ArchiverException If there is a problem creating the directory structure
-	 * @throws IOException If there is a problem copying the file
-	 */
-	protected void copyFile( ArchiveEntry entry, String vPath )
-			throws ArchiverException, IOException 
-	{
-		// don't add "" to the archive
-		if ( vPath.length() <= 0 ) 
-		{
-			return;
-		}
-		
-		File inFile = entry.getFile();
-		File outFile = new File( vPath );
+    /**
+     * Copies the specified file to the specified path, creating any ancestor directory
+     * structure as necessary.
+     *
+     * @param file  The file to copy (IOException will be thrown if this does not exist)
+     * @param vPath The fully qualified path to copy the file to.
+     * @throws ArchiverException If there is a problem creating the directory structure
+     * @throws IOException       If there is a problem copying the file
+     */
+    protected void copyFile( ArchiveEntry entry, String vPath )
+        throws ArchiverException, IOException
+    {
+        // don't add "" to the archive
+        if ( vPath.length() <= 0 )
+        {
+            return;
+        }
 
-		if ( outFile.exists() && outFile.lastModified() >= inFile.lastModified() ) 
-		{
-			//already up to date...
-			return;
-		}
-		
-		outFile.setLastModified( inFile.lastModified() );
-		
-		if ( ! inFile.isDirectory() )
-		{
-			if ( ! outFile.getParentFile().exists() ) 
-			{
-				//create the parent directory...
-				if ( ! outFile.getParentFile().mkdirs() ) 
-				{
-					//Failure, unable to create specified directory for some unknown reason.
-					throw new ArchiverException ("Unable to create directory or parent directory of "
-									+ outFile );
-				}
-			}
-			FileInputStream fIn = new FileInputStream( inFile );
-			FileOutputStream fout = new FileOutputStream( outFile );
-			try 
-			{
-				byte[] buffer = new byte[ 8 * 1024 ];
-				int count = 0;
-				do 
-				{
-					fout.write( buffer, 0, count );
-					count = fIn.read( buffer, 0, buffer.length );
-				} 
-				while ( count != -1 );
-			} 
-			finally 
-			{
-				try 
-				{
-					fIn.close();
-				}
-				catch ( IOException ioe ) 
-				{
-					fout.close();
-					throw ioe;
-				}
-				fout.close();
-			}
-		}
-		else 
-		{ //file is a directory
-			if ( outFile.exists() ) 
-			{
-				if ( ! outFile.isDirectory() ) 
-				{
-					//should we just delete the file and replace it with a directory?
-					//throw an exception, let the user delete the file manually.
-					throw new ArchiverException(
-							"Expected directory and found file at copy destination of "
-									+ inFile + " to " + outFile );
-				}
-			} 
-			else if ( ! outFile.mkdirs() ) 
-			{
-				//Failure, unable to create specified directory for some unknown reason.
-				throw new ArchiverException(
-						"Unable to create directory or parent directory of "
-								+ outFile );
-			}
+        File inFile = entry.getFile();
+        File outFile = new File( vPath );
 
-		}
-	}
+        if ( outFile.exists() && outFile.lastModified() >= inFile.lastModified() )
+        {
+            //already up to date...
+            return;
+        }
+
+        outFile.setLastModified( inFile.lastModified() );
+
+        if ( ! inFile.isDirectory() )
+        {
+            if ( ! outFile.getParentFile().exists() )
+            {
+                //create the parent directory...
+                if ( ! outFile.getParentFile().mkdirs() )
+                {
+                    //Failure, unable to create specified directory for some unknown reason.
+                    throw new ArchiverException( "Unable to create directory or parent directory of " + outFile );
+                }
+            }
+            FileUtils.copyFile( inFile, outFile );
+            chmod( outFile, entry.getMode() );
+        }
+        else
+        { //file is a directory
+            if ( outFile.exists() )
+            {
+                if ( ! outFile.isDirectory() )
+                {
+                    //should we just delete the file and replace it with a directory?
+                    //throw an exception, let the user delete the file manually.
+                    throw new ArchiverException(
+                        "Expected directory and found file at copy destination of " + inFile + " to " + outFile );
+                }
+            }
+            else if ( ! outFile.mkdirs() )
+            {
+                //Failure, unable to create specified directory for some unknown reason.
+                throw new ArchiverException( "Unable to create directory or parent directory of " + outFile );
+            }
+        }
+    }
+
+    private void chmod( File file, int mode )
+        throws ArchiverException
+    {
+        if ( ! Os.isArch( "unix" ) )
+        {
+            return;
+        }
+
+        try
+        {
+            Commandline commandline = new Commandline();
+
+            commandline.setWorkingDirectory( file.getParentFile().getAbsolutePath() );
+
+            commandline.setExecutable( "chmod" );
+
+            commandline.createArgument().setValue( Integer.toOctalString( mode & 0xfff ) );
+
+            commandline.createArgument().setValue( file.getAbsolutePath() );
+
+            CommandLineUtils.StringStreamConsumer stderr = new CommandLineUtils.StringStreamConsumer();
+
+            CommandLineUtils.StringStreamConsumer stdout = new CommandLineUtils.StringStreamConsumer();
+
+            int exitCode = CommandLineUtils.executeCommandLine( commandline, stderr, stdout );
+
+            if ( exitCode != 0 )
+            {
+                getLogger().warn( "-------------------------------" );
+                getLogger().warn( "Standard error:" );
+                getLogger().warn( "-------------------------------" );
+                getLogger().warn( stderr.getOutput() );
+                getLogger().warn( "-------------------------------" );
+                getLogger().warn( "Standard output:" );
+                getLogger().warn( "-------------------------------" );
+                getLogger().warn( stdout.getOutput() );
+                getLogger().warn( "-------------------------------" );
+
+                throw new ArchiverException( "chmod exit code was: " + exitCode );
+            }
+        }
+        catch ( CommandLineException e )
+        {
+            throw new ArchiverException( "Error while executing chmod.", e );
+        }
+    }
 }
