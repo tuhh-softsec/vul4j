21f668c3338944c5bce556db47e2baedba86921c
jesse <jesse@30dfa635-7bf8-0310-9608-e41f2d776689>
2006-03-22 17:32:44 +0000

    added utility methods on ReflectionUtils to get variables and values based on an object passed in

--- src/main/java/org/codehaus/plexus/util/ReflectionUtils.java
+++ src/main/java/org/codehaus/plexus/util/ReflectionUtils.java
@@ -3,6 +3,9 @@ package org.codehaus.plexus.util;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.AccessibleObject;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  *
@@ -16,7 +19,7 @@ public final class ReflectionUtils
     {
         // requirement for utility class
     }
-    
+
     public static Field getFieldByNameIncludingSuperclasses( String fieldName, Class clazz  )
     {
         Field retValue = null;
@@ -67,5 +70,90 @@ public final class ReflectionUtils
 
         return null;
     }
+
+
+    /**
+     * Generates a map of the fields and values on a given object,
+     * also pulls from superclasses
+     *
+     * @param object the object to generate the list of fields from
+     * @return map containing the fields and their values
+     */
+    public static Object getValueIncludingSuperclasses( String variable, Object object ) throws IllegalArgumentException
+    {
+        try
+        {
+            Class clazz = object.getClass();
+
+            Field field = clazz.getDeclaredField(variable);
+
+            field.setAccessible( true );
+
+            return field.get( object );
+        }
+        catch ( NoSuchFieldException nsfe )
+        {
+            throw new IllegalArgumentException( variable + " does not exist on " + object.getClass().getName() );
+        }
+        catch ( IllegalAccessException iae )
+        {
+            throw new IllegalArgumentException( variable + " could not be accessed on " + object.getClass().getName() );
+        }
+    }
+
+    /**
+     * Generates a map of the fields and values on a given object,
+     * also pulls from superclasses
+     *
+     * @param object the object to generate the list of fields from
+     * @return map containing the fields and their values
+     */
+    public static Map getVariablesAndValuesIncludingSuperclasses( Object object )
+    {
+        HashMap map = new HashMap ();
+
+        gatherVariablesAndValuesIncludingSuperclasses(object, map);
+
+        return map;
+    }
+
+
+    /**
+     * populates a map of the fields and values on a given object,
+     * also pulls from superclasses
+     *
+     * @param object the object to generate the list of fields from
+     * @param map to populate
+     */
+    private static void gatherVariablesAndValuesIncludingSuperclasses( Object object, Map map )
+    {
+
+        Class clazz = object.getClass();
+
+        Field[] fields = clazz.getDeclaredFields();
+
+        AccessibleObject.setAccessible( fields, true);
+
+        for (int i = 0; i < fields.length; ++i)
+        {
+            Field field = fields[i];
+            try
+            {
+                map.put( field.getName(), field.get( object ) );
+            }
+            catch (IllegalAccessException iae) {
+                map.put( field.getName(), null );
+            }
+
+        }
+
+        Class superclass = clazz.getSuperclass();
+
+        if ( !Object.class.equals(  superclass ) )
+        {
+            gatherVariablesAndValuesIncludingSuperclasses( superclass, map );
+        }
+    }
+
 
 }
