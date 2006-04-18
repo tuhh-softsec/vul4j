d34f2ae66c33a68d20de471a354cbcc139b4fbd0
Berin Lautenbach <blautenb@apache.org>
2006-04-18 10:24:00 +0000

    Add versioning logic from Xerces
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@394902 13f79535-47bb-0310-9956-ffa450edef68

--- c/src/framework/XSECW32Config.hpp
+++ c/src/framework/XSECW32Config.hpp
@@ -29,9 +29,15 @@
 
 #include <xercesc/util/XercesVersion.hpp>
 
-#define XSEC_VERSION	     "1.2.0"
+/*
+ * These are the high level numerics that need to be changed to bump the
+ * version number.
+ *
+ * They are used to create version strings - see the details at the end of this file
+ */
+
 #define XSEC_VERSION_MAJOR   1
-#define XSEC_VERSION_MEDIUM  2
+#define XSEC_VERSION_MEDIUM  3
 #define XSEC_VERSION_MINOR   0
 
 /*
@@ -141,7 +147,10 @@
 
 #define HAVE_WINCAPI 1
 
-#define HAVE_NSS 1
+// NSS Code is currently alpha.  It should work, but you will also
+// need to include the NSS libraries during the link.
+
+/* #define HAVE_NSS 1 */
 
 
 /*
@@ -175,5 +184,34 @@
 
 /* Windows direct.h */
 #define HAVE_DIRECT_H 1
+
+// --------------------------------------------------------------------------------
+//           Version Handling
+// --------------------------------------------------------------------------------
+
+/*
+ * The following code makes use of the Xerces version handling macros to define
+ * some constants that can be used during conditional compilation.
+ */
+
+/* This can be used for conditional compilation and for testing during
+ * autoconfigures.
+ *
+ * It will create a string of the form 10000 * MAJOR + 100 * MEDIUM + MINOR
+ * E.g. 10301 for version 1.3.1
+ */
+
+#define _XSEC_VERSION_FULL CALC_EXPANDED_FORM (XSEC_VERSION_MAJOR,XSEC_VERSION_MEDIUM,XSEC_VERSION_MINOR)
+
+/* Some useful strings for versioning - based on the same strings from Xerces */
+
+#define XSEC_FULLVERSIONSTR INVK_CAT3_SEP_UNDERSCORE(XSEC_VERSION_MAJOR,XSEC_VERSION_MEDIUM,XSEC_VERSION_MINOR)
+#define XSEC_FULLVERSIONDOT INVK_CAT3_SEP_PERIOD(XSEC_VERSION_MAJOR,XSEC_VERSION_MEDIUM,XSEC_VERSION_MINOR)
+#define XSEC_FULLVERSIONNUM INVK_CAT3_SEP_NIL(XSEC_VERSION_MAJOR,XSEC_VERSION_MEDIUM,XSEC_VERSION_MINOR)
+#define XSEC_VERSIONSTR     INVK_CAT2_SEP_UNDERSCORE(XSEC_VERSION_MAJOR,XSEC_VERSION_MEDIUM)
+
+/* The following is used for backwards compatibility with previous version handling */
+
+#define XSEC_VERSION	     "XSEC_FULLVERSIONDOT"
 
 
