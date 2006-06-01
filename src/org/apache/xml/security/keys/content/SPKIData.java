de9f05a35d9f17288e822fee4163c3265010beaf
Sean Mullan <mullan@apache.org>
2006-06-01 21:21:13 +0000

    Fixed findbugs bug: Unread field: SPKIData.log
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@410958 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/security/keys/content/SPKIData.java
+++ src/org/apache/xml/security/keys/content/SPKIData.java
@@ -16,13 +16,10 @@
  */
 package org.apache.xml.security.keys.content;
 
-
-
 import org.apache.xml.security.exceptions.XMLSecurityException;
 import org.apache.xml.security.utils.Constants;
 import org.apache.xml.security.utils.SignatureElementProxy;
 import org.w3c.dom.Element;
-
 
 /**
  *
@@ -30,10 +27,6 @@ import org.w3c.dom.Element;
  * $todo$ implement
  */
 public class SPKIData extends SignatureElementProxy implements KeyInfoContent {
-
-   /** {@link org.apache.commons.logging} logging facility */
-    static org.apache.commons.logging.Log log = 
-        org.apache.commons.logging.LogFactory.getLog(SPKIData.class.getName());
 
    /**
     * Constructor SPKIData
