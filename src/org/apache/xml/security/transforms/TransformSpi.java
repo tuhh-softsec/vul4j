03fced64da73310868a7ad9193b7bc7a425e0d2a
Sean Mullan <mullan@apache.org>
2006-09-25 20:51:36 +0000

    Change enginePerformTransform to throw UnsupportedOperationExc instead of sun-specific NotImplementedExc. Also, improve javadoc a little and use 4-space indents.
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@449812 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/xml/security/transforms/TransformSpi.java
+++ src/org/apache/xml/security/transforms/TransformSpi.java
@@ -25,109 +25,110 @@ import org.apache.xml.security.c14n.InvalidCanonicalizerException;
 import org.apache.xml.security.signature.XMLSignatureInput;
 import org.xml.sax.SAXException;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-
 /**
  * Base class which all Transform algorithms extend. The common methods that
- * have to be overridden are the {@link #enginePerformTransform(XMLSignatureInput, Transform)} method.
+ * have to be overridden are the 
+ * {@link #enginePerformTransform(XMLSignatureInput, Transform)} method.
  *
  * @author Christian Geuer-Pollmann
  */
 public abstract class TransformSpi {
-	/**
-	 * For API compatibility not thread safe.
-	 * @deprecated
-	 */
-	protected Transform _transformObject = null;
-	/**
-	 * Set the transform object. 
-	 * Depeprecated For API compatibility.
-	 * @param transform
-	 * @deprecated
-	 */
-	protected void setTransform(Transform transform) {
-	   this._transformObject = transform;
-	}
-   /**
-    * The mega method which MUST be implemented by the Transformation Algorithm.
-    *
-    * @param input {@link XMLSignatureInput} as the input of transformation
- * @param os where to output this transformation.
- * @param _transformObject TODO
-    * @return {@link XMLSignatureInput} as the result of transformation
-    * @throws CanonicalizationException
-    * @throws IOException
-    * @throws InvalidCanonicalizerException
-    * @throws ParserConfigurationException
-    * @throws SAXException
-    * @throws TransformationException
-    */
-   protected XMLSignatureInput enginePerformTransform(
-      XMLSignatureInput input, OutputStream os, Transform _transformObject)
-         throws IOException,
-                CanonicalizationException, InvalidCanonicalizerException,
-                TransformationException, ParserConfigurationException,
-                SAXException {
-   	    return enginePerformTransform(input, _transformObject);
-   }
-   /**
-    * The mega method which MUST be implemented by the Transformation Algorithm.
-    *
-    * @param input {@link XMLSignatureInput} as the input of transformation
- * @param _transformObject TODO
-    * @return {@link XMLSignatureInput} as the result of transformation
-    * @throws CanonicalizationException
-    * @throws IOException
-    * @throws InvalidCanonicalizerException
-    * @throws ParserConfigurationException
-    * @throws SAXException
-    * @throws TransformationException
-    */
-   protected XMLSignatureInput enginePerformTransform(
-      XMLSignatureInput input, Transform _transformObject)
-         throws IOException,
-                CanonicalizationException, InvalidCanonicalizerException,
-                TransformationException, ParserConfigurationException,
-                SAXException {
-	   //Default implementation overide with a much better
-	   TransformSpi tmp;
+    /**
+     * For API compatibility not thread safe.
+     * @deprecated
+     */
+    protected Transform _transformObject = null;
+    /**
+     * Set the transform object. 
+     * Depeprecated For API compatibility.
+     * @param transform the Transform
+     * @deprecated
+     */
+    protected void setTransform(Transform transform) {
+        this._transformObject = transform;
+    }
+    /**
+     * The mega method which MUST be implemented by the Transformation Algorithm.
+     *
+     * @param input {@link XMLSignatureInput} as the input of transformation
+     * @param os where to output this transformation.
+     * @param _transformObject the Transform
+     * @return {@link XMLSignatureInput} as the result of transformation
+     * @throws CanonicalizationException
+     * @throws IOException
+     * @throws InvalidCanonicalizerException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     * @throws TransformationException
+     */
+    protected XMLSignatureInput enginePerformTransform(
+        XMLSignatureInput input, OutputStream os, Transform _transformObject)
+        throws IOException,
+               CanonicalizationException, InvalidCanonicalizerException,
+               TransformationException, ParserConfigurationException,
+               SAXException {
+        return enginePerformTransform(input, _transformObject);
+    }
+    /**
+     * The mega method which MUST be implemented by the Transformation Algorithm.
+     * In order to be compatible with preexisting Transform implementations, 
+     * by default this implementation invokes the deprecated, thread-unsafe
+     * methods. Subclasses should override this with a thread-safe 
+     * implementation.
+     * 
+     * @param input {@link XMLSignatureInput} as the input of transformation
+     * @param _transformObject the Transform
+     * @return {@link XMLSignatureInput} as the result of transformation
+     * @throws CanonicalizationException
+     * @throws IOException
+     * @throws InvalidCanonicalizerException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     * @throws TransformationException
+     */
+    protected XMLSignatureInput enginePerformTransform(
+        XMLSignatureInput input, Transform _transformObject)
+        throws IOException,
+               CanonicalizationException, InvalidCanonicalizerException,
+               TransformationException, ParserConfigurationException,
+               SAXException {
+	//Default implementation overide with a much better
+	TransformSpi tmp;
 	try {
-		tmp = (TransformSpi) getClass().newInstance();
-		tmp.setTransform(_transformObject);
-		return tmp.enginePerformTransform(input);
+	    tmp = (TransformSpi) getClass().newInstance();
+	    tmp.setTransform(_transformObject);
+	    return tmp.enginePerformTransform(input);
 	} catch (InstantiationException e) {
-		throw new TransformationException("",e);
+	    throw new TransformationException("",e);
 	} catch (IllegalAccessException e) {
-		throw new TransformationException("",e);
+	    throw new TransformationException("",e);
 	}
-   }
+    }
 
-   /**
-    * The mega method which MUST be implemented by the Transformation Algorithm.
-    * @deprecated
-    * @param input {@link XMLSignatureInput} as the input of transformation
- * @param _transformObject TODO
-    * @return {@link XMLSignatureInput} as the result of transformation
-    * @throws CanonicalizationException
-    * @throws IOException
-    * @throws InvalidCanonicalizerException
-    * @throws ParserConfigurationException
-    * @throws SAXException
-    * @throws TransformationException
-    */
-   protected XMLSignatureInput enginePerformTransform(
-      XMLSignatureInput input)
-         throws IOException,
-                CanonicalizationException, InvalidCanonicalizerException,
-                TransformationException, ParserConfigurationException,
-                SAXException {
-	   throw new NotImplementedException();
-   };
-   /**
-    * Returns the URI representation of <code>Transformation algorithm</code>
-    *
-    * @return the URI representation of <code>Transformation algorithm</code>
-    */
-   protected abstract String engineGetURI();
+    /**
+     * The mega method which MUST be implemented by the Transformation Algorithm.
+     * @deprecated
+     * @param input {@link XMLSignatureInput} as the input of transformation
+     * @return {@link XMLSignatureInput} as the result of transformation
+     * @throws CanonicalizationException
+     * @throws IOException
+     * @throws InvalidCanonicalizerException
+     * @throws ParserConfigurationException
+     * @throws SAXException
+     * @throws TransformationException
+     */
+    protected XMLSignatureInput enginePerformTransform(
+        XMLSignatureInput input)
+        throws IOException,
+               CanonicalizationException, InvalidCanonicalizerException,
+               TransformationException, ParserConfigurationException,
+               SAXException {
+	throw new UnsupportedOperationException();
+    }
+    /**
+     * Returns the URI representation of <code>Transformation algorithm</code>
+     *
+     * @return the URI representation of <code>Transformation algorithm</code>
+     */
+    protected abstract String engineGetURI();
 }
