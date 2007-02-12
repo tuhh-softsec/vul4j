b8067e089853735c4accd4a784dfb362d75626fe
Sean Mullan <mullan@apache.org>
2007-02-12 22:43:09 +0000

    New test for bug 41510: org.apache.xml.security.keys.content.KeyValue.getPublicKey() returns null for DSA key
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@506726 13f79535-47bb-0310-9956-ffa450edef68

--- src_unitTests/org/apache/xml/security/test/signature/AllTests.java
+++ src_unitTests/org/apache/xml/security/test/signature/AllTests.java
@@ -5,16 +5,16 @@ import junit.framework.TestSuite;
 
 public class AllTests {
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite(
-				"Test for org.apache.xml.security.test.signature");
-		//$JUnit-BEGIN$
-		suite.addTest(CreateSignatureTest.suite());
-		suite.addTestSuite(X509DataTest.class);
-		suite.addTest(XMLSignatureInputTest.suite());
-		suite.addTest(UnknownAlgoSignatureTest.suite());
-		//$JUnit-END$
-		return suite;
-	}
-
+    public static Test suite() {
+	TestSuite suite = new TestSuite(
+			"Test for org.apache.xml.security.test.signature");
+	//$JUnit-BEGIN$
+	suite.addTest(CreateSignatureTest.suite());
+	suite.addTestSuite(X509DataTest.class);
+	suite.addTest(XMLSignatureInputTest.suite());
+	suite.addTest(UnknownAlgoSignatureTest.suite());
+	suite.addTest(KeyValueTest.suite());
+	//$JUnit-END$
+	return suite;
+    }
 }
