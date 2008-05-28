dd1dc09c4e3c56227ceb8173c219547bdd2d46aa
Matthew Jason Benson <mbenson@apache.org>
2008-05-28 20:18:35 +0000

    merge 656293; add FilteredGenerator
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/functor/trunk@661068 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/commons/functor/generator/FilteredGenerator.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.functor.generator;
+
+import org.apache.commons.functor.UnaryPredicate;
+import org.apache.commons.functor.UnaryProcedure;
+import org.apache.commons.functor.core.composite.ConditionalUnaryProcedure;
+
+/**
+ * Generator that filters another Generator by only passing through those elements
+ * that are matched by a specified UnaryPredicate.
+ *
+ * @version $Revision$ $Date$
+ */
+public class FilteredGenerator extends BaseGenerator {
+    private UnaryPredicate pred;
+
+    /**
+     * Create a new FilteredGenerator.
+     * @param wrapped Generator to wrap
+     * @param pred filtering UnaryPredicate
+     */
+    public FilteredGenerator(Generator wrapped, UnaryPredicate pred) {
+        super(wrapped);
+        if (wrapped == null) {
+            throw new IllegalArgumentException("Generator argument was null");
+        }
+        if (pred == null) {
+            throw new IllegalArgumentException("UnaryPredicate argument was null");
+        }
+        this.pred = pred;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void run(UnaryProcedure proc) {
+        getWrappedGenerator().run(new ConditionalUnaryProcedure(pred, proc));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj instanceof FilteredGenerator == false) {
+            return false;
+        }
+        FilteredGenerator other = (FilteredGenerator) obj;
+        return other.getWrappedGenerator().equals(getWrappedGenerator()) && other.pred.equals(pred);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int hashCode() {
+        int result = "FilteredGenerator".hashCode();
+        result <<= 2;
+        Generator gen = getWrappedGenerator();
+        result ^= gen == null ? 0 : gen.hashCode();
+        result <<= 2;
+        result ^= pred == null ? 0 : pred.hashCode();
+        return result;
+    }
+}
