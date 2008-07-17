83ca30853115afaf5bab45880a94a704d8282234
Cedric Brandes <cedric.brandes+github@gmail.com>
2008-07-17 07:17:22 +0000

    Switch to interface

---
+++ src/net/webassembletool/taglib/IReplaceableTag.java
@@ -0,0 +1,16 @@
+package net.webassembletool.taglib;
+
+import java.util.Map;
+
+/**
+ * Interface
+ * 
+ * TODO: Javadoc
+ * 
+ * @author Cedric Brandes
+ * 
+ */
+public interface IReplaceableTag {
+
+    public abstract Map<String, String> getReplaceRules();
+}
