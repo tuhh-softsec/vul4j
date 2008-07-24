3d3c4627bc5b6470d409947b9855e6d01aad65fc
bentmann <bentmann@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-07-24 08:28:18 +0000

    o Added unit test for CommandLineUtilsTest.translateCommandline()

--- src/test/java/org/codehaus/plexus/util/cli/CommandLineUtilsTest.java
+++ src/test/java/org/codehaus/plexus/util/cli/CommandLineUtilsTest.java
@@ -1,5 +1,6 @@
 package org.codehaus.plexus.util.cli;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Locale;
 import java.util.Properties;
@@ -70,6 +71,34 @@ public class CommandLineUtilsTest
             String variable = (String) it.next();
             assertEquals( variable.toUpperCase( Locale.ENGLISH ), variable );
         }
+    }
+
+    /**
+     * Tests the splitting of a command line into distinct arguments.
+     */
+    public void testTranslateCommandline()
+        throws Exception
+    {
+        assertCmdLineArgs( new String[] {}, null );
+        assertCmdLineArgs( new String[] {}, "" );
+
+        assertCmdLineArgs( new String[] { "foo", "bar" }, "foo bar" );
+        assertCmdLineArgs( new String[] { "foo", "bar" }, "   foo   bar   " );
+
+        assertCmdLineArgs( new String[] { "foo", " double quotes ", "bar" }, "foo \" double quotes \" bar" );
+        assertCmdLineArgs( new String[] { "foo", " single quotes ", "bar" }, "foo ' single quotes ' bar" );
+
+        assertCmdLineArgs( new String[] { "foo", " \" ", "bar" }, "foo ' \" ' bar" );
+        assertCmdLineArgs( new String[] { "foo", " ' ", "bar" }, "foo \" ' \" bar" );
+    }
+
+    private void assertCmdLineArgs( String[] expected, String cmdLine )
+        throws Exception
+    {
+        String[] actual = CommandLineUtils.translateCommandline( cmdLine );
+        assertNotNull( actual );
+        assertEquals( expected.length, actual.length );
+        assertEquals( Arrays.asList( expected ), Arrays.asList( actual ) );
     }
 
 }
