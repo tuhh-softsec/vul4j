8f5930a598e7ad722fa3022b73177a527ad53e74
Daphne Bellemin <daphne.bellemin@gmail.com>
2008-07-25 08:25:23 +0000

    Added HttpState in order to keep session

--- src/net/webassembletool/Context.java
+++ src/net/webassembletool/Context.java
@@ -4,19 +4,22 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+import org.apache.commons.httpclient.HttpState;
+
 /**
  * User context that can be used in the master application to define the user id
  * and the Locale. This context will be transmitted to the provider
  * applications.<br />
  * There is one instance of user context associated with each Thread.
  * 
- * @author François-Xavier Bonnet
+ * @author Franï¿½ois-Xavier Bonnet
  * 
  */
 public class Context {
     private final static String USER_PARAM_KEY = "user";
     private final static String LOCALE_PARAM_KEY = "locale";
     private Map<String, String> parameterMap = new TreeMap<String, String>();
+    private HttpState httpState;
 
     public Context() {
 	// Nothing to do
@@ -44,6 +47,14 @@ public class Context {
 
     public void setUser(String user) {
 	parameterMap.put(USER_PARAM_KEY, user);
+    }
+   
+    public HttpState getHttpState() {
+        return httpState;
+    }
+
+    public void setHttpState(HttpState httpState) {
+        this.httpState = httpState;
     }
 
     public Object getParameter(String key) {
