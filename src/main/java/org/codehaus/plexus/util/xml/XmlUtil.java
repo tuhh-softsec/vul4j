eecc7b8c0efac324eeb158375491ab43d26813b8
vsiveton <vsiveton@30dfa635-7bf8-0310-9608-e41f2d776689>
2008-10-29 23:18:30 +0000

    PLXUTILS-97:  Add new prettyFormat( Reader reader, Writer writer ) in XmlUtil
    o first add

--- src/main/java/org/codehaus/plexus/util/xml/XmlUtil.java
+++ src/main/java/org/codehaus/plexus/util/xml/XmlUtil.java
@@ -22,12 +22,20 @@ package org.codehaus.plexus.util.xml;
  */
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.Writer;
 
 import org.codehaus.plexus.util.IOUtil;
 import org.codehaus.plexus.util.ReaderFactory;
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.util.WriterFactory;
 import org.codehaus.plexus.util.xml.pull.MXParser;
 import org.codehaus.plexus.util.xml.pull.XmlPullParser;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 /**
  * Common XML utilities methods.
@@ -38,6 +46,12 @@ import org.codehaus.plexus.util.xml.pull.XmlPullParser;
  */
 public class XmlUtil
 {
+    /** The default line indenter size i.e. 2. */
+    public static final int DEFAULT_INDENTATION_SIZE = 2;
+
+    /** The default line separator ("\n" on UNIX) */
+    public static final String DEFAULT_LINE_SEPARATOR = System.getProperty( "line.separator" );
+
     /**
      * Determines if a given File shall be handled as XML.
      *
@@ -73,6 +87,256 @@ public class XmlUtil
         finally
         {
             IOUtil.close( reader );
+        }
+    }
+
+    /**
+     * Pretty format the input reader. For instance, the following input:
+     * <pre>
+     * &lt;div&gt;&lt;b&gt;content&lt;/b&gt;&lt;/div&gt;
+     * </pre>
+     * becomes
+     * <pre>
+     * &lt;div&gt;
+     *   &lt;b&gt;content&lt;/b&gt;
+     * &lt;/div&gt;
+     * </pre>
+     *
+     * @param reader not null
+     * @param writer not null
+     * @throws IOException if any or invalid xml content
+     * @see #prettyFormat(Reader, Writer, int, String)
+     * @see ReaderFactory to read an xml content
+     * @see WriterFactory to write an xml content
+     */
+    public static void prettyFormat( Reader reader, Writer writer )
+        throws IOException
+    {
+        prettyFormat( reader, writer, DEFAULT_INDENTATION_SIZE, DEFAULT_LINE_SEPARATOR );
+    }
+
+    /**
+     * Pretty format the input reader. For instance, the following input:
+     * <pre>
+     * &lt;div&gt;&lt;b&gt;content&lt;/b&gt;&lt;/div&gt;
+     * </pre>
+     * becomes
+     * <pre>
+     * &lt;div&gt;
+     *   &lt;b&gt;content&lt;/b&gt;
+     * &lt;/div&gt;
+     * </pre>
+     *
+     * @param reader not null
+     * @param writer not null
+     * @param indentSize positive number for the indentation
+     * @param lineSeparator the wanted line separator
+     * @throws IOException if any or invalid xml content
+     * @see ReaderFactory to read an xml content
+     * @see WriterFactory to write an xml content
+     */
+    public static void prettyFormat( Reader reader, Writer writer, int indentSize, String lineSeparator )
+        throws IOException
+    {
+        if ( reader == null )
+        {
+            throw new IllegalArgumentException( "The reader is null" );
+        }
+        if ( writer == null )
+        {
+            throw new IllegalArgumentException( "The writer is null" );
+        }
+        if ( indentSize < 0 )
+        {
+            indentSize = 0;
+        }
+
+        PrettyPrintXMLWriter xmlWriter = new PrettyPrintXMLWriter( writer );
+        xmlWriter.setLineIndenter( StringUtils.repeat( " ", indentSize ) );
+        xmlWriter.setLineSeparator( lineSeparator );
+
+        XmlPullParser parser = new MXParser();
+        try
+        {
+            parser.setInput( reader );
+
+            prettyFormatInternal( parser, xmlWriter );
+        }
+        catch ( XmlPullParserException e )
+        {
+            throw new IOException( "Unable to parse the XML: " + e.getMessage() );
+        }
+    }
+
+    /**
+     * Pretty format the input stream. For instance, the following input:
+     * <pre>
+     * &lt;div&gt;&lt;b&gt;content&lt;/b&gt;&lt;/div&gt;
+     * </pre>
+     * becomes
+     * <pre>
+     * &lt;div&gt;
+     *   &lt;b&gt;content&lt;/b&gt;
+     * &lt;/div&gt;
+     * </pre>
+     *
+     * @param is not null
+     * @param os not null
+     * @throws IOException if any or invalid xml content
+     * @see #prettyFormat(InputStream, OutputStream, int, String)
+     */
+    public static void prettyFormat( InputStream is, OutputStream os )
+        throws IOException
+    {
+        prettyFormat( is, os, DEFAULT_INDENTATION_SIZE, DEFAULT_LINE_SEPARATOR );
+    }
+
+    /**
+     * Pretty format the input stream. For instance, the following input:
+     * <pre>
+     * &lt;div&gt;&lt;b&gt;content&lt;/b&gt;&lt;/div&gt;
+     * </pre>
+     * becomes
+     * <pre>
+     * &lt;div&gt;
+     *   &lt;b&gt;content&lt;/b&gt;
+     * &lt;/div&gt;
+     * </pre>
+     *
+     * @param is not null
+     * @param os not null
+     * @param indentSize positive number for the indentation
+     * @param lineSeparator the wanted line separator
+     * @throws IOException if any or invalid xml content
+     */
+    public static void prettyFormat( InputStream is, OutputStream os, int indentSize, String lineSeparator )
+        throws IOException
+    {
+        if ( is == null )
+        {
+            throw new IllegalArgumentException( "The is is null" );
+        }
+        if ( os == null )
+        {
+            throw new IllegalArgumentException( "The os is null" );
+        }
+        if ( indentSize < 0 )
+        {
+            indentSize = 0;
+        }
+
+        Reader reader = null;
+
+        Writer out = new OutputStreamWriter( os );
+        PrettyPrintXMLWriter xmlWriter = new PrettyPrintXMLWriter( out );
+        xmlWriter.setLineIndenter( StringUtils.repeat( " ", indentSize ) );
+        xmlWriter.setLineSeparator( lineSeparator );
+
+        XmlPullParser parser = new MXParser();
+        try
+        {
+            reader = ReaderFactory.newXmlReader( is );
+
+            parser.setInput( reader );
+
+            prettyFormatInternal( parser, xmlWriter );
+        }
+        catch ( XmlPullParserException e )
+        {
+            throw new IOException( "Unable to parse the XML: " + e.getMessage() );
+        }
+        finally
+        {
+            IOUtil.close( reader );
+            IOUtil.close( out );
+        }
+    }
+
+    /**
+     * @param parser not null
+     * @param writer not null
+     * @throws XmlPullParserException if any
+     * @throws IOException if any
+     */
+    private static void prettyFormatInternal( XmlPullParser parser, PrettyPrintXMLWriter writer )
+        throws XmlPullParserException, IOException
+    {
+        boolean hasTag = false;
+        boolean hasComment = false;
+        int eventType = parser.getEventType();
+        while ( eventType != XmlPullParser.END_DOCUMENT )
+        {
+            if ( eventType == XmlPullParser.START_TAG )
+            {
+                hasTag = true;
+                if ( hasComment )
+                {
+                    writer.writeText( writer.getLineIndenter() );
+                    hasComment = false;
+                }
+                writer.startElement( parser.getName() );
+                for ( int i = 0; i < parser.getAttributeCount(); i++ )
+                {
+                    String key = parser.getAttributeName( i );
+                    String value = parser.getAttributeValue( i );
+                    writer.addAttribute( key, value );
+                }
+            }
+            else if ( eventType == XmlPullParser.TEXT )
+            {
+                String text = parser.getText();
+                if ( !text.trim().equals( "" ) )
+                {
+                    text = StringUtils.removeDuplicateWhitespace( text );
+                    writer.writeText( text );
+                }
+            }
+            else if ( eventType == XmlPullParser.END_TAG )
+            {
+                hasTag = false;
+                writer.endElement();
+            }
+            else if ( eventType == XmlPullParser.COMMENT )
+            {
+                hasComment = true;
+                if ( !hasTag )
+                {
+                    writer.writeMarkup( writer.getLineSeparator() );
+                    for ( int i = 0; i < writer.getDepth(); i++ )
+                    {
+                        writer.writeMarkup( writer.getLineIndenter() );
+                    }
+                }
+                writer.writeMarkup( "<!--" + parser.getText().trim() + " -->" );
+                if ( !hasTag )
+                {
+                    writer.writeMarkup( writer.getLineSeparator() );
+                    for ( int i = 0; i < writer.getDepth() - 1; i++ )
+                    {
+                        writer.writeMarkup( writer.getLineIndenter() );
+                    }
+                }
+            }
+            else if ( eventType == XmlPullParser.DOCDECL )
+            {
+                writer.writeMarkup( "<!DOCTYPE" + parser.getText() + ">" );
+                writer.endOfLine();
+            }
+            else if ( eventType == XmlPullParser.PROCESSING_INSTRUCTION )
+            {
+                writer.writeMarkup( "<?" + parser.getText() + "?>" );
+                writer.endOfLine();
+            }
+            else if ( eventType == XmlPullParser.CDSECT )
+            {
+                writer.writeMarkup( "<![CDATA[" + parser.getText() + "]]>" );
+            }
+            else if ( eventType == XmlPullParser.ENTITY_REF )
+            {
+                writer.writeMarkup( "&" + parser.getName() + ";" );
+            }
+
+            eventType = parser.nextToken();
         }
     }
 }
---
+++ src/test/java/org/codehaus/plexus/util/xml/XmlUtilTest.java
@@ -0,0 +1,169 @@
+package org.codehaus.plexus.util.xml;
+
+/*
+ * Copyright 2008 The Codehaus Foundation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import junit.framework.TestCase;
+
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.ReaderFactory;
+import org.codehaus.plexus.util.StringUtils;
+import org.codehaus.plexus.util.WriterFactory;
+
+/**
+ * Test the {@link XmlUtil} class.
+ *
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
+ */
+public class XmlUtilTest
+    extends TestCase
+{
+    private static final File basedir = new File( new File( "" ).getAbsolutePath() );
+
+    /** {@inheritDoc} */
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+    }
+
+    /** {@inheritDoc} */
+    protected void tearDown()
+        throws Exception
+    {
+        super.tearDown();
+    }
+
+    public void testPrettyFormatInputStreamOutputStream()
+        throws Exception
+    {
+        File testDocument = new File( basedir, "src/test/resources/testDocument.xhtml" );
+        assertTrue( testDocument.exists() );
+
+        InputStream is = null;
+        OutputStream os = null;
+        try
+        {
+            is = new FileInputStream( testDocument );
+            os = new FileOutputStream( new File( basedir, "target/test/prettyFormatTestDocumentOutputStream.xml" ) );
+
+            assertNotNull( is );
+            assertNotNull( os );
+
+            XmlUtil.prettyFormat( is, os );
+        }
+        finally
+        {
+            IOUtil.close( is );
+            IOUtil.close( os );
+        }
+    }
+
+    public void testPrettyFormatReaderWriter()
+        throws Exception
+    {
+        File testDocument = new File( basedir, "src/test/resources/testDocument.xhtml" );
+        assertTrue( testDocument.exists() );
+
+        Reader reader = null;
+        Writer writer = null;
+        try
+        {
+            reader = ReaderFactory.newXmlReader( testDocument );
+            writer = WriterFactory.newXmlWriter( new File( basedir, "target/test/prettyFormatTestDocumentWriter.xml" ) );
+
+            assertNotNull( reader );
+            assertNotNull( writer );
+
+            XmlUtil.prettyFormat( reader, writer );
+        }
+        finally
+        {
+            IOUtil.close( reader );
+            IOUtil.close( writer );
+        }
+    }
+
+    public void testPrettyFormatString()
+        throws Exception
+    {
+        File testDocument = new File( basedir, "src/test/resources/testDocument.xhtml" );
+        assertTrue( testDocument.exists() );
+
+        Reader reader = null;
+        Writer writer = null;
+        String content;
+        try
+        {
+            reader = ReaderFactory.newXmlReader( testDocument );
+            content = IOUtil.toString( reader );
+
+            reader = ReaderFactory.newXmlReader( testDocument );
+            writer = new StringWriter();
+            XmlUtil.prettyFormat( reader, writer );
+        }
+        finally
+        {
+            IOUtil.close( reader );
+            IOUtil.close( writer );
+        }
+
+        assertNotNull( content );
+
+        int countEOL = StringUtils.countMatches( content, XmlUtil.DEFAULT_LINE_SEPARATOR );
+        assertTrue( countEOL < StringUtils.countMatches( writer.toString(), XmlUtil.DEFAULT_LINE_SEPARATOR ) );
+    }
+
+    public void testPrettyFormatReaderWriter2()
+        throws Exception
+    {
+        File testDocument = new File( basedir, "src/test/resources/test.xdoc.xhtml" );
+        assertTrue( testDocument.exists() );
+
+        Reader reader = null;
+        Writer writer = null;
+        try
+        {
+            reader = ReaderFactory.newXmlReader( testDocument );
+            writer = WriterFactory.newXmlWriter( new File( basedir, "target/test/prettyFormatTestXdocWriter.xml" ) );
+
+            assertNotNull( reader );
+            assertNotNull( writer );
+
+            XmlUtil.prettyFormat( reader, writer );
+        }
+        finally
+        {
+            IOUtil.close( reader );
+            IOUtil.close( writer );
+        }
+    }
+}
