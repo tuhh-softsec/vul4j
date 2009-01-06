6fac12db7813a1de5e0468bc22b1a97d1528c95b
vsiveton <vsiveton@30dfa635-7bf8-0310-9608-e41f2d776689>
2009-01-06 23:52:43 +0000

    PLXUTILS-37: Ability to pick items from a list in a property declaration.
    o added support of indexed and mapped properties
    o updated documentation
    o added test case

--- src/main/java/org/codehaus/plexus/util/introspection/ReflectionValueExtractor.java
+++ src/main/java/org/codehaus/plexus/util/introspection/ReflectionValueExtractor.java
@@ -16,26 +16,34 @@ package org.codehaus.plexus.util.introspection;
  * limitations under the License.
  */
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import java.util.WeakHashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.codehaus.plexus.util.StringUtils;
 
 /**
- * Using simple dotted expressions extract the values from a MavenProject
- * instance, For example we might want to extract a value like:
- * project.build.sourceDirectory
+ * <p>Using simple dotted expressions to extract the values from an Object instance,
+ * For example we might want to extract a value like: <code>project.build.sourceDirectory</code></p>
+ *
+ * <p>The implementation supports indexed, nested and mapped properties similar to the JSP way.</p>
  *
  * @author <a href="mailto:jason@maven.org">Jason van Zyl </a>
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
  * @version $Id$
+ * @see <a href="http://struts.apache.org/1.x/struts-taglib/indexedprops.html">http://struts.apache.org/1.x/struts-taglib/indexedprops.html</a>
  */
 public class ReflectionValueExtractor
 {
-    private static final Class[] CLASS_ARGS = new Class[ 0 ];
+    private static final Class[] CLASS_ARGS = new Class[0];
 
-    private static final Object[] OBJECT_ARGS = new Object[ 0 ];
+    private static final Object[] OBJECT_ARGS = new Object[0];
 
     /**
      * Use a WeakHashMap here, so the keys (Class objects) can be garbage collected.
@@ -44,16 +52,56 @@ public class ReflectionValueExtractor
      */
     private static final Map classMaps = new WeakHashMap();
 
+    /**
+     * Indexed properties pattern, ie <code>(\\w+)\\[(\\d+)\\]</code>
+     */
+    private static final Pattern INDEXED_PROPS = Pattern.compile( "(\\w+)\\[(\\d+)\\]" );
+
+    /**
+     * Indexed properties pattern, ie <code>(\\w+)\\((.+)\\)</code>
+     */
+    private static final Pattern MAPPED_PROPS = Pattern.compile( "(\\w+)\\((.+)\\)" );
+
     private ReflectionValueExtractor()
     {
     }
-    
+
+    /**
+     * <p>The implementation supports indexed, nested and mapped properties.</p>
+     *
+     * <ul>
+     * <li>nested properties should be defined by a dot, i.e. "user.address.street"</li>
+     * <li>indexed properties (java.util.List or array instance) should be contains <code>(\\w+)\\[(\\d+)\\]</code>
+     * pattern, i.e. "user.addresses[1].street"</li>
+     * <li>mapped properties should be contains <code>(\\w+)\\((.+)\\)</code> pattern, i.e. "user.addresses(myAddress).street"</li>
+     * <ul>
+     *
+     * @param expression not null expression
+     * @param root not null object
+     * @return the object defined by the expression
+     * @throws Exception if any
+     */
     public static Object evaluate( String expression, Object root )
         throws Exception
     {
         return evaluate( expression, root, true );
     }
 
+    /**
+     * <p>The implementation supports indexed, nested and mapped properties.</p>
+     *
+     * <ul>
+     * <li>nested properties should be defined by a dot, i.e. "user.address.street"</li>
+     * <li>indexed properties (java.util.List or array instance) should be contains <code>(\\w+)\\[(\\d+)\\]</code>
+     * pattern, i.e. "user.addresses[1].street"</li>
+     * <li>mapped properties should be contains <code>(\\w+)\\((.+)\\)</code> pattern, i.e. "user.addresses(myAddress).street"</li>
+     * <ul>
+     *
+     * @param expression not null expression
+     * @param root not null object
+     * @return the object defined by the expression
+     * @throws Exception if any
+     */
     // TODO: don't throw Exception
     public static Object evaluate( String expression, Object root, boolean trimRootToken )
         throws Exception
@@ -75,27 +123,89 @@ public class ReflectionValueExtractor
 
         while ( parser.hasMoreTokens() )
         {
-            String token = parser.nextToken();
-
+            // if we have nothing, stop now
             if ( value == null )
             {
                 return null;
             }
 
+            String token = parser.nextToken();
+
             ClassMap classMap = getClassMap( value.getClass() );
 
-            String methodBase = StringUtils.capitalizeFirstLetter( token );
-            
-            String methodName = "get" + methodBase;
+            Method method;
+            Object[] localParams = OBJECT_ARGS;
 
-            Method method = classMap.findMethod( methodName, CLASS_ARGS );
-            
-            if ( method == null )
+            // do we have an indexed property?
+            Matcher matcher = INDEXED_PROPS.matcher( token );
+            if ( matcher.find() )
             {
-                // perhaps this is a boolean property??
-                methodName = "is" + methodBase;
-                
+                String methodBase = StringUtils.capitalizeFirstLetter( matcher.group( 1 ) );
+                String methodName = "get" + methodBase;
                 method = classMap.findMethod( methodName, CLASS_ARGS );
+                value = method.invoke( value, OBJECT_ARGS );
+                classMap = getClassMap( value.getClass() );
+
+                if ( classMap.getCachedClass().isArray() )
+                {
+                    value = Arrays.asList( (Object[]) value );
+                    classMap = getClassMap( value.getClass() );
+                }
+
+                if ( value instanceof List )
+                {
+                    // use get method on List interface
+                    localParams = new Object[1];
+                    localParams[0] = Integer.valueOf( matcher.group( 2 ) );
+                    method = classMap.findMethod( "get", localParams );
+                }
+                else
+                {
+                    throw new Exception( "The token '" + token
+                        + "' refers to a java.util.List or an array, but the value seems is an instance of '"
+                        + value.getClass() + "'." );
+                }
+            }
+            else
+            {
+                // do we have a mapped property?
+                matcher = MAPPED_PROPS.matcher( token );
+                if ( matcher.find() )
+                {
+                    String methodBase = StringUtils.capitalizeFirstLetter( matcher.group( 1 ) );
+                    String methodName = "get" + methodBase;
+                    method = classMap.findMethod( methodName, CLASS_ARGS );
+                    value = method.invoke( value, OBJECT_ARGS );
+                    classMap = getClassMap( value.getClass() );
+
+                    if ( value instanceof Map )
+                    {
+                        // use get method on List interface
+                        localParams = new Object[1];
+                        localParams[0] = matcher.group( 2 );
+                        method = classMap.findMethod( "get", localParams );
+                    }
+                    else
+                    {
+                        throw new Exception( "The token '" + token
+                            + "' refers to a java.util.Map, but the value seems is an instance of '"
+                            + value.getClass() + "'." );
+                    }
+                }
+                else
+                {
+                    String methodBase = StringUtils.capitalizeFirstLetter( token );
+                    String methodName = "get" + methodBase;
+                    method = classMap.findMethod( methodName, CLASS_ARGS );
+
+                    if ( method == null )
+                    {
+                        // perhaps this is a boolean property??
+                        methodName = "is" + methodBase;
+
+                        method = classMap.findMethod( methodName, CLASS_ARGS );
+                    }
+                }
             }
 
             if ( method == null )
@@ -103,7 +213,20 @@ public class ReflectionValueExtractor
                 return null;
             }
 
-            value = method.invoke( value, OBJECT_ARGS );
+            try
+            {
+                value = method.invoke( value, localParams );
+            }
+            catch ( InvocationTargetException e )
+            {
+                // catch array index issues gracefully, otherwise release
+                if ( e.getCause() instanceof IndexOutOfBoundsException )
+                {
+                    return null;
+                }
+
+                throw e;
+            }
         }
 
         return value;
--- src/test/java/org/codehaus/plexus/util/introspection/ReflectionValueExtractorTest.java
+++ src/test/java/org/codehaus/plexus/util/introspection/ReflectionValueExtractorTest.java
@@ -19,8 +19,11 @@ package org.codehaus.plexus.util.introspection;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
@@ -36,6 +39,11 @@ public class ReflectionValueExtractorTest
     {
         super.setUp();
 
+        Dependency dependency1 = new Dependency();
+        dependency1.setArtifactId("dep1");
+        Dependency dependency2 = new Dependency();
+        dependency2.setArtifactId("dep2");
+
         project = new Project();
         project.setModelVersion( "4.0.0" );
         project.setGroupId( "org.apache.maven" );
@@ -44,8 +52,8 @@ public class ReflectionValueExtractorTest
         project.setVersion( "2.0-SNAPSHOT" );
         project.setScm( new Scm() );
         project.getScm().setConnection( "scm-connection" );
-        project.addDependency( new Dependency() );
-        project.addDependency( new Dependency() );
+        project.addDependency( dependency1 );
+        project.addDependency( dependency2 );
         project.setBuild( new Build() );
     }
 
@@ -83,6 +91,45 @@ public class ReflectionValueExtractorTest
         Assert.assertEquals( 2, dependencies.size() );
 
         // ----------------------------------------------------------------------
+        // Dependencies - using index notation
+        // ----------------------------------------------------------------------
+
+        // List
+        Dependency dependency = (Dependency)ReflectionValueExtractor.evaluate( "project.dependencies[0]", project );
+
+        Assert.assertNotNull( dependency );
+
+        Assert.assertTrue( "dep1".equals(dependency.getArtifactId()) );
+
+        String artifactId = (String)ReflectionValueExtractor.evaluate( "project.dependencies[1].artifactId", project );
+
+        Assert.assertTrue( "dep2".equals(artifactId) );
+
+        // Array
+
+        dependency = (Dependency)ReflectionValueExtractor.evaluate( "project.dependenciesAsArray[0]", project );
+
+        Assert.assertNotNull( dependency );
+
+        Assert.assertTrue( "dep1".equals(dependency.getArtifactId()) );
+
+        artifactId = (String)ReflectionValueExtractor.evaluate( "project.dependenciesAsArray[1].artifactId", project );
+
+        Assert.assertTrue( "dep2".equals(artifactId) );
+
+        // Map
+
+        dependency = (Dependency)ReflectionValueExtractor.evaluate( "project.dependenciesAsMap(dep1)", project );
+
+        Assert.assertNotNull( dependency );
+
+        Assert.assertTrue( "dep1".equals(dependency.getArtifactId()) );
+
+        artifactId = (String)ReflectionValueExtractor.evaluate( "project.dependenciesAsMap(dep2).artifactId", project );
+
+        Assert.assertTrue( "dep2".equals(artifactId) );
+
+        // ----------------------------------------------------------------------
         // Build
         // ----------------------------------------------------------------------
 
@@ -95,6 +142,8 @@ public class ReflectionValueExtractorTest
         throws Exception
     {
         Assert.assertNull( ReflectionValueExtractor.evaluate( "project.foo", project ) );
+        Assert.assertNull( ReflectionValueExtractor.evaluate( "project.dependencies[10]", project ) );
+        Assert.assertNull( ReflectionValueExtractor.evaluate( "project.dependencies[0].foo", project ) );
     }
 
     public static class Project
@@ -194,6 +243,22 @@ public class ReflectionValueExtractorTest
         {
             return version;
         }
+
+        public Dependency[] getDependenciesAsArray()
+        {
+            return (Dependency[]) getDependencies().toArray( new Dependency[0]);
+        }
+
+        public Map getDependenciesAsMap()
+        {
+            Map ret = new HashMap();
+            for ( Iterator it = getDependencies().iterator(); it.hasNext();)
+            {
+                Dependency dep = (Dependency)it.next();
+                ret.put( dep.getArtifactId(), dep );
+            }
+            return ret;
+        }
     }
 
     public static class Build
@@ -203,7 +268,17 @@ public class ReflectionValueExtractorTest
 
     public static class Dependency
     {
+        private String artifactId;
 
+        public String getArtifactId()
+        {
+            return artifactId;
+        }
+
+        public void setArtifactId(String id)
+        {
+            artifactId = id;
+        }
     }
 
     public static class Scm
