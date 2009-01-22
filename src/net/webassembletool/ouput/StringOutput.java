b2d48865a6ef2d0d2fb7596907ca621d50fc7fb8
Stanislav Bernatsky <sbernatsky@ukr.net>
2009-01-22 11:35:26 +0000

    code clean up: use 'foreach'-style iteration

--- src/net/webassembletool/ouput/StringOutput.java
+++ src/net/webassembletool/ouput/StringOutput.java
@@ -4,8 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -52,12 +51,12 @@ public class StringOutput extends Output {
     }
 
     public void copyHeaders(HttpServletResponse response) {
-	for (Iterator<Map.Entry<Object, Object>> headersIterator = getHeaders()
-		.entrySet().iterator(); headersIterator.hasNext();) {
-	    Map.Entry<Object, Object> entry = headersIterator.next();
-	    if (!"content-length".equalsIgnoreCase(entry.getKey().toString()))
-		response.addHeader(entry.getKey().toString(), entry.getValue()
-			.toString());
+	for (Entry<?, ?> entry : getHeaders().entrySet()) {
+	    String key = entry.getKey().toString();
+	    String value = entry.getValue().toString();
+	    if (!"content-length".equalsIgnoreCase(key)) {
+		response.addHeader(key, value);
+	    }
 	}
     }
 
