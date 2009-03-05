dff48996d87995770b1d81bde8c16bddef83f6c3
Francois-Xavier <francois-xavier.bonnet@centraliens.net>
2009-03-05 18:53:54 +0000

    Unused import

--- src/test/java/net/webassembletool/test/junit/HttpTestCase.java
+++ src/test/java/net/webassembletool/test/junit/HttpTestCase.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
