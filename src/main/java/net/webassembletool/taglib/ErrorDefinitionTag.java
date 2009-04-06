27bf4304fe9af2094e27f0bf3473f7a4a864c79a
Cedric Brandes <cedric.brandes+github@gmail.com>
2009-04-06 10:00:33 +0000

    New If tag

--- src/main/java/net/webassembletool/taglib/ErrorDefinitionTag.java
+++ src/main/java/net/webassembletool/taglib/ErrorDefinitionTag.java
@@ -2,16 +2,15 @@ package net.webassembletool.taglib;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
+import javax.servlet.jsp.tagext.Tag;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * 'errorDefinition' JSP tag.
  * <p>
- * Displays the body's content of retrieved resource instead of the error
- * message in case when error happens while retrieving resource and error code
- * matches one specified by tag.
+ * Displays the body's content of retrieved resource instead of the error message in case when error happens while retrieving resource and error code matches one specified by tag.
  * <p>
- * <b>Note</b>: This tag can only be used inside a includeBlock or
- * includeTemplate tag
+ * <b>Note</b>: This tag can only be used inside a includeBlock or includeTemplate tag
  * <p>
  * Parameters:
  * <ul>
@@ -21,9 +20,7 @@ import javax.servlet.jsp.tagext.BodyTagSupport;
  * @author Cedric BRANDES
  */
 public class ErrorDefinitionTag extends BodyTagSupport {
-
     private static final long serialVersionUID = -4466967947717753242L;
-
     private Integer errorCode;
 
     public Integer getErrorCode() {
@@ -36,9 +33,22 @@ public class ErrorDefinitionTag extends BodyTagSupport {
 
     @Override
     public int doAfterBody() throws JspException {
-        ErrorManageableTag parent = (ErrorManageableTag) getParent();
+        ErrorManageableTag parent = getFirstAvailableParent();
+        if (parent == null) {
+            LogFactory.getLog(ErrorDefinitionTag.class).warn("No parent found, skipping tag");
+            return Tag.SKIP_BODY;
+        }
         String value = getBodyContent().getString();
         parent.getErrorMap().put(errorCode, value);
-        return SKIP_BODY;
+        return Tag.SKIP_BODY;
+    }
+
+    private ErrorManageableTag getFirstAvailableParent() {
+        ErrorManageableTag parent = null;
+        if (getParent() instanceof ErrorManageableTag)
+            parent = (ErrorManageableTag) getParent();
+        else if (getParent() instanceof IfTag && getParent().getParent() instanceof ErrorManageableTag)
+            parent = (ErrorManageableTag) getParent().getParent();
+        return parent;
     }
 }
