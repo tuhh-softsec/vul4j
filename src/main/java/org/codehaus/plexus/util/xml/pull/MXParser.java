592b418373cc9cc68ebe1ae75923cb803776d87a
vsiveton <vsiveton@30dfa635-7bf8-0310-9608-e41f2d776689>
2009-05-28 23:51:31 +0000

    PLXUTILS-110:  Wronf entity replacement in MXParser
    o more check

--- src/main/java/org/codehaus/plexus/util/xml/pull/MXParser.java
+++ src/main/java/org/codehaus/plexus/util/xml/pull/MXParser.java
@@ -562,9 +562,9 @@ public class MXParser
     {
         //      throw new XmlPullParserException("not allowed");
 
-        if ( !replacementText.startsWith( "&#" ) && this.entityName != null )
+        if ( !replacementText.startsWith( "&#" ) && this.entityName != null && replacementText.length() > 1 )
         {
-            String tmp = new String( replacementText ).substring( 1, replacementText.length() - 1 );
+            String tmp = replacementText.substring( 1, replacementText.length() - 1 );
             for ( int i = 0; i < this.entityName.length; i++ )
             {
                 if ( this.entityName[i] != null && this.entityName[i].equals( tmp ) )
