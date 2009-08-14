a286b91aa9ff358b1d6a46a9ad99ca397c4c7de6
Nicolas Richeton <nicolas.richeton@gmail.com>
2009-08-14 11:45:34 +0000

    Test case for bug 2837564

---
+++ webassembletool-core/src/test/java/net/webassembletool/cache/MemoryOutputTest.java
@@ -0,0 +1,43 @@
+package net.webassembletool.cache;
+
+import junit.framework.TestCase;
+
+public class MemoryOutputTest extends TestCase {
+
+	final static String CONTENT = "My content";
+	final static String CHARSET = "UTF-8";
+
+	/**
+	 * Ensure 'cacheMaxFileSize' from configuration works as expected.
+	 * @see  https://sourceforge.net/tracker/?func=detail&aid=2837564&group_id=209844&atid=1011350
+	 */
+	public void testCacheMaxFileSize() {
+		
+		// Size > Content : content is written
+		MemoryOutput memoryOutput = new MemoryOutput(500);
+		memoryOutput.setCharsetName(CHARSET);
+		memoryOutput.open();
+		memoryOutput.write(CONTENT);
+		memoryOutput.close();
+		assertFalse(memoryOutput.toResource().isEmpty());
+
+		// Size < Content : content is NOT written
+		memoryOutput = new MemoryOutput(5);
+		memoryOutput.setCharsetName(CHARSET);
+		memoryOutput.open();
+		memoryOutput.write(CONTENT);
+		memoryOutput.close();
+		assertTrue(memoryOutput.toResource().isEmpty());
+
+		// Size =0 (means no limit) : content is written
+		// see http://webassembletool.sourceforge.net/configuration.html
+		memoryOutput = new MemoryOutput(0);
+		memoryOutput.setCharsetName(CHARSET);
+		memoryOutput.open();
+		memoryOutput.write(CONTENT);
+		memoryOutput.close();
+		assertFalse(memoryOutput.toResource().isEmpty());
+
+	}
+
+}
