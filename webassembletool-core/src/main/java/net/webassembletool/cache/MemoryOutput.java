f1977b3642af0d1e4e97f0f2bc588c1c87426842
Nicolas Richeton <nicolas.richeton@gmail.com>
2009-08-14 11:55:48 +0000

    Fixed bug 2837564

--- webassembletool-core/src/main/java/net/webassembletool/cache/MemoryOutput.java
+++ webassembletool-core/src/main/java/net/webassembletool/cache/MemoryOutput.java
@@ -11,85 +11,116 @@ import net.webassembletool.output.OutputException;
  * Output implementation that stores the file and headers to a MemoryResource.
  * 
  * @author Francois-Xavier Bonnet
+ * @author nricheton
  * @see MemoryResource
  * 
  */
 public class MemoryOutput extends Output {
-    private final MemoryOutputStream out;
+	private final MemoryOutputStream out;
 
-    public MemoryOutput(int maxSize) {
-        out = new MemoryOutputStream(maxSize);
-    }
+	/**
+	 * Creates a MemoryOuput with the given size limit. If content exceeds
+	 * maxSize, the whole content is NOT written and the resource will remain
+	 * empty.
+	 * 
+	 * @param maxSize
+	 *            Value &gt;=0. 0 means 'no limit'.
+	 * 
+	 */
+	public MemoryOutput(int maxSize) {
+		out = new MemoryOutputStream(maxSize);
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void open() {
-        // nothing to do
-    }
+	/** {@inheritDoc} */
+	@Override
+	public void open() {
+		// nothing to do
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public OutputStream getOutputStream() {
-        return out;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public OutputStream getOutputStream() {
+		return out;
+	}
 
-    /** {@inheritDoc} */
-    @Override
-    public void close() {
-        try {
-            out.close();
-        } catch (IOException e) {
-            // should not happen
-            throw new OutputException(e);
-        }
-    }
+	/** {@inheritDoc} */
+	@Override
+	public void close() {
+		try {
+			out.close();
+		} catch (IOException e) {
+			// should not happen
+			throw new OutputException(e);
+		}
+	}
 
-    public MemoryResource toResource() {
-        MemoryResource result;
-        if (out.isTooBig()) {
-            result = new MemoryResource();
-        } else {
-            result = new MemoryResource(out.toByteArray(), getCharsetName(),
-                    getHeaders(), getStatusCode(), getStatusMessage());
-        }
-        out.clear();
-        return result;
-    }
+	public MemoryResource toResource() {
+		MemoryResource result;
+		if (out.isTooBig()) {
+			result = new MemoryResource();
+		} else {
+			result = new MemoryResource(out.toByteArray(), getCharsetName(),
+					getHeaders(), getStatusCode(), getStatusMessage());
+		}
+		out.clear();
+		return result;
+	}
 
-    public static final class MemoryOutputStream extends ByteArrayOutputStream {
-        private final int maxSize;
-        private boolean tooBig = false;
+	/**
+	 * Local implementation of an output stream for MemoryOuput.
+	 * 
+	 * @author Francois-Xavier Bonnet
+	 * @author nricheton
+	 * 
+	 */
+	public static final class MemoryOutputStream extends ByteArrayOutputStream {
+		private final int maxSize;
+		private boolean tooBig = false;
 
-        public MemoryOutputStream(int maxSize) {
-            this.maxSize = maxSize;
-        }
+		/**
+		 * 
+		 * Creates a MemoryOutputStream with the given size limit. If content
+		 * exceeds maxSize, the whole content is NOT written and the resource
+		 * will remain empty.
+		 * 
+		 * @param maxSize
+		 *            Value &gt;=0. 0 means 'no limit'.
+		 */
+		public MemoryOutputStream(int maxSize) {
+			this.maxSize = maxSize;
+		}
 
-        /** {@inheritDoc} */
-        @Override
-        public synchronized void write(int b) {
-            byte buf[] = new byte[] { (byte) b };
-            write(buf, 0, 1);
-        }
+		/** {@inheritDoc} */
+		@Override
+		public synchronized void write(int b) {
+			byte buf[] = new byte[] { (byte) b };
+			write(buf, 0, 1);
+		}
 
-        /** {@inheritDoc} */
-        @Override
-        public synchronized void write(byte[] b, int off, int len) {
-            if (!tooBig) {
-                if ((count + len) > maxSize) {
-                    tooBig = true;
-                } else {
-                    super.write(b, off, len);
-                }
-            }
-        }
+		/** {@inheritDoc} */
+		@Override
+		public synchronized void write(byte[] b, int off, int len) {
+			if (!tooBig) {
+				if (maxSize != 0 && (count + len) > maxSize) {
+					tooBig = true;
+				} else {
+					super.write(b, off, len);
+				}
+			}
+		}
 
-        public boolean isTooBig() {
-            return tooBig;
-        }
+		/**
+		 * Returns true if the content written did not reach maxSize.
+		 * 
+		 * @return true if content size exceeds maxSize
+		 */
+		public boolean isTooBig() {
+			return tooBig;
+		}
 
-        public void clear() {
-            count = 0;
-            buf = new byte[32];
-        }
-    }
+		public void clear() {
+			count = 0;
+			buf = new byte[32];
+		}
+	}
 }
