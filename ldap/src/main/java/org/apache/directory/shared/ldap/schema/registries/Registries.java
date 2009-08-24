c7bab82528010f20359c3f8b44f12014f49b608d
Emmanuel Lecharny <elecharny@apache.org>
2009-08-24 11:37:28 +0000

    First version of the movced Registries object (it still does not compile)
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/shared-schema@807157 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldap/src/main/java/org/apache/directory/shared/ldap/schema/registries/Registries.java
@@ -0,0 +1,251 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.shared.ldap.schema.registries;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.directory.server.schema.bootstrap.Schema;
+import org.apache.directory.server.schema.registries.SchemaLoader;
+import org.apache.directory.server.schema.registries.SchemaLoaderListener;
+
+
+/**
+ * Document this class.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$
+ */
+public class Registries
+{
+    /** The AttributeType registry */
+    private AttributeTypeRegistry attributeTypeRegistry;
+    
+    /** The ObjectClass registry */
+    private ObjectClassRegistry objectClassRegistry;
+
+
+    /** The LdapSyntax registry */
+    private ComparatorRegistry comparatorRegistry;
+
+    /** The DitContentRule registry */
+    private DITContentRuleRegistry ditContentRuleRegistry;
+
+    /** The DitStructureRule registry */
+    private DITStructureRuleRegistry ditStructureRuleRegistry;
+
+    /** The MatchingRule registry */
+    private MatchingRuleRegistry matchingRuleRegistry;
+
+    /** The MatchingRuleUse registry */
+    private MatchingRuleUseRegistry matchingRuleUseRegistry;
+
+    /** The NameForm registry */
+    private NameFormRegistry nameFormRegistry;
+
+    /** The Normalizer registry */
+    private NormalizerRegistry normalizerRegistry;
+
+    /** The OID registry */
+    private OidRegistry oidRegistry;
+
+    /** The SyntaxChecker registry */
+    private SyntaxCheckerRegistry syntaxCheckerRegistry;
+
+    /** The LdapSyntax registry */
+    private LdapSyntaxRegistry ldapSyntaxRegistry;
+    
+    //private Map<String,Schema> loadedByName = new HashMap<String, Schema>();
+    //private final SchemaLoader schemaLoader;
+    //private final String name;
+
+    //String getName();
+    
+    //Map<String, Schema> getLoadedSchemas();
+    
+    //void load( String schemaName ) throws Exception;
+    
+    //void load( String schemaName, Properties props ) throws Exception;
+
+    //void unload( String schemaName ) throws Exception;
+    
+    //SchemaLoader getSchemaLoader();
+    
+    public DefaultRegistries( String name, SchemaLoader schemaLoader, OidRegistry registry )
+    {
+        this.name = name;
+        this.schemaLoader = schemaLoader;
+        
+        this.schemaLoader.setListener( new SchemaLoaderListener() {
+            public void schemaLoaded( Schema schema )
+            {
+                loadedByName.put( schema.getSchemaName(), schema );
+            }
+        });
+        
+        oidRegistry = registry;
+        normalizerRegistry = new NormalizerRegistry();
+        comparatorRegistry = new ComparatorRegistry();
+        syntaxCheckerRegistry = new SyntaxCheckerRegistry();
+        ldapSyntaxRegistry = new LdapSyntaxRegistry();
+        matchingRuleRegistry = new MatchingRuleRegistry();
+        attributeTypeRegistry = new AttributeTypeRegistry( oidRegistry );
+        objectClassRegistry = new ObjectClassRegistry();
+        ditContentRuleRegistry = new DITContentRuleRegistry();
+        ditStructureRuleRegistry = new DITStructureRuleRegistry();
+        matchingRuleUseRegistry = new MatchingRuleUseRegistry();
+        nameFormRegistry = new NameFormRegistry();
+    }
+    
+    
+    /**
+     * @return The AttributeType registry
+     */
+    public AttributeTypeRegistry getAttributeTypeRegistry()
+    {
+        return attributeTypeRegistry;
+    }
+
+    
+    /**
+     * @return The Comparator registry
+     */
+    public ComparatorRegistry getComparatorRegistry()
+    {
+        return comparatorRegistry;
+    }
+
+    
+    /**
+     * @return The DITContentRule registry
+     */
+    public DITContentRuleRegistry getDitContentRuleRegistry()
+    {
+        return ditContentRuleRegistry;
+    }
+
+    
+    /**
+     * @return The DITStructureRule registry
+     */
+    public DITStructureRuleRegistry getDitStructureRuleRegistry()
+    {
+        return ditStructureRuleRegistry;
+    }
+
+    
+    /**
+     * @return The MatchingRule registry
+     */
+    public MatchingRuleRegistry getMatchingRuleRegistry()
+    {
+        return matchingRuleRegistry;
+    }
+
+    
+    /**
+     * @return The MatchingRuleUse registry
+     */
+    public MatchingRuleUseRegistry getMatchingRuleUseRegistry()
+    {
+        return matchingRuleUseRegistry;
+    }
+
+    
+    /**
+     * @return The NameForm registry
+     */
+    public NameFormRegistry getNameFormRegistry()
+    {
+        return nameFormRegistry;
+    }
+
+    
+    /**
+     * @return The Normalizer registry
+     */
+    public NormalizerRegistry getNormalizerRegistry()
+    {
+        return normalizerRegistry;
+    }
+
+    
+    /**
+     * @return The ObjectClass registry
+     */
+    public ObjectClassRegistry getObjectClassRegistry()
+    {
+        return objectClassRegistry;
+    }
+
+    
+    /**
+     * @return The getOid registry
+     */
+    public OidRegistry getOidRegistry()
+    {
+        return oidRegistry;
+    }
+
+    
+    /**
+     * @return The SyntaxChecker registry
+     */
+    public SyntaxCheckerRegistry getSyntaxCheckerRegistry()
+    {
+        return syntaxCheckerRegistry;
+    }
+
+    
+    /**
+     * @return The LdapSyntax registry
+     */
+    public LdapSyntaxRegistry getLdapSyntaxRegistry()
+    {
+        return ldapSyntaxRegistry;
+    }
+    
+
+    //List<Throwable> checkRefInteg();
+
+    //Schema getSchema( String schemaName );
+
+    /**
+     * Removes a schema from the loaded set without unloading the schema.
+     * This should be used ONLY when an enabled schema is deleted.
+     * 
+     * @param schemaName the name of the schema to remove
+     */
+    //void removeFromLoadedSet( String schemaName );
+    
+    /**
+     * Adds a schema to the loaded set but does not load the schema in 
+     * question.  This may be a temporary fix for new schemas being added
+     * which are enabled yet do not have any schema entities associated 
+     * with them to load.  In this case all objects added under this 
+     * schema will load when added instead of in bulk.
+     * 
+     * @param schema the schema object to add to the loaded set.
+     */
+    //void addToLoadedSet( Schema schema );
+}
