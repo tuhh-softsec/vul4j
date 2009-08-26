a6baedf32d39158df170c0e27db47fe077e79e8f
Alex Karasulu <akarasulu@apache.org>
2009-08-26 23:55:11 +0000

    moved and un-abstracted the AbstractSchema to be the DefaultSchema while moving it from apacheds to shared
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/shared-schema@808228 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldap/src/main/java/org/apache/directory/shared/ldap/schema/registries/DefaultSchema.java
@@ -0,0 +1,144 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License. 
+ *  
+ */
+package org.apache.directory.shared.ldap.schema.registries;
+
+
+
+/**
+ * The default Schema interface implementation.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ * @version $Rev$
+ */
+public class DefaultSchema implements Schema
+{
+    private static final String[] NONE = new String[0];
+    private static final String DEFAULT_OWNER = "uid=admin,ou=system";
+    
+    private boolean disabled;
+    private String[] dependencies;
+    private String owner;
+    private String name;
+    
+    
+    public DefaultSchema( String name )
+    {
+        this( name, null, null, false );
+    }
+    
+        
+    public DefaultSchema( String name, String owner )
+    {
+        this( name, owner, null, false );
+    }
+    
+        
+    public DefaultSchema( String name, String owner, String[] dependencies )
+    {
+        this( name, owner, dependencies, false );
+    }
+    
+        
+    public DefaultSchema( String name, String owner, String[] dependencies, boolean disabled )
+    {
+        if ( name == null )
+        {
+            throw new NullPointerException( "name cannot be null" );
+        }
+        
+        this.name = name;
+        
+        if ( owner != null )
+        {
+            this.owner = owner;
+        }
+        else
+        {
+            this.owner = DEFAULT_OWNER;
+        }
+        
+        if ( dependencies != null )
+        {
+            this.dependencies = dependencies;
+        }
+        else
+        {
+            this.dependencies = NONE;
+        }
+        
+        this.disabled = disabled;
+    }
+
+
+    public String[] getDependencies()
+    {
+        String[] copy = new String[dependencies.length];
+        System.arraycopy( dependencies, 0, copy, 0, dependencies.length );
+        return copy;
+    }
+
+    
+    public String getOwner()
+    {
+        return owner;
+    }
+
+    
+    public String getSchemaName()
+    {
+        return name;
+    }
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isDisabled()
+    {
+        return disabled;
+    }
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public boolean isEnabled()
+    {
+        return !disabled;
+    }
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void disable()
+    {
+        this.disabled = true;
+    }
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void enable()
+    {
+        this.disabled = false;
+    }
+}
