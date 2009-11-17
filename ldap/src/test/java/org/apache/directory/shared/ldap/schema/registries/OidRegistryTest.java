f83bb3bdb4142c54bd67baba363e0abe7176c45b
Emmanuel Lecharny <elecharny@apache.org>
2009-11-17 17:36:17 +0000

    Remove the clone test
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/shared-schema@881398 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/test/java/org/apache/directory/shared/ldap/schema/registries/OidRegistryTest.java
+++ ldap/src/test/java/org/apache/directory/shared/ldap/schema/registries/OidRegistryTest.java
@@ -19,11 +19,6 @@
  */
 package org.apache.directory.shared.ldap.schema.registries;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.apache.directory.shared.ldap.schema.AttributeType;
 import org.junit.Test;
 
 
@@ -37,41 +32,7 @@ public class OidRegistryTest
 {
 
     @Test
-    public void testClone() throws Exception
+    public void testCopy() throws Exception
     {
-        OidRegistry oidRegistry = new OidRegistry();
-        AttributeType at0 = new AttributeType( "1.1" );
-        at0.addName( "t", "test", "Test", "T" );
-
-        oidRegistry.register( at0 );
-
-        AttributeType at1 = new AttributeType( "1.2" );
-        at1.addName( "u", "unit", "Unit", "U" );
-
-        oidRegistry.register( at1 );
-        
-        // Clone the oidRegistry
-        OidRegistry clone = oidRegistry.clone();
-        
-        assertTrue( clone.hasOid( "1.1" ) );
-        assertTrue( clone.hasOid( "1.2" ) );
-        
-        at0.setOid( "2.1" );
-        assertFalse( oidRegistry.hasOid( "2.1" ) );
-        assertFalse( clone.hasOid( "2.1" ) );
-        
-        AttributeType at = (AttributeType)oidRegistry.getSchemaObject( "1.1" );
-        assertEquals( "2.1", at.getOid() );
-        
-        at = (AttributeType)clone.getSchemaObject( "1.1" );
-        assertEquals( "1.1", at.getOid() );
-
-        oidRegistry.unregister( "2.1" );
-        oidRegistry.unregister( "1.1" );
-        assertFalse( oidRegistry.hasOid( "1.1" ) );
-        assertTrue( oidRegistry.hasOid( "1.2" ) );
-        assertFalse( oidRegistry.hasOid( "2.1" ) );
-        assertTrue( clone.hasOid( "1.1" ) );
-        assertTrue( clone.hasOid( "1.2" ) );
     }
 }
