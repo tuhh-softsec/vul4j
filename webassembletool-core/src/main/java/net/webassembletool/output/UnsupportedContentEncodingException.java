4090c417eebf62bd82a28f8324b880ec7b0c3e6d
Francois-Xavier <francois-xavier.bonnet@centraliens.net>
2009-12-01 15:12:19 +0000

    Fixed warning

--- webassembletool-core/src/main/java/net/webassembletool/output/UnsupportedContentEncodingException.java
+++ webassembletool-core/src/main/java/net/webassembletool/output/UnsupportedContentEncodingException.java
@@ -1,6 +1,9 @@
 package net.webassembletool.output;
 
 public class UnsupportedContentEncodingException extends RuntimeException {
+
+	private static final long serialVersionUID = -3514450770500951780L;
+
 	public UnsupportedContentEncodingException(String message) {
 		super(message);
 	}
