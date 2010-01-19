5ad9ad0b1b450fdbd40635177ce756937d62e3be
Emmanuel Lecharny <elecharny@apache.org>
2010-01-19 22:38:46 +0000

    Challenged by Seelmann, here is a faster version of the UUID SC :)
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@900990 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/schema/syntaxCheckers/UuidSyntaxChecker.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/schema/syntaxCheckers/UuidSyntaxChecker.java
@@ -21,6 +21,7 @@ package org.apache.directory.shared.ldap.schema.syntaxCheckers;
 
 import org.apache.directory.shared.ldap.constants.SchemaConstants;
 import org.apache.directory.shared.ldap.schema.SyntaxChecker;
+import org.apache.directory.shared.ldap.util.StringTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,8 +42,12 @@ public class UuidSyntaxChecker extends SyntaxChecker
     /** The serialVersionUID */
     private static final long serialVersionUID = 1L;
 
-    private static final String UUID_REGEX = "^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$";
-    
+    // Tells if the byte is alphanumeric
+    private static boolean isHex( byte b )
+    {
+        return ( b > 0 ) && StringTools.ALPHA_DIGIT[b];
+    }
+
     /**
      * Creates a new instance of UUIDSyntaxChecker.
      */
@@ -69,15 +74,33 @@ public class UuidSyntaxChecker extends SyntaxChecker
             return false;
         }
 
-        String uuid = ( String ) value;
-        if ( ! uuid.matches( UUID_REGEX ) )
+        byte[] b = ((String)value).getBytes();
+        
+        if ( b.length < 36)
         {
-            LOG.debug( "Syntax invalid for '{}'", value );
             return false;
         }
         
-        // There is not that much more we can check.
-        LOG.debug( "Syntax valid for '{}'", value );
-        return true;
+        if ( 
+            isHex( b[0] ) && isHex( b[1] ) && isHex( b[2] ) && isHex( b[3] ) &
+            isHex( b[4] ) && isHex( b[5] ) && isHex( b[6] ) && isHex( b[7] ) &
+            b[8] == '-' &
+            isHex( b[9] ) && isHex( b[10] ) && isHex( b[11] ) && isHex( b[12] ) &
+            b[13] == '-' &
+            isHex( b[14] ) && isHex( b[15] ) && isHex( b[16] ) && isHex( b[17] ) &
+            b[18] == '-' &
+            isHex( b[19] ) && isHex( b[20] ) && isHex( b[21] ) && isHex( b[22] ) &
+            b[23] == '-' &
+            isHex( b[24] ) && isHex( b[25] ) && isHex( b[26] ) && isHex( b[27] ) &
+            isHex( b[28] ) && isHex( b[29] ) && isHex( b[30] ) && isHex( b[31] ) &
+            isHex( b[32] ) && isHex( b[33] ) && isHex( b[34] ) && isHex( b[35] ) )
+        {
+            // There is not that much more we can check.
+            LOG.debug( "Syntax valid for '{}'", value );
+            return true;
+        }
+
+        LOG.debug( "Syntax invalid for '{}'", value );
+        return false;
     }
 }
