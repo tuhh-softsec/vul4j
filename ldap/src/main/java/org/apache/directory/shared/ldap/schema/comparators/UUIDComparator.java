905703def6a74d227e703b7f8ba98f5d3faf27d7
Emmanuel Lecharny <elecharny@apache.org>
2010-03-10 22:53:41 +0000

    Removed useless imports.
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@921609 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/schema/comparators/UUIDComparator.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/schema/comparators/UUIDComparator.java
@@ -21,7 +21,6 @@ package org.apache.directory.shared.ldap.schema.comparators;
 
 
 import org.apache.directory.shared.ldap.schema.LdapComparator;
-import org.apache.directory.shared.ldap.util.StringTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
