b0441d169330268b21766d01ee2afdb6e8020c90
Francois-Xavier <francois-xavier.bonnet@centraliens.net>
2010-04-12 14:13:24 +0000

    Added serial version uid

--- webassembletool-core/src/main/java/net/webassembletool/UserContext.java
+++ webassembletool-core/src/main/java/net/webassembletool/UserContext.java
@@ -1,17 +1,17 @@
- /* 
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  */
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package net.webassembletool;
 
 import java.io.Serializable;
@@ -33,14 +33,16 @@ import org.apache.http.protocol.HttpContext;
  * @contributor Nicolas Richeton
  * 
  */
-public class UserContext implements Serializable{
+public class UserContext implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private String user;
-	private HttpContext httpContext;
+	private final HttpContext httpContext;
+
 	public HttpContext getHttpContext() {
 		return httpContext;
 	}
 
-	private CookieStore cookieStore;
+	private final CookieStore cookieStore;
 
 	public UserContext() {
 		// Create a local instance of cookie store
@@ -74,10 +76,11 @@ public class UserContext implements Serializable{
 		result.append(" Cookies={\n");
 		for (Cookie cookie : cookieStore.getCookies()) {
 			result.append("\t");
-			if (cookie.isSecure())
+			if (cookie.isSecure()) {
 				result.append("https");
-			else
+			} else {
 				result.append("http");
+			}
 			result.append("://");
 			result.append(cookie.getDomain());
 			result.append(cookie.getPath());
