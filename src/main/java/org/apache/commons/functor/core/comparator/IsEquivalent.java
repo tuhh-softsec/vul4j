211a74a38e1bb67428b86df4d4dfb4d5e0b456db
Matthew Jason Benson <mbenson@apache.org>
2010-05-03 16:19:16 +0000

    no warning to suppress
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/functor/trunk@940501 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/functor/core/comparator/IsEquivalent.java
+++ src/main/java/org/apache/commons/functor/core/comparator/IsEquivalent.java
@@ -40,7 +40,6 @@ public final class IsEquivalent<T> implements BinaryPredicate<T, T>, Serializabl
     /**
      * Basic IsEquivalent instance.
      */
-    @SuppressWarnings("unchecked")
     public static final IsEquivalent<Comparable<?>> INSTANCE = IsEquivalent.<Comparable<?>> instance();
 
     private Comparator<? super T> comparator;
