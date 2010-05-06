f01b7909a1b06b07e13e5f541c6aecb8fa77afa8
Scott Cantor <scantor@apache.org>
2010-05-06 14:23:19 +0000

    https://issues.apache.org/bugzilla/show_bug.cgi?id=49256
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@941751 13f79535-47bb-0310-9956-ffa450edef68

--- c/src/dsig/DSIGTransformXPathFilter.cpp
+++ c/src/dsig/DSIGTransformXPathFilter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 The Apache Software Foundation.
+ * Copyright 2002-2010 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -207,10 +207,9 @@ unsigned int DSIGTransformXPathFilter::getExprNum(void) {
 
 DSIGXPathFilterExpr * DSIGTransformXPathFilter::expr(unsigned int n) {
 
-	if (n > m_exprs.size())
-		return NULL;
+	if (n < m_exprs.size())
+		return m_exprs[n];
 
-	return m_exprs[n];
+	return NULL;
 
 }
-
