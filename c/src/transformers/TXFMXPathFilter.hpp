3254f865dc0df68c452717dca3d5b175a455aba9
Scott Cantor <scantor@apache.org>
2010-05-06 18:15:25 +0000

    https://issues.apache.org/bugzilla/show_bug.cgi?id=49260
    git-svn-id: https://svn.apache.org/repos/asf/xml/security/trunk@941838 13f79535-47bb-0310-9956-ffa450edef68

--- c/src/transformers/TXFMXPathFilter.hpp
+++ c/src/transformers/TXFMXPathFilter.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 The Apache Software Foundation.
+ * Copyright 2002-2010 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@
  */
 
 #if !defined (TXFMXPATHFILTER_HEADER)
-#define XFMXPATHFILTER_HEADER
+#define TXFMXPATHFILTER_HEADER
 
 #include <xsec/transformers/TXFMBase.hpp>
 #include <xsec/utils/XSECXPathNodeList.hpp>
