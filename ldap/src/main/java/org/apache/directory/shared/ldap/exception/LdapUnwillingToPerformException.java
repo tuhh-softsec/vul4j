7a26499d682e7f63cf582dc4a4244866f6bbe232
Emmanuel Lecharny <elecharny@apache.org>
2010-07-28 22:33:29 +0000

    Created some constructors that use the ResultCodeEnum.UNWILLING_TO_PERFORM value
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@980255 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/exception/LdapUnwillingToPerformException.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/exception/LdapUnwillingToPerformException.java
@@ -6,16 +6,16 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ *
  */
 package org.apache.directory.shared.ldap.exception;
 
@@ -25,7 +25,7 @@ import org.apache.directory.shared.ldap.message.ResultCodeEnum;
 
 
 /**
- * An LDAPException that extends the {@link LdapOperationException} 
+ * An LDAPException that extends the {@link LdapOperationException}
  * carrying with it the corresponding result codes for this condition.
  *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
@@ -34,7 +34,17 @@ public class LdapUnwillingToPerformException extends LdapOperationException
 {
     /** The serial version UUID */
     static final long serialVersionUID = 1L;
-    
+
+    /**
+     * Creates a new instance of LdapUnwillingToPerformException, with
+     * a default ResultCode to UNWILING_TO_PERFORM.
+     */
+    public LdapUnwillingToPerformException()
+    {
+        super( ResultCodeEnum.UNWILLING_TO_PERFORM, null );
+    }
+
+
     /**
      * Creates a new instance of LdapUnwillingToPerformException.
      *
@@ -51,7 +61,18 @@ public class LdapUnwillingToPerformException extends LdapOperationException
 
     /**
      * Creates a new instance of LdapUnwillingToPerformException.
-     * 
+     *
+     * @param message The exception message
+     */
+    public LdapUnwillingToPerformException( String message )
+    {
+        super( ResultCodeEnum.UNWILLING_TO_PERFORM, message );
+    }
+
+
+    /**
+     * Creates a new instance of LdapUnwillingToPerformException.
+     *
      * @param resultCode the ResultCodeEnum for this exception
      */
     public LdapUnwillingToPerformException( ResultCodeEnum resultCode )
@@ -65,7 +86,7 @@ public class LdapUnwillingToPerformException extends LdapOperationException
     /**
      * Checks to make sure the resultCode value is right for this exception
      * type.
-     * 
+     *
      * @throws IllegalArgumentException
      *             if the result code is not one of
      *             {@link ResultCodeEnum#UNWILLING_TO_PERFORM},
@@ -78,7 +99,7 @@ public class LdapUnwillingToPerformException extends LdapOperationException
             case UNWILLING_TO_PERFORM :
             case UNAVAILABLE_CRITICAL_EXTENSION :
                 return;
-                
+
             default:
                 throw new IllegalArgumentException( I18n.err( I18n.ERR_04140, resultCode ) );
         }
