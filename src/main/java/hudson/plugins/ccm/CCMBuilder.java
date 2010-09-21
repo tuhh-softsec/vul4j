9a7568c97b7bbae5900e98bd37e5fae054b86c7b
kinow <kinow@dev.java.net>
2010-09-21 16:42:38 +0000

    Removed the code that created the CCM config file and started to use a FileCallable object.

--- src/main/java/hudson/plugins/ccm/CCMBuilder.java
+++ src/main/java/hudson/plugins/ccm/CCMBuilder.java
@@ -25,22 +25,18 @@ package hudson.plugins.ccm;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
-import hudson.Util;
 import hudson.model.AbstractBuild;
 import hudson.model.Build;
 import hudson.model.BuildListener;
 import hudson.model.Descriptor;
+import hudson.model.Result;
+import hudson.plugins.ccm.config.CCMConfigCallable;
 import hudson.plugins.ccm.model.CCM;
-import hudson.plugins.ccm.util.CCMUtil;
 import hudson.tasks.Builder;
 import hudson.util.ArgumentListBuilder;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.io.Serializable;
 import java.util.Map;
 
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -61,10 +57,11 @@ import org.kohsuke.stapler.StaplerRequest;
 * will be invoked. 
 * 
 * @author Bruno P. Kinoshita - http://www.kinoshita.eti.br
-* @since 7 april, 2010 
+* @since 1.0
 */
 public class CCMBuilder 
 extends Builder 
+implements Serializable
 {
 
 	/**
@@ -91,6 +88,21 @@ extends Builder
     @Extension
     public static final CCMBuilderDescription DESCRIPTOR = new CCMBuilderDescription();
     
+    /**
+     * Name of generated config file for CCM.
+     */
+    public final static String CCM_CONFIG_FILE = "ccm.config.xml";
+    
+    /**
+     * Name of generated result file of CCM.
+     */
+    public static final String CCM_RESULT_FILE = "ccm.result.xml";
+	
+    /**
+     * Default number of metrics.
+     */
+    public static final Integer DEFAULT_NUMBER_OF_METRICS = 30;
+    
     @DataBoundConstructor
     public CCMBuilder(
     		String ccmName, 
@@ -104,7 +116,7 @@ extends Builder
 		this.srcFolder = srcFolder;
 		this.recursive = recursive;
 		this.outputXml = outputXml;
-		this.numMetrics = ((numMetrics == null || numMetrics.length()<=0) ? "30" : numMetrics);
+		this.numMetrics = ((numMetrics == null || numMetrics.length()<=0) ? DEFAULT_NUMBER_OF_METRICS.toString() : numMetrics);
 	}
     
     public String getCcmName() 
@@ -157,118 +169,12 @@ extends Builder
     	{
     		if ( this.getCcmName() != null && installation.getName().equals(this.getCcmName()))
     		{
-    			//TOTHINK Check if the executable exists.
     			foundInstallation =  installation;
     		}
     	}
     	
     	return foundInstallation;
     }
-    
-	/**
-	 * <p>Creates the XML input file for CCM.exe. It uses data provided by 
-	 * user in the project configuration page (e.g.:{@link #recursive}).</p> 
-	 * @param workspace
-	 * @param listener
-	 * @throws IOException
-	 */
-    private void createXMLConfig(FilePath workspace, BuildListener listener) 
-	throws IOException
-	{
-		// TBD: the file name is hard coded... fix it later.
-		File ccmConfigFile = new File(workspace.getRemote(), "ccm.config.xml");
-		
-		listener.getLogger().println("Creating CCM config file " + ccmConfigFile.getAbsolutePath());
-		//TBD: improve this
-		ccmConfigFile.createNewFile();
-		
-		StringBuffer buffer = new StringBuffer();
-		
-		// TBD: eck! correct this. later...
-		buffer.append("<ccm>\n");
-		buffer.append("<exclude></exclude>\n");
-		buffer.append("<analyze>\n");
-		buffer.append("<folder>"+CCMUtil.getSrcFolderRelativeToWorkspace(this.getSrcFolder(), workspace)+"</folder>\n");
-		buffer.append("</analyze>\n");
-		buffer.append("<recursive>"+CCMUtil.yesOrNo(this.isRecursive())+"</recursive>\n");
-		buffer.append("<outputXML>yes</outputXML>\n");
-		buffer.append("<numMetrics>"+this.getNumMetrics()+"</numMetrics>\n");
-		buffer.append("</ccm>\n");
-		
-		listener.getLogger().println("Writing CCM configuration into file");
-		listener.getLogger().println(buffer.toString());
-		
-		FileWriter writer = new FileWriter(ccmConfigFile);
-		writer.append(buffer.toString());
-		writer.flush(); // TBD: do it better.
-		writer.close();
-		
-	}
-	
-    /**
-     * <p>The CCM executable, by default, prints a header before the results in 
-     * XML format.</p>
-     * 
-     * @param file The CCM result file.
-     * @param logger Log.
-     */
-    private void fixResultFile(File file, PrintStream logger) 
-	{
-		BufferedReader reader = null;
-		
-		StringBuffer newFile = new StringBuffer();
-		
-		logger.println("Removing CCM header line...");
-		try 
-		{
-			reader = new BufferedReader( new FileReader(file) );
-			String line = null;
-			
-			boolean found = false;
-			while((line = reader.readLine()) != null )
-			{
-				if ( !found && line.contains("Loading configuration from") )
-				{
-					found = true;
-					//newFile.append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
-				} else {
-					newFile.append(line);
-					//TOTHINK CCM is Win32 only. What if it is running in Wine?
-					// Perhaps we should use System.getProperty... line.separator?
-					newFile.append("\n");
-				}				
-			}
-			
-		} catch (IOException e) {
-			logger.println("Error removing header from CCM result file ["+file+"]: " + e.getMessage());
-		} finally {
-			if ( reader != null )
-			{
-				try {
-					reader.close();
-				} catch (IOException e2) {
-					e2.printStackTrace();
-				}
-			}
-		}
-		
-		FileWriter writer = null;
-		try {
-			writer = new FileWriter(file);
-			writer.write(newFile.toString().trim());
-		} catch (IOException e) {
-			logger.println("Error fix CCM result file: " + e.getMessage());
-		}finally {
-			if ( writer != null )
-			{
-				try {
-					writer.close();
-				} catch (IOException e2) {
-					e2.printStackTrace();
-				}
-			}
-		}
-	}
     
 	/**
 	 * <p>Called when the job is executed.</p>
@@ -281,64 +187,75 @@ extends Builder
 	 * {@link CCMPublisher}.</p>
 	 */
 	@Override
-	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher,
-			BuildListener listener) 
-	throws InterruptedException, IOException {
-		
-		ArgumentListBuilder args = new ArgumentListBuilder();
+	public boolean perform(
+			final AbstractBuild<?, ?> build, 
+			final Launcher launcher,
+			final BuildListener listener ) 
+	throws InterruptedException, IOException 
+	{
+		// List of arguments
+		final ArgumentListBuilder args = new ArgumentListBuilder();
     	
-    	CCMBuilderInstallation installation = getCCM();
-    	String execName = installation.getPathToCCM();
+		// CCM installation
+    	final CCMBuilderInstallation installation = getCCM();
+    	
+    	// ------------------------- 
+    	// Check CCM installation    
+    	// ------------------------- 
     	if ( installation == null )
     	{
-    		listener.fatalError("Invalid CCM installation");
+    		listener.error("Missing CCM installation");
+    		build.setResult(Result.FAILURE);
     		return false;
-    	} else {
-    		File exec = installation.getExecutable();
-    		if ( ! installation.getExists() )
-    		{
-    			listener.fatalError(exec+" doesn't exist");
-    			return false;
-    		}
-    		listener.getLogger().println("Path To CCM.exe: " + execName);
-    		args.add(execName);
-    	}
+    	} 
     	
+    	// ------------------------- 
+    	// Preparing arguments list   
+    	// ------------------------- 
+    	
+    	// Path to CCM.exe
+    	final String pathToCCM = installation.getExecutable(launcher);
+    	listener.getLogger().println("Path To CCM.exe: " + pathToCCM);
+    	args.add(pathToCCM);		
+    	
+    	final FilePath workspace = build.getWorkspace();
+    	// ------------------------- 
+    	// Creating CCM config file   
+    	// ------------------------- 
+        
     	// create project ccm config file
-    	FilePath workspace = build.getWorkspace();
-    	try {
-			createXMLConfig(workspace, listener);
-		} catch (IOException e) {
-			Util.displayIOException(e,listener);
-			e.printStackTrace( listener.fatalError("Error creating CCM config file") );
-			return false;
-		}
-		args.add(new File(workspace.getRemote(), "ccm.config.xml"));
-		
-		//According to the Ant builder source code, in order to launch a program 
+        
+        CCMConfigCallable ccmConfigGenerator = new CCMConfigCallable( srcFolder, recursive, numMetrics, listener );
+        String ccmConfigFile = workspace.act( ccmConfigGenerator );
+    	args.add(ccmConfigFile);
+    	
+    	//According to the Ant builder source code, in order to launch a program 
         //from the command line in windows, we must wrap it into cmd.exe.  This 
         //way the return code can be used to determine whether or not the build failed.
-        if(!launcher.isUnix()) {
-            args.prepend("cmd.exe","/C");
-            args.add(">", "ccm.result.xml");
-            args.add("&&","exit","%%ERRORLEVEL%%");
-        } else {
-        	listener.fatalError("CCM can be run only in Win platforms.");
-        	return false;
-        }
+        if( ! launcher.isUnix() ) // maybe user is using Wine? 
+        {
+            args.prepend( "cmd.exe", "/C" );
+            args.add( ">", CCM_RESULT_FILE );
+            args.add( "&&", "exit", "%%ERRORLEVEL%%" );
+        } 
+
+		// ------------------------- 
+    	// Executing CCM    
+    	// ------------------------- 
         
-        // Try to execute the command
-        listener.getLogger().println("Executing command: "+args.toStringWithQuote());
-        try {
+        listener.getLogger().println("Executing CCM command: "+args.toStringWithQuote());
+        
+        try 
+        {
             Map<String,String> env = build.getEnvironment(listener);
             int r = launcher.launch().cmds(args).envs(env).stdout(listener).pwd(build.getModuleRoot()).join();
             
-            fixResultFile( new File(workspace.getRemote(), "ccm.result.xml"), listener.getLogger());
-            
             return r==0;
-        } catch (IOException e) {
-            Util.displayIOException(e,listener);
-            e.printStackTrace( listener.fatalError("command execution failed") );
+        }
+        catch (Exception e) 
+        {
+            e.printStackTrace( listener.error("CCM command execution failed") );
+            build.setResult(Result.FAILURE);
             return false;
         }
 		
