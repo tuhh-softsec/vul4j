58385d1444943b803542e89d992291668827af28
Philippe De Oliveira <philippe.deoliveira@gmail.com>
2010-10-05 14:52:59 +0000

    death code removed

--- webassembletool-core/src/main/java/net/webassembletool/file/FileResource.java
+++ webassembletool-core/src/main/java/net/webassembletool/file/FileResource.java
@@ -8,8 +8,8 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import net.webassembletool.ResourceContext;
 import net.webassembletool.output.Output;
@@ -78,10 +78,7 @@ public class FileResource extends Resource {
 				output.addHeader(header.getKey(), header.getValue());
 			}
 		}
-		// String charset = output.getHeader("content-encoding");
-		// if (charset != null) {
-		// output.setCharsetName(charset);
-		// }
+
 		if (file != null) {
 			InputStream inputStream = new FileInputStream(file);
 			try {
