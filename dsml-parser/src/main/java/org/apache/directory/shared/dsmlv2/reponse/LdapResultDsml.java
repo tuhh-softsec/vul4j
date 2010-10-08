f9dd27606e3b13a896c4cbbed04846791c89ca4e
Pierre-Arnaud Marcelot <pamarcelot@apache.org>
2010-10-08 09:32:51 +0000

    Cleaner test. Thanks elecharny for the tip.
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1005747 13f79535-47bb-0310-9956-ffa450edef68

--- dsml-parser/src/main/java/org/apache/directory/shared/dsmlv2/reponse/LdapResultDsml.java
+++ dsml-parser/src/main/java/org/apache/directory/shared/dsmlv2/reponse/LdapResultDsml.java
@@ -80,7 +80,7 @@ public class LdapResultDsml implements DsmlDecorator
         // Matched DN
         DN matchedDN = result.getMatchedDn();
 
-        if ( ( matchedDN != null ) && ( !matchedDN.getName().equals( "" ) ) )
+        if ( !DN.EMPTY_DN.equals( matchedDN ) )
         {
             root.addAttribute( "matchedDN", matchedDN.getName() );
         }
