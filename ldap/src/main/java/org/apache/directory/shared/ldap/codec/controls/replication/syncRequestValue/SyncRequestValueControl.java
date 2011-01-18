981fd9649a68f8d14f3c9aa4383af33a695ca9d4
Emmanuel Lecharny <elecharny@apache.org>
2011-01-18 15:35:03 +0000

    Forgot one Control
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1060419 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/codec/controls/replication/syncRequestValue/SyncRequestValueControl.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/codec/controls/replication/syncRequestValue/SyncRequestValueControl.java
@@ -20,6 +20,7 @@
 package org.apache.directory.shared.ldap.codec.controls.replication.syncRequestValue;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 import org.apache.directory.shared.asn1.ber.tlv.TLV;
 import org.apache.directory.shared.asn1.ber.tlv.UniversalTag;
@@ -235,6 +236,26 @@ public class SyncRequestValueControl  extends AbstractControl
         }
         
         return value;
+    }
+
+
+
+
+    /**
+     * @see Object#equals(Object)
+     */
+    public boolean equals( Object o )
+    {
+        if ( !super.equals( o ) )
+        {
+            return false;
+        }
+
+        SyncRequestValueControl otherControl = ( SyncRequestValueControl ) o;
+        
+        return ( mode == otherControl.mode ) && 
+            ( reloadHint == otherControl.reloadHint ) &&
+            ( Arrays.equals( cookie, otherControl.cookie ) );
     }
 
 
