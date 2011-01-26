6b5ee824cc1462ef37ff22d58c01c0003b4cc1cf
Simone Tripodi <simonetripodi@apache.org>
2011-01-26 14:07:40 +0000

    fixed javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1063727 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/spi/ObjectCreationFactory.java
+++ src/main/java/org/apache/commons/digester3/spi/ObjectCreationFactory.java
@@ -21,9 +21,8 @@ import org.xml.sax.Attributes;
 
 /**
  * Interface for use with {@link FactoryCreateRule}.
- * The rule calls {@link #createObject} to create an object
- * to be pushed onto the <code>Digester</code> stack
- * whenever it is matched.
+ * The rule calls {@link org.apache.commons.digester3.RulesBinder#createObject} to create an object
+ * to be pushed onto the {@code Digester} stack whenever it is matched.
  */
 public interface ObjectCreationFactory<T> {
 
