bde52173d05ecfe6d039cdd482d596a1ea0958f0
Alex Karasulu <akarasulu@apache.org>
2011-01-26 01:20:45 +0000

    moving syntaxChecker to proper area
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/trunk@1063551 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/src/main/java/org/apache/directory/shared/ldap/aci/ACIItemSyntaxChecker.java
+++ ldap/src/main/java/org/apache/directory/shared/ldap/aci/ACIItemSyntaxChecker.java
@@ -17,11 +17,10 @@
  *  under the License. 
  *  
  */
-package org.apache.directory.shared.ldap.model.schema.syntaxCheckers;
+package org.apache.directory.shared.ldap.aci;
 
 import java.text.ParseException;
 
-import org.apache.directory.shared.ldap.aci.ACIItemChecker;
 import org.apache.directory.shared.ldap.model.constants.SchemaConstants;
 import org.apache.directory.shared.ldap.model.schema.SchemaManager;
 import org.apache.directory.shared.ldap.model.schema.SyntaxChecker;
