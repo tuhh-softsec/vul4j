4d1c4e91b9438e223b3648a6a112007ecfaead1c
Simone Tripodi <simonetripodi@apache.org>
2011-01-28 20:43:40 +0000

    first checkin of LinkedCallParamBuilder interface, chained when callParam() is invoked
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1064863 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/commons/digester3/rulesbinder/LinkedCallParamBuilder.java
@@ -0,0 +1,49 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.digester3.rulesbinder;
+
+/**
+ * Builder chained when invoking {@link LinkedRuleBuilder#callParam(int)}.
+ */
+public interface LinkedCallParamBuilder extends BackToLinkedRuleBuilder {
+
+    /**
+     * Sets the attribute from which to save the parameter value.
+     *
+     * @param attributeName The attribute from which to save the parameter value
+     * @return this builder instance
+     */
+    LinkedCallParamBuilder fromAttribute(String attributeName);
+
+    /**
+     * Flags the parameter to be set from the stack.
+     *
+     * @param fromStack the parameter flag to be set from the stack
+     * @return this builder instance
+     */
+    LinkedCallParamBuilder fromStack(boolean fromStack);
+
+    /**
+     * Sets the position of the object from the top of the stack.
+     *
+     * @param stackIndex The position of the object from the top of the stack
+     * @return this builder instance
+     */
+    LinkedCallParamBuilder withStackIndex(int stackIndex);
+
+}
