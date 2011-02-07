99d3c29d42b36eea68fbffb979e054c8e3a7bb03
Emmanuel Lecharny <elecharny@apache.org>
2011-02-07 20:51:43 +0000

    Fixing some more warnings
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/m1@1068126 13f79535-47bb-0310-9956-ffa450edef68

--- asn1-ber/src/main/java/org/apache/directory/shared/asn1/ber/grammar/Grammar.java
+++ asn1-ber/src/main/java/org/apache/directory/shared/asn1/ber/grammar/Grammar.java
@@ -6,16 +6,16 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ *
  */
 package org.apache.directory.shared.asn1.ber.grammar;
 
@@ -26,24 +26,24 @@ import org.apache.directory.shared.asn1.ber.Asn1Container;
 
 /**
  * The interface which expose common behavior of a Grammar implementer.
- * 
+ *
  * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
  */
-public interface Grammar
+public interface Grammar<E extends Asn1Container>
 {
     /**
      * This method, when called, execute an action on the current data stored in
      * the container.
-     * 
+     *
      * @param asn1Container Store the data being processed.
      * @throws DecoderException Thrown when an unrecoverable error occurs.
      */
-    void executeAction( Asn1Container asn1Container ) throws DecoderException;
+    void executeAction( E asn1Container ) throws DecoderException;
 
 
     /**
      * Get the grammar name
-     * 
+     *
      * @return Return the grammar's name
      */
     String getName();
@@ -51,7 +51,7 @@ public interface Grammar
 
     /**
      * Set the grammar's name
-     * 
+     *
      * @param name The grammar name
      */
     void setName( String name );
