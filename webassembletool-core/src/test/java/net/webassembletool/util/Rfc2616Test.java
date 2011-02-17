50eb8665d7ed923215bf944bb53e5bc127cba62b
Anton Liulichev <anton.liulichev@gmail.com>
2011-02-17 16:26:35 +0000

    http://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=21 fixed.

--- webassembletool-core/src/test/java/net/webassembletool/util/Rfc2616Test.java
+++ webassembletool-core/src/test/java/net/webassembletool/util/Rfc2616Test.java
@@ -8,6 +8,7 @@ import net.webassembletool.output.Output;
 import net.webassembletool.resource.Resource;
 
 import org.apache.http.impl.cookie.DateUtils;
+import org.junit.Assert;
 
 /**
  * Tests for RFC2616 handling.
@@ -17,51 +18,84 @@ import org.apache.http.impl.cookie.DateUtils;
  */
 public class Rfc2616Test extends TestCase {
 
-    /**
-     * Ensure max-date is correctly used (in seconds). See
-     * https://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=17
-     */
-    public void test_getExpiration() {
+	/**
+	 * Ensure max-date is correctly used (in seconds). See
+	 * https://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=17
+	 */
+	public void test_getExpiration() {
 
-        final Date now = new Date();
+		final Date now = new Date();
 
-        Resource r = new Resource() {
+		Resource r = new Resource() {
 
-            @Override
-            public void render(Output output) throws IOException {
-            }
+			@Override
+			public void render(Output output) throws IOException {
+			}
 
-            @Override
-            public void release() {
-            }
+			@Override
+			public void release() {
+			}
 
-            @Override
-            public int getStatusCode() {
-                return 200;
-            }
+			@Override
+			public int getStatusCode() {
+				return 200;
+			}
 
-            @Override
-            public String getHeader(String name) {
-                if (name.equals("Cache-control")) {
-                    return "max-age=60";
-                }
+			@Override
+			public String getHeader(String name) {
+				if (name.equals("Cache-control")) {
+					return "max-age=60";
+				}
 
-                if (name.equals("Date")) {
-                    return DateUtils.formatDate(now);
-                }
+				if (name.equals("Date")) {
+					return DateUtils.formatDate(now);
+				}
 
-                return null;
-            }
+				return null;
+			}
 
-        };
+		};
 
-        Date d = Rfc2616.getExpiration(r);
-        assertTrue(d.after(now));
+		Date d = Rfc2616.getExpiration(r);
+		assertTrue(d.after(now));
 
-        // Distance will not be exactly 60000.
-        long distance = (d.getTime() - now.getTime());
-        assertTrue(distance > 59000 && distance < 61000);
+		// Distance will not be exactly 60000.
+		long distance = (d.getTime() - now.getTime());
+		assertTrue(distance > 59000 && distance < 61000);
 
-    }
+	}
+
+	public void test_getDate() {
+
+		Resource resourceWithoutDate = new Resource() {
+
+			@Override
+			public void render(Output output) throws IOException {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void release() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public int getStatusCode() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public String getHeader(String name) {
+
+				return null;
+			}
+		};
+
+		Date date = Rfc2616.getDate(resourceWithoutDate);
+		Assert.assertNotNull(date);
+	}
 
 }
