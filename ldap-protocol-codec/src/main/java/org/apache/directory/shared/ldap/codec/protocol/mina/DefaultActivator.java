9f34164d4ab1783ce6d1fa818e454f887f8ac463
Alex Karasulu <akarasulu@apache.org>
2011-02-27 21:32:00 +0000

    adding an activator for the shared-ldap-protocol-codec bundle
    git-svn-id: https://svn.apache.org/repos/asf/directory/shared/branches/akarasulu@1075152 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ldap-protocol-codec/src/main/java/org/apache/directory/shared/ldap/codec/protocol/mina/DefaultActivator.java
@@ -0,0 +1,65 @@
+/*
+ *   Licensed to the Apache Software Foundation (ASF) under one
+ *   or more contributor license agreements.  See the NOTICE file
+ *   distributed with this work for additional information
+ *   regarding copyright ownership.  The ASF licenses this file
+ *   to you under the Apache License, Version 2.0 (the
+ *   "License"); you may not use this file except in compliance
+ *   with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
+package org.apache.directory.shared.ldap.codec.protocol.mina;
+
+
+import org.apache.directory.shared.ldap.codec.api.LdapCodecServiceFactory;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+
+/**
+ * The {@link org.osgi.framework.BundleActivator} for the codec.
+ *
+ * @author <a href="mailto:dev@directory.apache.org">Apache Directory Project</a>
+ */
+@SuppressWarnings({"UnusedDeclaration"})
+public class DefaultActivator implements BundleActivator
+{
+    private LdapProtocolCodecFactory factory;
+    private ServiceRegistration registration;
+
+
+    @SuppressWarnings({"UnusedDeclaration"})
+    public DefaultActivator()
+    {
+        this.factory = new LdapProtocolCodecFactory();
+    }
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void start( BundleContext bundleContext ) throws Exception
+    {
+        registration = bundleContext.registerService( LdapProtocolCodecFactory.class.getName(), factory, null );
+        LdapCodecServiceFactory.getSingleton().registerProtocolCodecFactory( factory );
+    }
+    
+
+    /**
+     * {@inheritDoc}
+     */
+    public void stop( BundleContext bundleContext ) throws Exception
+    {
+        registration.unregister();
+    }
+}
