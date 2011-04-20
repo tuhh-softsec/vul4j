b59704ace158008d420fc659d10dfad5dc69471d
Colm O hEigeartaigh <coheigea@apache.org>
2011-04-20 09:56:37 +0000

    Reverting previous accidental commit to DOMSignedInfo.
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-java/trunk@1095344 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/jcp/xml/dsig/internal/dom/DOMSignedInfo.java
+++ src/main/java/org/jcp/xml/dsig/internal/dom/DOMSignedInfo.java
@@ -175,13 +175,7 @@ public final class DOMSignedInfo extends DOMStructure implements SignedInfo {
             // Impossible
         }
 
-        boolean excludeComments = true;
-        String c14nAlgorithm = canonicalizationMethod.getAlgorithm();
-        if (CanonicalizationMethod.EXCLUSIVE_WITH_COMMENTS.equals(c14nAlgorithm)
-            || CanonicalizationMethod.INCLUSIVE_WITH_COMMENTS.equals(c14nAlgorithm)) {
-            excludeComments = false;
-        }
-        DOMSubTreeData subTree = new DOMSubTreeData(localSiElem, excludeComments);
+        DOMSubTreeData subTree = new DOMSubTreeData(localSiElem, true);
 
         try {
             ((DOMCanonicalizationMethod) 
