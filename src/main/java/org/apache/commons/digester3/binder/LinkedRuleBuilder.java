f35446928567c3c4d2a28a1e513f88e78ea35fff
Simone Tripodi <simonetripodi@apache.org>
2011-05-15 14:25:50 +0000

    restored the PathCallParamBuilder class PathCallParamBuilder plugged in the Digester EDSL
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1103392 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/binder/LinkedRuleBuilder.java
+++ src/main/java/org/apache/commons/digester3/binder/LinkedRuleBuilder.java
@@ -82,6 +82,13 @@ public final class LinkedRuleBuilder
     }
 
     /**
+     * Construct a "call parameter" rule that will save the body text of this element as the parameter value.
+     */
+    public PathCallParamBuilder callParamPath() {
+        return this.addProvider(new PathCallParamBuilder(this.keyPattern, this.namespaceURI, this.mainBinder, this));
+    }
+
+    /**
      * Uses an {@link ObjectCreationFactory} to create a new object which it pushes onto the object stack.
      *
      * When the element is complete, the object will be popped.
