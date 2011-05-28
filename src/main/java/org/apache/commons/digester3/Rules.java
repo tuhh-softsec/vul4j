932ffa70fd59e4ef56ff217e3e1005979c1b1354
Simone Tripodi <simonetripodi@apache.org>
2011-05-28 15:32:10 +0000

    fixed checkstyle violation: Redundant 'public' modifier.
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1128673 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/Rules.java
+++ src/main/java/org/apache/commons/digester3/Rules.java
@@ -34,19 +34,19 @@ public interface Rules
     /**
      * Return the Digester instance with which this Rules instance is associated.
      */
-    public Digester getDigester();
+    Digester getDigester();
 
     /**
      * Set the Digester instance with which this Rules instance is associated.
      * 
      * @param digester The newly associated Digester instance
      */
-    public void setDigester( Digester digester );
+    void setDigester( Digester digester );
 
     /**
      * Return the namespace URI that will be applied to all subsequently added <code>Rule</code> objects.
      */
-    public String getNamespaceURI();
+    String getNamespaceURI();
 
     /**
      * Set the namespace URI that will be applied to all subsequently added <code>Rule</code> objects.
@@ -54,7 +54,7 @@ public interface Rules
      * @param namespaceURI Namespace URI that must match on all subsequently added rules, or <code>null</code> for
      *            matching regardless of the current namespace URI
      */
-    public void setNamespaceURI( String namespaceURI );
+    void setNamespaceURI( String namespaceURI );
 
     // --------------------------------------------------------- Public Methods
 
@@ -64,12 +64,12 @@ public interface Rules
      * @param pattern Nesting pattern to be matched for this Rule
      * @param rule Rule instance to be registered
      */
-    public void add( String pattern, Rule rule );
+    void add( String pattern, Rule rule );
 
     /**
      * Clear all existing Rule instance registrations.
      */
-    public void clear();
+    void clear();
 
     /**
      * Return a List of all registered Rule instances that match the specified nesting pattern, or a zero-length List if
@@ -80,13 +80,13 @@ public interface Rules
      *            namespace URI
      * @param pattern Nesting pattern to be matched
      */
-    public List<Rule> match( String namespaceURI, String pattern );
+    List<Rule> match( String namespaceURI, String pattern );
 
     /**
      * Return a List of all registered Rule instances, or a zero-length List if there are no registered Rule instances.
      * If more than one Rule instance has been registered, they <strong>must</strong> be returned in the order
      * originally registered through the <code>add()</code> method.
      */
-    public List<Rule> rules();
+    List<Rule> rules();
 
 }
