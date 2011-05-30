352ecebcd97ba7f24033813e9a5f8e14cc074e4e
Simone Tripodi <simonetripodi@apache.org>
2011-05-30 07:19:36 +0000

    fixed checkstyle violations '(' is not followed by whitespace. ')' is not preceded with whitespace.
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1129020 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/annotations/handlers/FactoryCreateHandler.java
+++ src/main/java/org/apache/commons/digester3/annotations/handlers/FactoryCreateHandler.java
@@ -38,11 +38,11 @@ public final class FactoryCreateHandler
      */
     public void handle( FactoryCreate annotation, Class<?> element, RulesBinder rulesBinder )
     {
-        FactoryCreateBuilder builder = rulesBinder.forPattern(annotation.pattern())
-            .withNamespaceURI(annotation.namespaceURI())
+        FactoryCreateBuilder builder = rulesBinder.forPattern( annotation.pattern() )
+            .withNamespaceURI( annotation.namespaceURI() )
             .factoryCreate()
-            .overriddenByAttribute(annotation.attributeName().length() > 0 ? annotation.attributeName() : null)
-            .ignoreCreateExceptions(annotation.ignoreCreateExceptions());
+            .overriddenByAttribute( annotation.attributeName().length() > 0 ? annotation.attributeName() : null )
+            .ignoreCreateExceptions( annotation.ignoreCreateExceptions() );
 
         if ( FactoryCreate.DefaultObjectCreationFactory.class != annotation.factoryClass() )
         {
