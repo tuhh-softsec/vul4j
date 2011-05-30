b0930d5152e0961ac9c9d69e0dbb0ee83d95b0d4
Simone Tripodi <simonetripodi@apache.org>
2011-05-30 21:12:07 +0000

    added missing Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1129376 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/plugins/RuleFinder.java
+++ src/main/java/org/apache/commons/digester3/plugins/RuleFinder.java
@@ -56,9 +56,13 @@ public abstract class RuleFinder
      * The properties object holds any xml attributes the user may have specified on the plugin declaration in order to
      * indicate how to locate the plugin rules.
      * <p>
-     * 
-     * @throws PluginConfigurationException if the algorithm finds a source of rules, but there is something invalid
-     *             about that source.
+     *
+     * @param d The digester instance where locating plugin classes
+     * @param pluginClass The plugin Java class
+     * @param p The properties object that holds any xml attributes the user may have specified on the plugin
+     *          declaration in order to indicate how to locate the plugin rules.
+     * @throws PluginException if the algorithm finds a source of rules, but there is something invalid
+     *         about that source.
      */
     public abstract RuleLoader findLoader( Digester d, Class<?> pluginClass, Properties p )
         throws PluginException;
