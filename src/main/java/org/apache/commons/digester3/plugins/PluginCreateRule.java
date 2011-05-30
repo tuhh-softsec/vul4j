170323d999d66fbf6a2ef2e975c9c8a75c1a3c3a
Simone Tripodi <simonetripodi@apache.org>
2011-05-30 21:35:15 +0000

    fixed javadoc parameter
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1129384 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/plugins/PluginCreateRule.java
+++ src/main/java/org/apache/commons/digester3/plugins/PluginCreateRule.java
@@ -592,7 +592,7 @@ public class PluginCreateRule
      * It would be really nice if the Digester class provided a way for this functionality to just be invoked directly.
      *
      * @param rules The rules which {@link Rule#end(String, String)} method has to be fired
-     * @param namespace the namespace URI of the matching element, or an empty string if the parser is not namespace
+     * @param namespaceURI the namespace URI of the matching element, or an empty string if the parser is not namespace
      *            aware or the element has no namespace
      * @param name the local name if the parser is namespace aware, or just the element name otherwise
      * @throws Exception if any error occurs
