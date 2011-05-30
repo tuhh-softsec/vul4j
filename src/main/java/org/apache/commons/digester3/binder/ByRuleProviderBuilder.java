e40d1943feec49d18fbea1482d0ffe8d4175ac49
Simone Tripodi <simonetripodi@apache.org>
2011-05-30 08:20:32 +0000

    fixed checkstyle violation: Type Javadoc comment is missing an @param <R> tag.
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1129047 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/binder/ByRuleProviderBuilder.java
+++ src/main/java/org/apache/commons/digester3/binder/ByRuleProviderBuilder.java
@@ -25,6 +25,7 @@ import org.apache.commons.digester3.Rule;
  * Builder chained when invoking
  * {@link LinkedRuleBuilder#addRuleCreatedBy(org.apache.commons.digester3.binder.RuleProvider)}.
  *
+ * @param <R> The rule type will be returned by this builder
  * @since 3.0
  */
 public final class ByRuleProviderBuilder<R extends Rule>
