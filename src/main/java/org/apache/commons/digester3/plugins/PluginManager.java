32c64a649cb2d3399965ca55c90d3cc64599db40
Simone Tripodi <simonetripodi@apache.org>
2011-05-30 21:09:23 +0000

    fixed checkstyle violation: Expected an @return tag.
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1129374 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/plugins/PluginManager.java
+++ src/main/java/org/apache/commons/digester3/plugins/PluginManager.java
@@ -112,6 +112,7 @@ public class PluginManager
      * Return the declaration object with the specified class. If no such plugin is known, null is returned.
      *
      * @param className The {@link Declaration} class name
+     * @return The Declaration instance obtained by the input class name
      */
     public Declaration getDeclarationByClass( String className )
     {
