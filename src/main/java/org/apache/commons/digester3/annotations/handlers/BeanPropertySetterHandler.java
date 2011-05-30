cccdfa3ef1413f14f0ee94a236cbb0f3bbcb13fe
Simone Tripodi <simonetripodi@apache.org>
2011-05-30 07:16:54 +0000

    fixed checkstyle violation: ')' is not preceded with whitespace.
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/digester3/trunk@1129019 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/digester3/annotations/handlers/BeanPropertySetterHandler.java
+++ src/main/java/org/apache/commons/digester3/annotations/handlers/BeanPropertySetterHandler.java
@@ -42,7 +42,7 @@ public final class BeanPropertySetterHandler
         rulesBinder.forPattern( annotation.pattern() )
             .withNamespaceURI( annotation.namespaceURI() )
             .setBeanProperty()
-            .withName( element.getName());
+            .withName( element.getName() );
     }
 
 }
