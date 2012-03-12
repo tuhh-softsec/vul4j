9e36608dfa53bcc901fdc9da58292f70d5767d15
Sylvain Sicard <sylvain.sicard@orange.com>
2012-03-12 08:29:08 +0000

    Make the tag replace "composable" http://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=82

--- esigate-taglib/src/main/java/org/esigate/taglib/ReplaceTag.java
+++ esigate-taglib/src/main/java/org/esigate/taglib/ReplaceTag.java
@@ -3,70 +3,81 @@ package org.esigate.taglib;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 import javax.servlet.jsp.tagext.Tag;
+
 import org.slf4j.LoggerFactory;
 
 /**
- * Only used inside a tag that implements IReplaceableTag. This tag add a replace rule to the parent tag, which will be applied on render.
+ * Only used as a descendant of a tag that implements IReplaceableTag. This tag
+ * add a replace rule to the first replaceable parent tag, which will be applied
+ * on render.
  * <hr>
  * 
  * @author Cedric Brandes, 27 juin 08
+ * @author Sylvain Sicard
  */
 public class ReplaceTag extends BodyTagSupport {
-    private static final long serialVersionUID = 1L;
-    private String expression;
+	private static final long serialVersionUID = 1L;
+	private String expression;
 
-    public String getExpression() {
-        return expression;
-    }
+	/**
+	 * Add a new replace rule to the first replaceable parent tag. The
+	 * expression to replace of the rule is the expression parameter, the new
+	 * value is the content of the body
+	 * 
+	 * @see javax.servlet.jsp.tagext.BodyTagSupport#doAfterBody()
+	 */
+	@Override
+	public int doAfterBody() throws JspException {
+		ReplaceableTag parent = getFirstReplaceableParent();
+		if (parent == null) {
+			LoggerFactory.getLogger(ReplaceTag.class).warn(
+					"No replaceable parent found, skipping tag");
+			return Tag.SKIP_BODY;
+		}
+		if (expression == null) {
+			expression = "";
+		}
+		String newExpression = getBodyContent().getString();
+		parent.getReplaceRules().put(expression, newExpression);
+		return Tag.SKIP_BODY;
+	}
 
-    /**
-     * Sets the expression to replace.
-     * 
-     * @param expression
-     *            Regular expression to be replaced by the tag body
-     */
-    public void setExpression(String expression) {
-        this.expression = expression;
-    }
+	@Override
+	public int doEndTag() throws JspException {
+		if (getBodyContent() == null) {
+			ReplaceableTag parent = getFirstReplaceableParent();
+			if (parent == null) {
+				LoggerFactory.getLogger(ReplaceTag.class).warn(
+						"No replaceable parent found, skipping tag");
+				return Tag.SKIP_BODY;
+			}
+			parent.getReplaceRules().put(expression, "");
+		}
+		return Tag.SKIP_BODY;
+	}
 
-    /**
-     * Add a new replace rule to the parent tag. The expression to replace of the rule is the expression parameter, the new value is the content of the body
-     * 
-     * @see javax.servlet.jsp.tagext.BodyTagSupport#doAfterBody()
-     */
-    @Override
-    public int doAfterBody() throws JspException {
-        ReplaceableTag parent = getFirstAvailableParent();
-        if (parent == null) {
-            LoggerFactory.getLogger(ReplaceTag.class).warn("No parent found, skipping tag");
-            return Tag.SKIP_BODY;
-        }
-        if (expression == null)
-            expression = "";
-        String newExpression = getBodyContent().getString();
-        parent.getReplaceRules().put(expression, newExpression);
-        return Tag.SKIP_BODY;
-    }
+	public String getExpression() {
+		return expression;
+	}
 
-    @Override
-    public int doEndTag() throws JspException {
-        if (getBodyContent() == null) {
-            ReplaceableTag parent = getFirstAvailableParent();
-            if (parent == null) {
-                LoggerFactory.getLogger(ReplaceTag.class).warn("No parent found, skipping tag");
-                return Tag.SKIP_BODY;
-            }
-            parent.getReplaceRules().put(expression, "");
-        }
-        return Tag.SKIP_BODY;
-    }
+	protected ReplaceableTag getFirstReplaceableParent() {
+		Tag tag = getParent();
+		while (!(tag instanceof ReplaceableTag)) {
+			if (tag == null) {
+				break;
+			}
+			tag = tag.getParent();
+		}
+		return (ReplaceableTag) tag;
+	}
 
-    private ReplaceableTag getFirstAvailableParent() {
-        ReplaceableTag parent = null;
-        if (getParent() instanceof ReplaceableTag)
-            parent = (ReplaceableTag) getParent();
-        else if (getParent() instanceof IfTag && getParent().getParent() instanceof ReplaceableTag)
-            parent = (ReplaceableTag) getParent().getParent();
-        return parent;
-    }
+	/**
+	 * Sets the expression to replace.
+	 * 
+	 * @param expression
+	 *            Regular expression to be replaced by the tag body
+	 */
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
 }
