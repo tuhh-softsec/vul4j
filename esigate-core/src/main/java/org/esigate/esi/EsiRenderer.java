3861bd6048f6efb7fb6d70f95de8788d4303c56e
Aurelien Loth <aurelien.loth@gmail.com>
2012-11-08 14:59:25 +0000

    Adding support for fragment not found in esi:try

--- esigate-core/src/main/java/org/esigate/esi/EsiRenderer.java
+++ esigate-core/src/main/java/org/esigate/esi/EsiRenderer.java
@@ -1,16 +1,15 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package org.esigate.esi;
@@ -37,91 +36,111 @@ import org.slf4j.LoggerFactory;
  * @author Francois-Xavier Bonnet
  */
 public class EsiRenderer implements Renderer, Appendable {
-	private final static Logger LOG = LoggerFactory.getLogger(EsiRenderer.class);
-	private final static Pattern PATTERN = Pattern.compile("(<esi:[^>]*>)|(</esi:[^>]*>)|(<!--esi)|(-->)");
 
-	private final Parser parser = new Parser(PATTERN, IncludeElement.TYPE, Comment.TYPE, CommentElement.TYPE, RemoveElement.TYPE, VarsElement.TYPE, ChooseElement.TYPE, WhenElement.TYPE,
-			OtherwiseElement.TYPE, TryElement.TYPE, AttemptElement.TYPE, ExceptElement.TYPE, InlineElement.TYPE, ReplaceElement.TYPE, FragmentElement.TYPE);
-	private Writer out;
-	private Map<String, CharSequence> fragmentsToReplace;
-	private final String page;
-	private final String name;
-	private boolean write = true;
+    private final static Logger LOG = LoggerFactory.getLogger(EsiRenderer.class);
 
-	public String getName() {
-		return name;
-	}
+    private final static Pattern PATTERN = Pattern.compile("(<esi:[^>]*>)|(</esi:[^>]*>)|(<!--esi)|(-->)");
 
-	public void setWrite(boolean write) {
-		this.write = write;
-	}
+    private final Parser parser = new Parser(PATTERN, IncludeElement.TYPE, Comment.TYPE, CommentElement.TYPE,
+        RemoveElement.TYPE, VarsElement.TYPE, ChooseElement.TYPE, WhenElement.TYPE,
+        OtherwiseElement.TYPE, TryElement.TYPE, AttemptElement.TYPE, ExceptElement.TYPE, InlineElement.TYPE,
+        ReplaceElement.TYPE, FragmentElement.TYPE);
 
-	/**
-	 * Constructor used to render a complete page
-	 */
-	public EsiRenderer() {
-		page = null;
-		name = null;
-	}
+    private Writer out;
 
-	/**
-	 * Constructor used to render a fragment Retrieves a fragment inside a page.<br />
-	 * 
-	 * Extracts html between <code>&lt;esi:fragment name="myFragment"&gt;</code>
-	 * and <code>&lt;/esi:fragment&gt;</code>
-	 * 
-	 * @param page
-	 * @param name
-	 */
-	public EsiRenderer(String page, String name) {
-		this.page = page;
-		this.name = name;
-		write = false;
-	}
+    private Map<String, CharSequence> fragmentsToReplace;
 
-	public Map<String, CharSequence> getFragmentsToReplace() {
-		return fragmentsToReplace;
-	}
+    private final String page;
 
-	public void setFragmentsToReplace(Map<String, CharSequence> fragmentsToReplace) {
-		this.fragmentsToReplace = fragmentsToReplace;
-	}
+    private final String name;
 
-	/** {@inheritDoc} */
-	public void render(HttpRequest originalRequest, String content, Writer out) throws IOException, HttpErrorPage {
-		if (name != null)
-			LOG.debug("Rendering fragment " + name + " in page " + page);
-		this.out = out;
-		if (content == null) {
-			return;
-		}
-		parser.setHttpRequest(originalRequest);
-		parser.parse(content, this);
-	}
+    private boolean write = true;
 
-	public Appendable append(CharSequence csq) throws IOException {
-		if (write) {
-			out.append(csq);
-		}
-		return this;
-	}
+    private boolean found = false;
 
-	public Appendable append(char c) throws IOException {
-		if (write) {
-			out.append(c);
-		}
-		return this;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Appendable append(CharSequence csq, int start, int end) throws IOException {
-		if (write) {
-			out.append(csq, start, end);
-		}
-		return this;
-	}
+    public void setWrite(boolean write) {
+        this.write = write;
+    }
 
-	public boolean isWrite() {
-		return this.write;
-	}
+    /**
+     * Constructor used to render a complete page
+     */
+    public EsiRenderer() {
+        page = null;
+        name = null;
+    }
+
+    /**
+     * Constructor used to render a fragment Retrieves a fragment inside a page.<br />
+     * 
+     * Extracts html between <code>&lt;esi:fragment name="myFragment"&gt;</code>
+     * and <code>&lt;/esi:fragment&gt;</code>
+     * 
+     * @param page
+     * @param name
+     */
+    public EsiRenderer(String page, String name) {
+        this.page = page;
+        this.name = name;
+        write = false;
+    }
+
+    public Map<String, CharSequence> getFragmentsToReplace() {
+        return fragmentsToReplace;
+    }
+
+    public void setFragmentsToReplace(Map<String, CharSequence> fragmentsToReplace) {
+        this.fragmentsToReplace = fragmentsToReplace;
+    }
+
+    /** {@inheritDoc} */
+    public void render(HttpRequest originalRequest, String content, Writer out) throws IOException, HttpErrorPage {
+        if (name != null) {
+            LOG.debug("Rendering fragment " + name + " in page " + page);
+        }
+        this.out = out;
+        if (content == null) {
+            return;
+        }
+        parser.setHttpRequest(originalRequest);
+        parser.parse(content, this);
+        if (name != null && this.found == false) {
+            throw new HttpErrorPage(502, "Fragment " + name + " not found", "Fragment " + name + " not found");
+        }
+    }
+
+    public Appendable append(CharSequence csq) throws IOException {
+        if (write) {
+            out.append(csq);
+        }
+        return this;
+    }
+
+    public Appendable append(char c) throws IOException {
+        if (write) {
+            out.append(c);
+        }
+        return this;
+    }
+
+    public Appendable append(CharSequence csq, int start, int end) throws IOException {
+        if (write) {
+            out.append(csq, start, end);
+        }
+        return this;
+    }
+
+    public boolean isWrite() {
+        return this.write;
+    }
+
+    public void setFound(boolean found) {
+        this.found = found;
+
+    }
 
 }
--- esigate-core/src/test/java/org/esigate/esi/TryElementTest.java
+++ esigate-core/src/test/java/org/esigate/esi/TryElementTest.java
@@ -1,16 +1,15 @@
-/* 
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package org.esigate.esi;
@@ -27,71 +26,148 @@ import org.esigate.test.MockHttpResponse;
 
 public class TryElementTest extends TestCase {
 
-	private EsiRenderer tested;
-	private MockHttpRequest request;
+    private EsiRenderer tested;
 
-	@Override
-	protected void setUp() throws IOException, HttpErrorPage {
-		MockDriver provider = new MockDriver("mock");
-		provider.addResource("/test", "test");
-		provider.addResource("http://www.foo.com/test", "test");
-		request = new MockHttpRequest();
-		provider.initHttpRequestParams(request, new MockHttpResponse(), null);
-		tested = new EsiRenderer();
-	}
+    private MockHttpRequest request;
 
-	public void testTry() throws IOException, HttpErrorPage {
-		String page = "begin <esi:try>" + "<esi:attempt><esi:include src=\"http://www.foo.com/test\" /></esi:attempt>" + "</esi:try> end";
-		StringWriter out = new StringWriter();
-		tested.render(request, page, out);
-		assertEquals("begin test end", out.toString());
-	}
+    @Override
+    protected void setUp() throws IOException, HttpErrorPage {
+        MockDriver provider = new MockDriver("mock");
+        provider.addResource("/test", "test");
+        provider.addResource("http://www.foo.com/test", "test");
+        provider.addResource("http://www.foo.com/testFragment", "before fragment <esi:fragment name=\"fragmentFound\">FRAGMENT FOUND</esi:fragment> after fragment");
+        provider.addResource("http://www.foo.com/testWithoutFragment", "no fragment here");
+        request = new MockHttpRequest();
+        provider.initHttpRequestParams(request, new MockHttpResponse(), null);
+        tested = new EsiRenderer();
+    }
 
-	public void testAttempt1() throws IOException, HttpErrorPage {
-		String page = "begin <esi:try>" + "<esi:attempt>abc <esi:include src=\"http://www.foo.com/test\" /> cba</esi:attempt>" + "<esi:except>inside except</esi:except>" + "</esi:try> end";
-		StringWriter out = new StringWriter();
-		tested.render(request, page, out);
-		assertEquals("begin abc test cba end", out.toString());
-	}
+    public void testTry() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>" + "<esi:attempt><esi:include src=\"http://www.foo.com/test\" /></esi:attempt>"
+                + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin test end", out.toString());
+    }
 
-	public void testAttempt2() throws IOException, HttpErrorPage {
-		String page = "begin <esi:try>" + "<esi:attempt>abc " + "<esi:include src=\"http://www.foo.com/test\" />" + "<esi:include src='http://www.foo.com/not-found' onerror='continue' />"
-				+ " cba</esi:attempt>" + "<esi:except>inside except</esi:except>" + "</esi:try> end";
-		StringWriter out = new StringWriter();
-		tested.render(request, page, out);
-		assertEquals("begin abc test cba end", out.toString());
-	}
+    public void testAttempt1() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>" + "<esi:attempt>abc <esi:include src=\"http://www.foo.com/test\" /> cba</esi:attempt>"
+                + "<esi:except>inside except</esi:except>" + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin abc test cba end", out.toString());
+    }
 
-	public void testExcept1() throws IOException, HttpErrorPage {
-		String page = "begin <esi:try>" + "<esi:attempt>abc <esi:include src=\"http://www.foo2.com/test\" /> cba</esi:attempt>" + "<esi:except>inside except</esi:except>" + "</esi:try> end";
-		StringWriter out = new StringWriter();
-		tested.render(request, page, out);
-		assertEquals("begin inside except end", out.toString());
-	}
+    public void testAttempt2() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>" + "<esi:attempt>abc " + "<esi:include src=\"http://www.foo.com/test\" />"
+                + "<esi:include src='http://www.foo.com/not-found' onerror='continue' />"
+                + " cba</esi:attempt>" + "<esi:except>inside except</esi:except>" + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin abc test cba end", out.toString());
+    }
 
-	public void testExcept2() throws IOException, HttpErrorPage {
-		String page = "begin <esi:try>" + "<esi:attempt> " + "<esi:include src='http://www.foo.com/test' /> abc <esi:include src=\"http://www.foo2.com/test\" /> cba" + "</esi:attempt>"
-				+ "<esi:except>inside except</esi:except>" + "</esi:try> end";
-		StringWriter out = new StringWriter();
-		tested.render(request, page, out);
-		assertEquals("begin inside except end", out.toString());
-	}
+    public void testExcept1() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>" + "<esi:attempt>abc <esi:include src=\"http://www.foo2.com/test\" /> cba</esi:attempt>"
+                + "<esi:except>inside except</esi:except>" + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin inside except end", out.toString());
+    }
 
-	public void testMultipleExcept() throws IOException, HttpErrorPage {
-		String page = "begin <esi:try>" + "<esi:attempt> " + "<esi:attempt>abc <esi:include src='http://www.foo2.com/test' /> cba</esi:attempt>" + "</esi:attempt>"
-				+ "<esi:except code='500'>inside incorrect except</esi:except>" + "<esi:except code='404'>inside correct except</esi:except>"
-				+ "<esi:except code='412'>inside incorrect except</esi:except>" + "<esi:except>inside default except</esi:except>" + "</esi:try> end";
-		StringWriter out = new StringWriter();
-		tested.render(request, page, out);
-		assertEquals("begin inside correct except end", out.toString());
-	}
+    public void testExcept2() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>"
+                + "<esi:attempt> "
+                + "<esi:include src='http://www.foo.com/test' /> abc <esi:include src=\"http://www.foo2.com/test\" /> cba"
+                + "</esi:attempt>"
+                + "<esi:except>inside except</esi:except>" + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin inside except end", out.toString());
+    }
 
-	public void testDefaultExcept() throws IOException, HttpErrorPage {
-		String page = "begin <esi:try>" + "<esi:attempt> " + "<esi:attempt>abc <esi:include src='http://www.foo2.com/test' /> cba</esi:attempt>" + "</esi:attempt>"
-				+ "<esi:except code='500'>inside incorrect except</esi:except>" + "<esi:except code='412'>inside incorrect except</esi:except>" + "<esi:except>inside default except</esi:except>"
-				+ "</esi:try> end";
-		StringWriter out = new StringWriter();
-		tested.render(request, page, out);
-		assertEquals("begin inside default except end", out.toString());
-	}
+    public void testMultipleExcept() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>" + "<esi:attempt> "
+                + "<esi:attempt>abc <esi:include src='http://www.foo2.com/test' /> cba</esi:attempt>"
+                + "</esi:attempt>"
+                + "<esi:except code='500'>inside incorrect except</esi:except>"
+                + "<esi:except code='404'>inside correct except</esi:except>"
+                + "<esi:except code='412'>inside incorrect except</esi:except>"
+                + "<esi:except>inside default except</esi:except>" + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin inside correct except end", out.toString());
+    }
+
+    public void testDefaultExcept() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>" + "<esi:attempt> "
+                + "<esi:attempt>abc <esi:include src='http://www.foo2.com/test' /> cba</esi:attempt>"
+                + "</esi:attempt>"
+                + "<esi:except code='500'>inside incorrect except</esi:except>"
+                + "<esi:except code='412'>inside incorrect except</esi:except>"
+                + "<esi:except>inside default except</esi:except>"
+                + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin inside default except end", out.toString());
+    }
+
+    public void testTryCatchFragmentNotFound() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>"
+                + "<esi:attempt> "
+                + "<esi:attempt>abc <esi:include src='http://www.foo2.com/test' fragment='fragmentNotFound'/> cba</esi:attempt>"
+                + "</esi:attempt>"
+                + "<esi:except>NOT FOUND</esi:except>"
+                + "</esi:try> end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin NOT FOUND end", out.toString());
+    }
+
+    public void testTryFragmentNotFound() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>"
+                + "<esi:attempt> "
+                + "<esi:attempt>abc<esi:include src='http://www.foo.com/testFragment' fragment='fragmentFound'/> cba</esi:attempt>"
+                + "</esi:attempt>"
+                + "</esi:try>end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin  abcFRAGMENT FOUND cbaend", out.toString());
+    }
+    
+    public void testTryFragmentNotFound2() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>"
+                + "<esi:attempt> "
+                + "<esi:attempt>abc<esi:include src='http://www.foo.com/testWithoutFragment' fragment='fragmentFound'/> cba</esi:attempt>"
+                + "</esi:attempt>"
+                + "</esi:try>end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin end", out.toString());
+    }
+    
+    public void testTryCatchFragmentNotFound2() throws IOException, HttpErrorPage {
+        String page =
+            "begin <esi:try>"
+                + "<esi:attempt> "
+                + "<esi:attempt>abc<esi:include src='http://www.foo.com/testWithoutFragment' fragment='fragmentFound'/> cba</esi:attempt>"
+                + "</esi:attempt>"
+                + "<esi:except>NOT FOUND</esi:except>"
+                + "</esi:try>end";
+        StringWriter out = new StringWriter();
+        tested.render(request, page, out);
+        assertEquals("begin NOT FOUNDend", out.toString());
+    }
+    
+    
 }
