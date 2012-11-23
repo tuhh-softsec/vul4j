1d7bed90289372032f6e1a9d6a42f87da581e9b7
Nicolas Richeton <nicolas.richeton@gmail.com>
2012-11-23 17:05:46 +0000

    Log fix

--- esigate-core/src/main/java/org/esigate/esi/EsiRenderer.java
+++ esigate-core/src/main/java/org/esigate/esi/EsiRenderer.java
@@ -37,9 +37,9 @@ import org.slf4j.LoggerFactory;
  */
 public class EsiRenderer implements Renderer, Appendable {
 
-	private final static Logger LOG = LoggerFactory.getLogger(EsiRenderer.class);
+    private final static Logger LOG = LoggerFactory.getLogger(EsiRenderer.class);
 
-	private final static Pattern PATTERN = Pattern.compile("(<esi:[^>]*>)|(</esi:[^>]*>)");
+ 	private final static Pattern PATTERN = Pattern.compile("(<esi:[^>]*>)|(</esi:[^>]*>)");
 	private final static Pattern PATTERN_COMMENTS = Pattern.compile("(<!--esi)|(-->)");
 
 	private final Parser parser = new Parser(PATTERN, IncludeElement.TYPE, CommentElement.TYPE, RemoveElement.TYPE, VarsElement.TYPE, ChooseElement.TYPE, WhenElement.TYPE, OtherwiseElement.TYPE,
@@ -47,109 +47,109 @@ public class EsiRenderer implements Renderer, Appendable {
 
 	private final Parser parserComments = new Parser(PATTERN_COMMENTS, Comment.TYPE);
 
-	private Writer out;
+    private Writer out;
 
-	private Map<String, CharSequence> fragmentsToReplace;
+    private Map<String, CharSequence> fragmentsToReplace;
 
-	private final String page;
+    private final String page;
 
-	private final String name;
+    private final String name;
 
-	private boolean write = true;
+    private boolean write = true;
 
-	private boolean found = false;
+    private boolean found = false;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setWrite(boolean write) {
-		this.write = write;
-	}
+    public void setWrite(boolean write) {
+        this.write = write;
+    }
 
-	/**
-	 * Constructor used to render a complete page
-	 */
-	public EsiRenderer() {
-		page = null;
-		name = null;
-	}
+    /**
+     * Constructor used to render a complete page
+     */
+    public EsiRenderer() {
+        page = null;
+        name = null;
+    }
 
-	/**
-	 * Constructor used to render a fragment Retrieves a fragment inside a page.<br />
-	 * 
-	 * Extracts html between <code>&lt;esi:fragment name="myFragment"&gt;</code>
-	 * and <code>&lt;/esi:fragment&gt;</code>
-	 * 
-	 * @param page
-	 * @param name
-	 */
-	public EsiRenderer(String page, String name) {
-		this.page = page;
-		this.name = name;
-		write = false;
-	}
+    /**
+     * Constructor used to render a fragment Retrieves a fragment inside a page.<br />
+     * 
+     * Extracts html between <code>&lt;esi:fragment name="myFragment"&gt;</code>
+     * and <code>&lt;/esi:fragment&gt;</code>
+     * 
+     * @param page
+     * @param name
+     */
+    public EsiRenderer(String page, String name) {
+        this.page = page;
+        this.name = name;
+        write = false;
+    }
 
-	public Map<String, CharSequence> getFragmentsToReplace() {
-		return fragmentsToReplace;
-	}
+    public Map<String, CharSequence> getFragmentsToReplace() {
+        return fragmentsToReplace;
+    }
 
-	public void setFragmentsToReplace(Map<String, CharSequence> fragmentsToReplace) {
-		this.fragmentsToReplace = fragmentsToReplace;
-	}
+    public void setFragmentsToReplace(Map<String, CharSequence> fragmentsToReplace) {
+        this.fragmentsToReplace = fragmentsToReplace;
+    }
 
-	/** {@inheritDoc} */
-	public void render(HttpRequest originalRequest, String content, Writer out) throws IOException, HttpErrorPage {
-		if (name != null) {
-			LOG.debug("Rendering fragment " + name + " in page " + page);
-		}
-		this.out = out;
-		if (content == null) {
-			return;
-		}
+    /** {@inheritDoc} */
+    public void render(HttpRequest originalRequest, String content, Writer out) throws IOException, HttpErrorPage {
+        if (name != null) {
+            LOG.debug("Rendering fragment {} in page {}", name, page);
+        }
+        this.out = out;
+        if (content == null) {
+            return;
+        }
+        
+        // Pass 1. Remove esi comments
+        StringBuilder contentWithoutComments = new StringBuilder();
+        parserComments.setHttpRequest(originalRequest);
+        parserComments.parse(content, contentWithoutComments);
 
-		// Pass 1. Remove esi comments
-		StringBuilder contentWithoutComments = new StringBuilder();
-		parserComments.setHttpRequest(originalRequest);
-		parserComments.parse(content, contentWithoutComments);
+        // Pass 2. Process ESI
+        parser.setHttpRequest(originalRequest);
+        parser.parse(contentWithoutComments, this);
+        
+        if (name != null && this.found == false) {
+            throw new HttpErrorPage(502, "Fragment " + name + " not found", "Fragment " + name + " not found");
+        }
+    }
 
-		// Pass 2. Process ESI
-		parser.setHttpRequest(originalRequest);
-		parser.parse(contentWithoutComments, this);
+    public Appendable append(CharSequence csq) throws IOException {
+        if (write) {
+            out.append(csq);
+        }
+        return this;
+    }
 
-		if (name != null && this.found == false) {
-			throw new HttpErrorPage(502, "Fragment " + name + " not found", "Fragment " + name + " not found");
-		}
-	}
+    public Appendable append(char c) throws IOException {
+        if (write) {
+            out.append(c);
+        }
+        return this;
+    }
 
-	public Appendable append(CharSequence csq) throws IOException {
-		if (write) {
-			out.append(csq);
-		}
-		return this;
-	}
+    public Appendable append(CharSequence csq, int start, int end) throws IOException {
+        if (write) {
+            out.append(csq, start, end);
+        }
+        return this;
+    }
 
-	public Appendable append(char c) throws IOException {
-		if (write) {
-			out.append(c);
-		}
-		return this;
-	}
+    public boolean isWrite() {
+        return this.write;
+    }
 
-	public Appendable append(CharSequence csq, int start, int end) throws IOException {
-		if (write) {
-			out.append(csq, start, end);
-		}
-		return this;
-	}
+    public void setFound(boolean found) {
+        this.found = found;
 
-	public boolean isWrite() {
-		return this.write;
-	}
-
-	public void setFound(boolean found) {
-		this.found = found;
-
-	}
+    }
 
 }
