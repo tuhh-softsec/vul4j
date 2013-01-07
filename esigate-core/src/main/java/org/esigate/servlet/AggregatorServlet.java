57a3c98815b33a7c5373d294f89c377ed467a4da
Nicolas Richeton <nicolas.richeton@gmail.com>
2013-01-07 17:46:25 +0000

    0000117: VirtualProvider (as in VirtualHost) : select provider according to host name https://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=117

--- esigate-core/src/main/java/org/esigate/servlet/AggregatorServlet.java
+++ esigate-core/src/main/java/org/esigate/servlet/AggregatorServlet.java
@@ -16,6 +16,9 @@
 package org.esigate.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -23,6 +26,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+import org.esigate.Driver;
 import org.esigate.DriverFactory;
 import org.esigate.HttpErrorPage;
 import org.esigate.aggregator.AggregateRenderer;
@@ -32,13 +37,68 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Servlet used to proxy requests from a remote application.
+ * <p>
+ * Parameters are :
+ * <ul>
+ * <li>provider (optional): single provider name</li>
+ * <li>providers (optional): comma-separated list of provider mappings based on
+ * host requested. Format is: host1=provider,host2=provider2</li>
+ * </ul>
  * 
  * @author Francois-Xavier Bonnet
+ * @author Nicols Richeton
  */
 public class AggregatorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = LoggerFactory.getLogger(AggregatorServlet.class);
-	private String provider;
+	private String provider = null;
+	private Map<String, String> providerMappings = null ;
+	
+	/**
+	 * Return current provider mappings, parsed from the "providers" init param.
+	 * 
+	 * @return map or null if mappings are not used.
+	 */
+	public Map<String, String> getProviderMappings() {
+		return providerMappings;
+	}
+
+	/**
+	 * Get the provider to use based on "name"
+	 * <p>
+	 * 
+	 * This methods allows overriding in unit test
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public Driver getDriver(String name) {
+		return DriverFactory.getInstance(name);
+	}
+	
+	/**
+	 * Select the provider for this request.
+	 * <p>
+	 * Perform selection based on the Host header.
+	 * 
+	 * @param request
+	 * @return provider name or null.
+	 */
+	public String selectProvider(HttpServletRequest request) {
+		// Select provider. null is valid (default)
+		String targetProvider = provider;
+		if (providerMappings != null) {
+			String host = request.getHeader("Host");
+			if (host != null) {
+				host = host.toLowerCase(Locale.ENGLISH);
+				String mapping = providerMappings.get(host);
+				if (mapping != null)
+					targetProvider = mapping;
+			}
+		}
+
+		return targetProvider;
+	}
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -48,9 +108,14 @@ public class AggregatorServlet extends HttpServlet {
 			relUrl = relUrl.substring(request.getServletPath().length());
 		}
 		LOG.debug("Aggregating {}", relUrl);
+		
+		// Select provider. null is valid (default)
+		String targetProvider = selectProvider( request );
+		
+		// Process ressource
 		HttpServletMediator mediator = new HttpServletMediator(request, response, getServletContext());
 		try {
-			DriverFactory.getInstance(provider).proxy(relUrl, mediator.getHttpRequest(), new AggregateRenderer(), new EsiRenderer());
+			getDriver(targetProvider).proxy(relUrl, mediator.getHttpRequest(), new AggregateRenderer(), new EsiRenderer());
 		} catch (HttpErrorPage e) {
 			mediator.sendResponse(e.getHttpResponse());
 		}
@@ -60,5 +125,19 @@ public class AggregatorServlet extends HttpServlet {
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 		provider = config.getInitParameter("provider");
+		
+		// Load mappings
+		String providersString = config.getInitParameter("providers");
+		if (providersString != null) {
+			providerMappings = new HashMap<String, String>();
+			String[] providersArray = StringUtils.split(providersString, ",");
+			for (String p : providersArray) {
+				String[] mapping = StringUtils.split(p, "=");
+				providerMappings.put(StringUtils.trim(mapping[0]
+						.toLowerCase(Locale.ENGLISH)), StringUtils
+						.trim(mapping[1]));
+			}
+		}
+	
 	}
 }
---
+++ esigate-core/src/test/java/org/esigate/servlet/AggregatorServletTest.java
@@ -0,0 +1,91 @@
+package org.esigate.servlet;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class AggregatorServletTest {
+
+	protected class TestServletConfig implements ServletConfig {
+		public String getServletName() {
+			return "aggregator";
+		}
+
+		public ServletContext getServletContext() {
+			return null;
+		}
+
+		public Enumeration getInitParameterNames() {
+			return null;
+		}
+
+		public String getInitParameter(String name) {
+			if (name.equals("provider"))
+				return "single";
+
+			if (name.equals("providers"))
+				return "suB.domaiN.com=provider1,sub2.domAin.com=provider2";
+
+			return null;
+		}
+	}
+
+	@Test
+	public void testConfig() throws ServletException {
+		AggregatorServlet servlet = new AggregatorServlet();
+		servlet.init(new TestServletConfig());
+		Assert.assertEquals("provider1",
+				servlet.getProviderMappings().get("sub.domain.com"));
+		Assert.assertEquals("provider2",
+				servlet.getProviderMappings().get("sub2.domain.com"));
+
+	}
+
+	@Test
+	public void testProviderSelection() throws ServletException, IOException {
+		AggregatorServlet servlet = new AggregatorServlet();
+		servlet.init(new TestServletConfig());
+
+		HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+		Mockito.when(request.getContextPath()).thenReturn("test");
+		Mockito.when(request.getRequestURI())
+				.thenReturn("test/servlet/request");
+		Mockito.when(request.getProtocol()).thenReturn("HTTP/1.1");
+		Mockito.when(request.getMethod()).thenReturn("GET");
+		Mockito.when(request.getServletPath()).thenReturn("servlet");
+
+		Mockito.when(request.getHeader("Host")).thenReturn("sub2.domain.com");
+		Assert.assertEquals("provider2", servlet.selectProvider(request));
+
+		request = Mockito.mock(HttpServletRequest.class);
+		Mockito.when(request.getContextPath()).thenReturn("test");
+		Mockito.when(request.getRequestURI())
+				.thenReturn("test/servlet/request");
+		Mockito.when(request.getProtocol()).thenReturn("HTTP/1.1");
+		Mockito.when(request.getMethod()).thenReturn("GET");
+		Mockito.when(request.getServletPath()).thenReturn("servlet");
+
+		Mockito.when(request.getHeader("Host")).thenReturn("sub.domain.com");
+		Assert.assertEquals("provider1", servlet.selectProvider(request));
+
+		request = Mockito.mock(HttpServletRequest.class);
+		Mockito.when(request.getContextPath()).thenReturn("test");
+		Mockito.when(request.getRequestURI())
+				.thenReturn("test/servlet/request");
+		Mockito.when(request.getProtocol()).thenReturn("HTTP/1.1");
+		Mockito.when(request.getMethod()).thenReturn("GET");
+		Mockito.when(request.getServletPath()).thenReturn("servlet");
+		Assert.assertEquals("single", servlet.selectProvider(request));
+
+	}
+
+}
