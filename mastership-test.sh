b5f189cf6411176e1bd47ae7b2f018eed4f51347
Jonathan Hart <jono@onlab.us>
2013-02-23 22:11:13 -0800

    Small change to the way I run ONOS for testing

--- mastership-test.sh
+++ mastership-test.sh
@@ -1,1 +1,2 @@
-java -Dlogback.configurationFile=logback.xml -cp target/floodlight-only.jar:lib/*:lib/titan/* net.onrc.onos.registry.controller.RegistryRunner $1
+#java -Dlogback.configurationFile=logback.xml -cp target/floodlight-only.jar:lib/*:lib/titan/* net.onrc.onos.registry.controller.RegistryRunner $1
+java -cp target/floodlight-only.jar:lib/*:lib/titan/* net.floodlightcontroller.core.Main -cf onos.properties
