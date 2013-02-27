79ac5058395979fca1153fb4732110f3eeb50cc0
Masayoshi Kobayashi <masayoshi@onlab.us>
2013-02-27 19:16:14 +0000

    Added script to manipulate controller setting for mininet switches

---
+++ ctrl-local.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+controller="localhost"
+switches=`ifconfig -a | grep sw |grep -v eth | awk '{print $1}'`
+
+function host2ip (){
+   ip=`grep $1 /etc/hosts |grep -v "ip6"|  awk '{print $1}'`
+   echo $ip
+}
+
+url=""
+for c in $controller; do
+  url="$url tcp:`host2ip $c`:6633"
+done
+echo $url
+for s in $switches; do
+    echo "set switch $s controller $url"
+    sudo ovs-vsctl set-controller $s $url
+done
