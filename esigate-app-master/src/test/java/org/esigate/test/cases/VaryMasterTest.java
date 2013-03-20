67068a19b929c0922649d9cb9cd52ced5b0c4592
Francois-Xavier <francois-xavier.bonnet@centraliens.net>
2013-03-20 04:15:58 +0000

    https://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=92 Vary:cookie not working
    Fixed in HttpClient 4.3-beta-2-SNAPSHOT
    Uncommented and cleaned the unit tests

--- esigate-app-master/src/test/java/org/esigate/test/cases/VaryMasterTest.java
+++ esigate-app-master/src/test/java/org/esigate/test/cases/VaryMasterTest.java
@@ -1,122 +1,101 @@
 package org.esigate.test.cases;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-
 import junit.framework.TestCase;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.params.ClientPNames;
-import org.apache.http.client.params.CookiePolicy;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.cookie.BasicClientCookie;
-import org.junit.Ignore;
-import org.xml.sax.SAXException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * "Vary" header testing
  * 
  * @author Nicolas Richeton
  */
-@Ignore("Should work only after HttpClient 4.3")
 public class VaryMasterTest extends TestCase {
-	private final static String APPLICATION_PATH = "http://localhost:8080/esigate-app-master/";
-	private static String PAGE_CACHE_AUTO = "vary-auto.jsp";
-	private static String PAGE_CACHE_DELAY = "vary.jsp";
+	private static final Logger LOG = LoggerFactory.getLogger(VaryMasterTest.class);
 
 	/**
 	 * Send a request with a Cookie "test-cookie" to vary.jsp (which will get
 	 * content from provider) and ensure the result is valid.
 	 * 
-	 * @param addCookie
-	 * @param value
-	 * @param refresh
-	 * @return
-	 * @throws MalformedURLException
-	 * @throws IOException
-	 * @throws SAXException
+	 * @param cookieValue
+	 * @param forceRefresh
+	 * @return Page timestamp. Can be used to detect cache hits.
+	 * @throws Exception
 	 */
-	private String doCookieRequest(String page, String cookieValue, boolean forceRefresh) throws Exception {
-		DefaultHttpClient client = new DefaultHttpClient();
-		client.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY);
-		HttpGet request = new HttpGet(APPLICATION_PATH + page);
+	private String doCookieRequest(String cookieValue, boolean forceRefresh) throws Exception {
+		CookieStore cookieStore = new BasicCookieStore();
+		HttpClientContext context = new HttpClientContext();
+		context.setCookieStore(cookieStore);
+		CloseableHttpClient client = HttpClients.createDefault();
+		RequestConfig config = RequestConfig.custom().setCookieSpec(CookieSpecs.BROWSER_COMPATIBILITY).build();
+		HttpGet request = new HttpGet("http://localhost:8080/esigate-app-master/vary.jsp");
+		request.setConfig(config);
+
 		if (cookieValue != null) {
 			BasicClientCookie cookie = new BasicClientCookie("test-cookie", cookieValue);
 			cookie.setDomain("localhost");
 			cookie.setPath("/");
-			client.getCookieStore().addCookie(cookie);
+			cookieStore.addCookie(cookie);
 		}
 		if (forceRefresh) {
 			request.addHeader("Cache-Control", "no-cache");
 		}
-		HttpResponse response = client.execute(request);
+		HttpResponse response = client.execute(request, context);
 		// Ensure content is valid.
 		String text = IOUtils.toString(response.getEntity().getContent());
 		assertNotNull(text);
-		System.err.println(cookieValue + " -> " + text);
+		LOG.debug("----- Request with cookie " + cookieValue + " and forceRefresh=" + forceRefresh + " -----> \n" + text);
 		if (cookieValue != null) {
 			assertTrue("no value '" + cookieValue + "' found", text.contains(cookieValue));
 		} else {
 			assertTrue("no cookie found", text.contains("no cookie"));
 		}
 
-		client.getConnectionManager().shutdown();
+		client.close();
 
-		// Return page timestamp. Can be used to detect cache hits.
+		//
 		return text.substring(text.indexOf("stime") + 5, text.indexOf("etime"));
-	}
-
-	/**
-	 * Run all tests with auto caching based on response.
-	 * 
-	 * @throws Exception
-	 */
-	public void shouldRespectBlockVariantsCacheAuto() throws Exception {
-		testBlockVary(PAGE_CACHE_AUTO);
-	}
-
-	/**
-	 * Run all tests with caching based on configuration.
-	 * 
-	 * @throws Exception
-	 */
-	public void shouldRespectBlockVariantsCacheTtlFixed() throws Exception {
-		testBlockVary(PAGE_CACHE_DELAY);
-
 	}
 
 	/**
 	 * Call test page with different cookie values and ensure cache use and
 	 * content are valid.
 	 * 
-	 * @param page
-	 * 
 	 * @throws Exception
 	 */
-	public void testBlockVary(String page) throws Exception {
+	public void testBlockVary() throws Exception {
 		// FIXME HttpClient cache does not support Vary: Cookie. See
 		// https://issues.apache.org/jira/browse/HTTPCLIENT-1190
-		// // This is for initialization : get JSESSIONID
-		doCookieRequest(page, "init", false);
+
+		doCookieRequest("init", false);
 
 		// Cache test
-		String value1 = doCookieRequest(page, "value1", false);
-		String valueNull = doCookieRequest(page, null, false);
-		assertEquals(value1, doCookieRequest(page, "value1", false));
-		String value2 = doCookieRequest(page, "value2", false);
-		assertEquals(valueNull, doCookieRequest(page, null, false));
-		assertEquals(value1, doCookieRequest(page, "value1", false));
-		assertEquals(value2, doCookieRequest(page, "value2", false));
-		String value3 = doCookieRequest(page, "value3", false);
-		assertEquals(valueNull, doCookieRequest(page, null, false));
-		assertEquals(value3, doCookieRequest(page, "value3", false));
+		String value1 = doCookieRequest("value1", false);
+		String valueNull = doCookieRequest(null, false);
+		assertEquals(value1, doCookieRequest("value1", false));
+		String value2 = doCookieRequest("value2", false);
+		assertEquals(valueNull, doCookieRequest(null, false));
+		assertEquals(value1, doCookieRequest("value1", false));
+		assertEquals(value2, doCookieRequest("value2", false));
+		String value3 = doCookieRequest("value3", false);
+		assertEquals(valueNull, doCookieRequest(null, false));
+		assertEquals(value3, doCookieRequest("value3", false));
 
-		// Test refresh
-		String value1Refresh = doCookieRequest(page, "value1", true);
+		// Test refresh but in fact there should be no refresh in an include
+		String value1Refresh = doCookieRequest("value1", true);
 		// assertFalse(value1.equals(value1Refresh));
-		assertEquals(value1Refresh, doCookieRequest(page, "value1", false));
+		assertEquals(value1Refresh, doCookieRequest("value1", false));
 
 	}
 
--- esigate-app-provider/src/main/webapp/vary.jsp
+++ esigate-app-provider/src/main/webapp/vary.jsp
@@ -6,8 +6,8 @@
 	prefix="assemble"%>	
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <% response.addHeader("Vary", "Cookie"); %>
-<% response.addHeader( "Cache-Control", "max-age=60"); %>
-<% response.addDateHeader( "Date", System.currentTimeMillis()); %>
+<% response.addHeader( "Cache-Control", "public, max-age=60"); %>
+<% response.setDateHeader( "Date", System.currentTimeMillis()); %>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
@@ -17,21 +17,19 @@
 
 This page display first cookie content.
 <assemble:block name="block1">
-<% if ( request.getCookies() != null  ){ 
+<% 	
 	boolean found = false;
-	Cookie[] cookies = request.getCookies();
-	for( int i = 0; i < cookies.length; i ++){
-		if( "test-cookie" .equals(cookies[i].getName())){
-			%><%=cookies[i].getValue() %><% 
-			found = true; 
+	if ( request.getCookies() != null  ) { 
+		Cookie[] cookies = request.getCookies();
+		for( int i = 0; i < cookies.length; i ++) {
+			if( "test-cookie" .equals(cookies[i].getName())) {
+				%><%=cookies[i].getValue() %><% 
+				found = true; 
+			}
 		}
 	}
-	
-	 if ( !found ){ %>
-	<%="no cookie" %>
-	<% } 
-
- } %>
+	if ( !found ){ %><%="no cookie" %><% } 
+%>
 Generation time :  stime<%=System.currentTimeMillis() %>etime
 </assemble:block>
 </body>
