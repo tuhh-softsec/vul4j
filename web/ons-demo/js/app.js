79b8d839dec543c8cbe6e21dd63ead487562f1cd
Paul Greyson <someone.named.paul@gmail.com>
2013-04-09 09:34:46 -0700

    integrate svg us map

--- web/ons-demo/js/app.js
+++ web/ons-demo/js/app.js
@@ -1232,6 +1232,20 @@ function sync(svg) {
 }
 
 svg = createTopologyView();
+
+
+d3.xml("assets/map.svg", "image/svg+xml", function(xml) {
+  var importedNode = document.importNode(xml.documentElement, true);
+  var paths = importedNode.querySelectorAll('path');
+  var i;
+  for (i=0; i < paths.length; i+=1) {
+  	svg.append('svg:path')
+  		.attr('class', 'state')
+  		.attr('d', d3.select(paths.item(i)).attr('d'))
+  		.attr('transform', 'translate(-500 -500)scale(1 1.7)')
+  }
+});
+
 updateSelectedFlows();
 
 d3.select('#showFlowChooser').on('click', function () {
