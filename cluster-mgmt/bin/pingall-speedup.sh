9763143e8adc378ce29450d9d5cbeb55cf3e95d4
Masayoshi Kobayashi <masayoshi@onlab.us>
2013-04-10 04:42:39 +0000

    fix in pingall

--- cluster-mgmt/bin/pingall-speedup.sh
+++ cluster-mgmt/bin/pingall-speedup.sh
@@ -4,8 +4,9 @@ if [ $# != 1 ]; then
 elif [ ! -f ${HOME}/ONOS/web/$1  ]; then
   echo "no such flowdef file: $1"
 fi
-echo "Raw data at /tmp/.pingall.result.$$"
-dsh "cd ONOS/web; ./pingallm-local.py $1" > /tmp/.pingall.result.$$
-cat /tmp/.pingall.result.$$ | grep "Pingall flow" | sort -n -k 4 
-cat /tmp/.pingall.result.$$ | grep "Pingall Result" | awk '{s+=$5; f+=$7; i+=$9}END{printf("Pingall Result: success %d fail %d incomplete %d\n",s,f,i)}'
+logfile="/tmp/.$USER.pingall.result.$$"
+echo "Raw data at $logfile"
+dsh "cd ONOS/web; ./pingallm-local.py $1" > $logfile 
+cat $logfile | grep "Pingall flow" | sort -n -k 4 
+cat $logfile | grep "Pingall Result" | awk '{s+=$5; f+=$7; i+=$9}END{printf("Pingall Result: success %d fail %d incomplete %d\n",s,f,i)}'
 
