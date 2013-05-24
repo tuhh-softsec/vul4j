tuhh-softsec/vul4j

commit ce4bf4c5212188692850f06ee6874196104646e9

Use apache-commons-compress for bzip2 compression/decompression
Plexus Archiver contains old bzip2 compression and decompression code
which is appearently forked from Apache Ant.  The same code is
currently maintained as part of Apache Commons Compress.

The bundled bzip2 code is very outdated.  It has several bugs, serious
performance problems, including CVE-2012-2098 vulnerability, which can
be used to cause denial of service.

To fix security vulnerability and prevent future problems bundled
bzip2 code is removed and replaced by calls to Apache Commons Compress
library.

src/main/java/org/codehaus/plexus/archiver/bzip2/BZip2Compressor.java
@@ -17,6 +17,7 @@ package org.codehaus.plexus.archiver.bzip2;
  *  limitations under the License.
  */
 
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
 import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.util.Compressor;
 import org.codehaus.plexus.util.IOUtil;
@@ -31,7 +32,7 @@ import java.io.IOException;
 public class BZip2Compressor
     extends Compressor
 {
-    private CBZip2OutputStream zOut;
+    private BZip2CompressorOutputStream zOut;
     
     /**
      * perform the GZip compression operation.
@@ -45,7 +46,7 @@ public class BZip2Compressor
                 new BufferedOutputStream( new FileOutputStream( getDestFile() ) );
             bos.write( 'B' );
             bos.write( 'Z' );
-            zOut = new CBZip2OutputStream( bos );
+            zOut = new BZip2CompressorOutputStream( bos );
             compress( getSource(), zOut );
         }
         catch ( IOException ioe )

src/main/java/org/codehaus/plexus/archiver/bzip2/BZip2UnArchiver.java
@@ -24,6 +24,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.codehaus.plexus.archiver.AbstractUnArchiver;
 import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.util.IOUtil;
@@ -53,7 +54,7 @@ public class BZip2UnArchiver
                               + getDestFile().getAbsolutePath() );
 
             FileOutputStream out = null;
-            CBZip2InputStream zIn = null;
+            BZip2CompressorInputStream zIn = null;
             FileInputStream fis = null;
             BufferedInputStream bis = null;
             try
@@ -90,7 +91,7 @@ public class BZip2UnArchiver
         }
     }
 
-    public static CBZip2InputStream getBZip2InputStream( InputStream bis )
+    public static BZip2CompressorInputStream getBZip2InputStream( InputStream bis )
         throws IOException
     {
         int b = bis.read();
@@ -103,7 +104,7 @@ public class BZip2UnArchiver
         {
             return null;
         }
-        return new CBZip2InputStream( bis );
+        return new BZip2CompressorInputStream( bis );
     }
 
     protected void execute( String path, File outputDirectory )

src/main/java/org/codehaus/plexus/archiver/tar/TarArchiver.java
@@ -25,12 +25,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.zip.GZIPOutputStream;
 
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
 import org.codehaus.plexus.archiver.AbstractArchiver;
 import org.codehaus.plexus.archiver.ArchiveEntry;
 import org.codehaus.plexus.archiver.ArchiverException;
 import org.codehaus.plexus.archiver.ResourceIterator;
 import org.codehaus.plexus.archiver.UnixStat;
-import org.codehaus.plexus.archiver.bzip2.CBZip2OutputStream;
 import org.codehaus.plexus.archiver.util.EnumeratedAttribute;
 import org.codehaus.plexus.archiver.util.ResourceUtils;
 import org.codehaus.plexus.components.io.attributes.PlexusIoResourceAttributes;
@@ -623,7 +623,7 @@ public class TarArchiver
             {
                 ostream.write( 'B' );
                 ostream.write( 'Z' );
-                return new CBZip2OutputStream( ostream );
+                return new BZip2CompressorOutputStream( ostream );
             }
             return ostream;
         }

src/main/java/org/codehaus/plexus/archiver/tar/TarUnArchiver.java
@@ -17,8 +17,8 @@ package org.codehaus.plexus.archiver.tar;
  *  limitations under the License.
  */
 
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.codehaus.plexus.archiver.ArchiverException;
-import org.codehaus.plexus.archiver.bzip2.CBZip2InputStream;
 import org.codehaus.plexus.archiver.util.EnumeratedAttribute;
 import org.codehaus.plexus.archiver.zip.AbstractZipUnArchiver;
 import org.codehaus.plexus.util.IOUtil;
@@ -200,7 +200,7 @@ public class TarUnArchiver
                         throw new ArchiverException( "Invalid bz2 file." + file.toString() );
                     }
                 }
-                return new CBZip2InputStream( istream );
+                return new BZip2CompressorInputStream( istream );
             }
             return istream;
         }

pom.xml
@@ -58,6 +58,11 @@
       <version>2.0.6</version>
     </dependency>
     <dependency>
+      <groupId>org.apache.commons</groupId>
+      <artifactId>commons-compress</artifactId>
+      <version>1.5</version>
+    </dependency>
+    <dependency>
       <groupId>junit</groupId>
       <artifactId>junit</artifactId>
       <version>4.11</version>

src/main/java/org/codehaus/plexus/archiver/bzip2/BZip2Constants.java
@@ -1,110 +1,0 @@
-package org.codehaus.plexus.archiver.bzip2;
-
-/*
- * Copyright  2001,2004 The Apache Software Foundation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
-
-/*
- * This package is based on the work done by Keiron Liddle, Aftex Software
- * <keiron@aftexsw.com> to whom the Ant project is very grateful for his
- * great code.
- */
-
-/**
- * Base class for both the compress and decompress classes.
- * Holds common arrays, and static data.
- *
- * @version $Revision$ $Date$
- *          from org.apache.ant.tools.bzip2.BZip2Constants v1.8
- */
-public interface BZip2Constants
-{
-
-    int baseBlockSize = 100000;
-
-    int MAX_ALPHA_SIZE = 258;
-
-    int MAX_CODE_LEN = 23;
-
-    int RUNA = 0;
-
-    int RUNB = 1;
-
-    int N_GROUPS = 6;
-
-    int G_SIZE = 50;
-
-    int N_ITERS = 4;
-
-    int MAX_SELECTORS = ( 2 + ( 900000 / G_SIZE ) );
-
-    int NUM_OVERSHOOT_BYTES = 20;
-
-    int[] rNums = {
-        619, 720, 127, 481, 931, 816, 813, 233, 566, 247,
-        985, 724, 205, 454, 863, 491, 741, 242, 949, 214,
-        733, 859, 335, 708, 621, 574, 73, 654, 730, 472,
-        419, 436, 278, 496, 867, 210, 399, 680, 480, 51,
-        878, 465, 811, 169, 869, 675, 611, 697, 867, 561,
-        862, 687, 507, 283, 482, 129, 807, 591, 733, 623,
-        150, 238, 59, 379, 684, 877, 625, 169, 643, 105,
-        170, 607, 520, 932, 727, 476, 693, 425, 174, 647,
-        73, 122, 335, 530, 442, 853, 695, 249, 445, 515,
-        909, 545, 703, 919, 874, 474, 882, 500, 594, 612,
-        641, 801, 220, 162, 819, 984, 589, 513, 495, 799,
-        161, 604, 958, 533, 221, 400, 386, 867, 600, 782,
-        382, 596, 414, 171, 516, 375, 682, 485, 911, 276,
-        98, 553, 163, 354, 666, 933, 424, 341, 533, 870,
-        227, 730, 475, 186, 263, 647, 537, 686, 600, 224,
-        469, 68, 770, 919, 190, 373, 294, 822, 808, 206,
-        184, 943, 795, 384, 383, 461, 404, 758, 839, 887,
-        715, 67, 618, 276, 204, 918, 873, 777, 604, 560,
-        951, 160, 578, 722, 79, 804, 96, 409, 713, 940,
-        652, 934, 970, 447, 318, 353, 859, 672, 112, 785,
-        645, 863, 803, 350, 139, 93, 354, 99, 820, 908,
-        609, 772, 154, 274, 580, 184, 79, 626, 630, 742,
-        653, 282, 762, 623, 680, 81, 927, 626, 789, 125,
-        411, 521, 938, 300, 821, 78, 343, 175, 128, 250,
-        170, 774, 972, 275, 999, 639, 495, 78, 352, 126,
-        857, 956, 358, 619, 580, 124, 737, 594, 701, 612,
-        669, 112, 134, 694, 363, 992, 809, 743, 168, 974,
-        944, 375, 748, 52, 600, 747, 642, 182, 862, 81,
-        344, 805, 988, 739, 511, 655, 814, 334, 249, 515,
-        897, 955, 664, 981, 649, 113, 974, 459, 893, 228,
-        433, 837, 553, 268, 926, 240, 102, 654, 459, 51,
-        686, 754, 806, 760, 493, 403, 415, 394, 687, 700,
-        946, 670, 656, 610, 738, 392, 760, 799, 887, 653,
-        978, 321, 576, 617, 626, 502, 894, 679, 243, 440,
-        680, 879, 194, 572, 640, 724, 926, 56, 204, 700,
-        707, 151, 457, 449, 797, 195, 791, 558, 945, 679,
-        297, 59, 87, 824, 713, 663, 412, 693, 342, 606,
-        134, 108, 571, 364, 631, 212, 174, 643, 304, 329,
-        343, 97, 430, 751, 497, 314, 983, 374, 822, 928,
-        140, 206, 73, 263, 980, 736, 876, 478, 430, 305,
-        170, 514, 364, 692, 829, 82, 855, 953, 676, 246,
-        369, 970, 294, 750, 807, 827, 150, 790, 288, 923,
-        804, 378, 215, 828, 592, 281, 565, 555, 710, 82,
-        896, 831, 547, 261, 524, 462, 293, 465, 502, 56,
-        661, 821, 976, 991, 658, 869, 905, 758, 745, 193,
-        768, 550, 608, 933, 378, 286, 215, 979, 792, 961,
-        61, 688, 793, 644, 986, 403, 106, 366, 905, 644,
-        372, 567, 466, 434, 645, 210, 389, 550, 919, 135,
-        780, 773, 635, 389, 707, 100, 626, 958, 165, 504,
-        920, 176, 193, 713, 857, 265, 203, 50, 668, 108,
-        645, 990, 626, 197, 510, 357, 358, 850, 858, 364,
-        936, 638
-    };
-}

commit 3a379a3ba390041748b2f6906e48ca4144bbc926

Fix double 'BZBZ' magic string regression in bzip2 archives
Commit ce4bf4c5 introduced a regression where archives created with
plexus-archiver would only be opened by plexus-archiver and no other tool. In
similar fashion plexus-archiver was unable to extract proper archives because it
removed "BZ" magic header before passing the stream to commons-compress

Signed-off-by: Kristian Rosenvold <krosenvold@apache.org>

src/main/java/org/codehaus/plexus/archiver/bzip2/BZip2Compressor.java
@@ -44,8 +44,6 @@ public class BZip2Compressor
         {
             BufferedOutputStream bos =
                 new BufferedOutputStream( new FileOutputStream( getDestFile() ) );
-            bos.write( 'B' );
-            bos.write( 'Z' );
             zOut = new BZip2CompressorOutputStream( bos );
             compress( getSource(), zOut );
         }

src/main/java/org/codehaus/plexus/archiver/bzip2/BZip2UnArchiver.java
@@ -94,16 +94,6 @@ public class BZip2UnArchiver
     public static BZip2CompressorInputStream getBZip2InputStream( InputStream bis )
         throws IOException
     {
-        int b = bis.read();
-        if ( b != 'B' )
-        {
-            return null;
-        }
-        b = bis.read();
-        if ( b != 'Z' )
-        {
-            return null;
-        }
         return new BZip2CompressorInputStream( bis );
     }
 

src/main/java/org/codehaus/plexus/archiver/tar/TarArchiver.java
@@ -631,8 +631,6 @@ public class TarArchiver
             }
             else if ( BZIP2.equals( value ) )
             {
-                ostream.write( 'B' );
-                ostream.write( 'Z' );
                 return new BZip2CompressorOutputStream( ostream );
             }
             return ostream;

src/main/java/org/codehaus/plexus/archiver/tar/TarUnArchiver.java
@@ -192,14 +192,6 @@ public class TarUnArchiver
             }
             else if ( BZIP2.equals( value ) )
             {
-                final char[] magic = new char[]{'B', 'Z'};
-                for ( int i = 0; i < magic.length; i++ )
-                {
-                    if ( istream.read() != magic[ i ] )
-                    {
-                        throw new ArchiverException( "Invalid bz2 file." + file.toString() );
-                    }
-                }
                 return new BZip2CompressorInputStream( istream );
             }
             return istream;