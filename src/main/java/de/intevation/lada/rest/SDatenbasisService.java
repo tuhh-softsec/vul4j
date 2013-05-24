4d5326079e5f48c66f92274d08147d626ddd8fe5
Raimund Renkert <rrenkert@intevation.de>
2013-05-24 11:55:41 +0200

    New REST service for 'Datenbasis Stammdaten'.

---
+++ src/main/java/de/intevation/lada/rest/SDatenbasisService.java
@@ -0,0 +1,62 @@
+package de.intevation.lada.rest;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import de.intevation.lada.data.Repository;
+import de.intevation.lada.model.SDatenbasis;
+
+/**
+ * This class produces a RESTful service to read the contents of SDatenbasis table.
+ * 
+ * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
+ */
+@Path("/datenbasis")
+@RequestScoped
+public class SDatenbasisService
+{
+    /**
+     * The Repository for SDatenbasis.
+     */
+    @Inject @Named
+    private Repository repository;
+
+    /**
+     * The logger for this class
+     */
+    @Inject
+    private Logger logger;
+
+    /**
+     * Request all SDatenbasis objects.
+     *
+     * @return JSON Object via Rest service
+     */
+    @GET
+    @Produces("text/json")
+    public List<SDatenbasis> findAll() {
+        List<SDatenbasis> result = repository.findAll(SDatenbasis.class);
+        return result;
+    }
+
+    /**
+     * Request a single SDatenbasis via its id.
+     *
+     * @param id The mst_id
+     * @return JSON Object via REST service.
+     */
+    @GET
+    @Path("/{id}")
+    @Produces("text/json")
+    public SDatenbasis findById(@PathParam("id") String id) {
+        return repository.findById(SDatenbasis.class, id);
+    }
+}
