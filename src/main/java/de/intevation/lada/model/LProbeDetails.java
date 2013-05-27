b9c9bc44930646652b0b3e897b1d63717cafb084
Torsten Irländer <torsten.irlaender@intevation.de>
2013-05-27 17:31:00 +0200

    Refactored LProbeDetails class. The class now inherits from the LProbe class. Added function setLrobe which will set the values from LProbe in LProbeDetails.

--- src/main/java/de/intevation/lada/model/LProbeDetails.java
+++ src/main/java/de/intevation/lada/model/LProbeDetails.java
@@ -4,32 +4,34 @@ import java.io.Serializable;
 import java.util.List;
 
 
-public class LProbeDetails implements Serializable
+public class LProbeDetails extends LProbe
 {
 
-    private LProbe lprobe;
     private List<LOrt> lorts;
     private List<LKommentarP> lkommentar;
 
-    public LProbeDetails() {
-    }
-
-    public LProbeDetails(
-        LProbe lprobe,
-        List<LOrt> lorts,
-        List<LKommentarP> lkommentar
-    ) {
-        this.lprobe = lprobe;
-        this.lorts = lorts;
-        this.setLkommentar(lkommentar);
-    }
-
-    public LProbe getLprobe() {
-        return lprobe;
-    }
-
     public void setLprobe(LProbe lprobe) {
-        this.lprobe = lprobe;
+      this.setProbeId(lprobe.getProbeId());
+      this.setMstId(lprobe.getMstId());
+      this.setUmwId(lprobe.getUmwId());
+      this.setTest(lprobe.isTest());
+      this.setHauptprobenNr(lprobe.getHauptprobenNr());
+      this.setBaId(lprobe.getBaId());
+      this.setMediaDesk(lprobe.getMediaDesk());
+      this.setMedia(lprobe.getMedia());
+      this.setProbenartId(lprobe.getProbenartId());
+      this.setDatenbasisId(lprobe.getDatenbasisId());
+      this.setProbeentnahmeBeginn(lprobe.getProbeentnahmeBeginn());
+      this.setProbeentnahmeEnde(lprobe.getProbeentnahmeEnde());
+      this.setMittelungsdauer(lprobe.getMittelungsdauer());
+      this.setLetzteAenderung(lprobe.getLetzteAenderung());
+      this.setErzeugerId(lprobe.getErzeugerId());
+      this.setProbeNehmerId(lprobe.getProbeNehmerId());
+      this.setMpKat(lprobe.getMpKat());
+      this.setMplId(lprobe.getMplId());
+      this.setMprId(lprobe.getMprId());
+      this.setSolldatumBeginn(lprobe.getSolldatumBeginn());
+      this.setSolldatumEnde(lprobe.getSolldatumEnde());
     }
 
     public List<LOrt> getLort() {
