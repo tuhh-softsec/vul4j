5ca2db93d1cf19c0aa8569bc6ed2a9e8720c00b6
Raimund Renkert <rrenkert@intevation.de>
2013-05-31 14:04:01 +0200

    New service to create a LProbe.

--- src/main/java/de/intevation/lada/rest/LProbeService.java
+++ src/main/java/de/intevation/lada/rest/LProbeService.java
@@ -1,11 +1,14 @@
 package de.intevation.lada.rest;
 
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -91,13 +94,66 @@ public class LProbeService {
         return repository.filter(mstId, uwbId, begin);
     }
 
+    @POST
+    @Path("/create")
+    @Produces("text/json")
+    @Consumes("application/json")
+    public String create(LProbe probe) {
+        boolean success = repository.create(probe);
+        if(success) {
+            return "[{" +
+                   "returncode: 200," +
+                   "errorfields: []," +
+                   createWarningFields() +
+                   "}]";
+        }
+        else {
+            int generalError = repository.getGeneralError();
+            String response = "[{ \"returncode\": " + generalError + ", ";
+            response += "\"errors\": {" + createErrorFields() + "}, ";
+            response += "\"warnings\": {" + createWarningFields() + "}";
+            response += "}]";
+            return response;
+        }
+    }
 
-    //    200 Ok
-    //    Anderes ist Fehler.
-    //    {
-    //        returncode: 500,
-    //        field: [
-    //           {fieldname: validator_error_code},
-    //        ]
-    //    }
+    private String createWarningFields() {
+        Map<String, Integer> warnings = repository.getWarnings();
+        String response = "\"fields\": [";
+        if (warnings == null || warnings.isEmpty()) {
+            response += "]";
+            return response;
+        }
+        boolean first = true;
+        for (Map.Entry<String, Integer> entry: warnings.entrySet()) {
+            if (!first) {
+                response +=",";
+            }
+            response += "{" + "\"" + entry.getKey() +
+                "\": " + entry.getValue() + "}";
+            first = false;
+        }
+        response += "]";
+        return response;
+    }
+
+    private String createErrorFields() {
+        Map<String, Integer> errors = repository.getErrors();
+        String response = "\"fields\": [";
+        if (errors.isEmpty()) {
+            response += "]";
+            return response;
+        }
+        boolean first = true;
+        for (Map.Entry<String, Integer> entry: errors.entrySet()) {
+            if (!first) {
+                response +=",";
+            }
+            response += "{" + "\"" + entry.getKey() +
+                "\": " + entry.getValue() + "}";
+            first = false;
+        }
+        response += "]";
+        return response;
+    }
 }
