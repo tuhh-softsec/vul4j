38f3634696e69cf8ce0bf58b958d55362f7f14a4
Torsten Irländer <torsten.irlaender@intevation.de>
2013-06-07 15:20:51 +0200

    Get Success-Code directly from repository when initialising the response.

--- src/main/java/de/intevation/lada/rest/LProbeService.java
+++ src/main/java/de/intevation/lada/rest/LProbeService.java
@@ -51,12 +51,8 @@ public class LProbeService {
     @Path("/{id}")
     @Produces("text/json")
     public Response findById(@PathParam("id") String id) {
-        boolean success = true;
         LProbe item = repository.findById(LProbe.class, id);
-        if (item == null) {
-            success = false;
-        }
-        Response response = new Response(success, repository.getGeneralError(), item);
+        Response response = new Response(repository.getSuccess(), repository.getGeneralError(), item);
         response.setWarnings(repository.getWarnings());
         response.setErrors(repository.getErrors());
         return response;
