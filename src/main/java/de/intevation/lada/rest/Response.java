ee46ca8f2c297b266a9357cba46337576c3a042b
Torsten Irländer <torsten.irlaender@intevation.de>
2013-06-07 11:45:46 +0200

    Formatting. Make the data attribute of type Object to make it more general.

--- src/main/java/de/intevation/lada/rest/Response.java
+++ src/main/java/de/intevation/lada/rest/Response.java
@@ -1,59 +1,62 @@
 package de.intevation.lada.rest;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import de.intevation.lada.model.LProbe;
-
+/**
+* This class is nice!.
+*
+* @author <a href="mailto:torsten@intevation.de">Torsten Irländer</a>
+*/
 @SuppressWarnings("serial")
 public class Response implements java.io.Serializable {
-	
-	private Boolean success;
-	private String message;
-	private List<LProbe> data;
-	private String errors;
-	private String warnings;
-	
-	public Response(Boolean success, String message, LProbe data) {
-		super();
-		this.success = success;
-		this.message = message;
-		this.data = new ArrayList<LProbe>();
-		this.data.add(data);
-	}
-	public Response(boolean success, String message, List<LProbe> data) {
-		this.success = success;
-		this.message = message;
-		this.data = data;
-	}
-	public Boolean getSuccess() {
-		return success;
-	}
-	public void setSuccess(Boolean success) {
-		this.success = success;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public List<LProbe> getData() {
-		return data;
-	}
-	public void setData(List<LProbe> data) {
-		this.data = data;
-	}
-	public String getErrors() {
-		return errors;
-	}
-	public void setErrors(String errors) {
-		this.errors = errors;
-	}
-	public String getWarnings() {
-		return warnings;
-	}
-	public void setWarnings(String warnings) {
-		this.warnings = warnings;
-	}
+
+    private Boolean success;
+    private String message;
+    private Object data;
+    private String errors;
+    private String warnings;
+
+    public Response(boolean success, String message, Object data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getErrors() {
+        return errors;
+    }
+
+    public void setErrors(String errors) {
+        this.errors = errors;
+    }
+
+    public String getWarnings() {
+        return warnings;
+    }
+
+    public void setWarnings(String warnings) {
+        this.warnings = warnings;
+    }
 }
