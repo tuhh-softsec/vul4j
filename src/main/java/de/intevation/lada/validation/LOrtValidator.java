bb7444308634e43a9a1fc2dccd3477a045622212
Raimund Renkert <rrenkert@intevation.de>
2013-06-12 16:11:13 +0200

    Added validator for LOrt.

---
+++ src/main/java/de/intevation/lada/validation/LOrtValidator.java
@@ -0,0 +1,31 @@
+package de.intevation.lada.validation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+import de.intevation.lada.model.LOrt;
+
+@ApplicationScoped
+@Named("lortvalidator")
+public class LOrtValidator
+implements Validator
+{
+
+    @Override
+    public Map<String, Integer> validate(Object object)
+    throws ValidationException {
+        Map<String, Integer> warnings = new HashMap<String, Integer>();
+        if (!(object instanceof LOrt)) {
+            Map<String, Integer> errors = new HashMap<String, Integer>();
+            errors.put("lort", 610);
+            throw new ValidationException(errors);
+        }
+
+        //TODO: more validation, see LSB: VI - Konsistenzregeln
+        return warnings;
+    }
+
+}
