500b9030e1de4c733f750bab036374e86ddc8a9d
Raimund Renkert <rrenkert@intevation.de>
2013-06-13 17:09:59 +0200

    Added a concrete readonly-repository for 'stammdaten'.

---
+++ src/main/java/de/intevation/lada/data/ReadOnlyRepository.java
@@ -0,0 +1,30 @@
+package de.intevation.lada.data;
+
+import java.util.Map;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import de.intevation.lada.rest.Response;
+
+@RequestScoped
+@Named("readolyrepository")
+public class ReadOnlyRepository
+extends Repository
+{
+
+    @Override
+    public Response create(Object object) {
+        return null;
+    }
+
+    @Override
+    public Response update(Object object) {
+        return null;
+    }
+
+    @Override
+    public Response filter(Map<String, String> keys) {
+        return null;
+    }
+}
