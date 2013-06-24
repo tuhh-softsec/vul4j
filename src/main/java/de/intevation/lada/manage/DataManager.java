27225aa66778b83950020de32416b1c581405501
Raimund Renkert <rrenkert@intevation.de>
2013-06-24 14:11:48 +0200

    New interface for data manager and generic implementation of a data manager.

---
+++ src/main/java/de/intevation/lada/manage/DataManager.java
@@ -0,0 +1,47 @@
+package de.intevation.lada.manage;
+
+import javax.ejb.EJBTransactionRolledbackException;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.TransactionRequiredException;
+
+
+@Stateless
+@Named("datamanager")
+public class DataManager
+implements Manager
+{
+    @Inject
+    private EntityManager em;
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void create(Object object)
+    throws EntityExistsException,
+        IllegalArgumentException,
+        EJBTransactionRolledbackException,
+        TransactionRequiredException {
+        em.persist(object);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void update(Object object)
+    throws EntityExistsException,
+        IllegalArgumentException,
+        EJBTransactionRolledbackException,
+        TransactionRequiredException {
+        em.merge(object);
+    }
+
+    @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public <T> void delete(String id, Class<T> clazz) throws Exception {
+        
+    }
+}
---
+++ src/main/java/de/intevation/lada/manage/Manager.java
@@ -0,0 +1,29 @@
+package de.intevation.lada.manage;
+
+import javax.ejb.EJBTransactionRolledbackException;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityExistsException;
+import javax.persistence.TransactionRequiredException;
+
+@Stateless
+public interface Manager
+{
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void create(Object object)
+    throws EntityExistsException,
+        IllegalArgumentException,
+        EJBTransactionRolledbackException,
+        TransactionRequiredException;
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void update(Object object)
+    throws EntityExistsException,
+        IllegalArgumentException,
+        EJBTransactionRolledbackException,
+        TransactionRequiredException;
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public <T> void delete(String id, Class<T> clazz) throws Exception;
+}
