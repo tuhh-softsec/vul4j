fe27e279a3048e5ca9596327d90e46da1705326f
Sean Mullan <mullan@apache.org>
2013-06-26 19:39:52 +0000

    unmarshalKeyInfo should throw NPE when xmlStructure is null as defined by the KeyInfoFactory API.
    git-svn-id: https://svn.apache.org/repos/asf/santuario/xml-security-java/trunk@1497056 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/jcp/xml/dsig/internal/dom/DOMKeyInfoFactory.java
+++ src/main/java/org/apache/jcp/xml/dsig/internal/dom/DOMKeyInfoFactory.java
@@ -139,7 +139,10 @@ public final class DOMKeyInfoFactory extends KeyInfoFactory {
     @Override
     public KeyInfo unmarshalKeyInfo(XMLStructure xmlStructure) 
         throws MarshalException {
-        if (xmlStructure == null || !(xmlStructure instanceof javax.xml.crypto.dom.DOMStructure)) {
+        if (xmlStructure == null) {
+            throw new NullPointerException("xmlStructure cannot be null");
+        }
+        if (!(xmlStructure instanceof javax.xml.crypto.dom.DOMStructure)) {
             throw new ClassCastException("xmlStructure must be of type DOMStructure");
         }
         Node node = 
