5e01a703d7ae7e938136ed84b3c3eaf76dfc52ff
Raimund Renkert <rrenkert@intevation.de>
2013-07-04 17:24:03 +0200

    Removed unused imports.

--- src/main/java/de/intevation/lada/model/LMesswert.java
+++ src/main/java/de/intevation/lada/model/LMesswert.java
@@ -3,15 +3,12 @@ package de.intevation.lada.model;
 // Generated 21.05.2013 16:58:30 by Hibernate Tools 3.4.0.CR1
 
 import java.util.Date;
+
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
--- src/main/java/de/intevation/lada/model/Ort.java
+++ src/main/java/de/intevation/lada/model/Ort.java
@@ -2,19 +2,13 @@ package de.intevation.lada.model;
 
 // Generated 21.05.2013 16:58:30 by Hibernate Tools 3.4.0.CR1
 
-import java.io.Serializable;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
