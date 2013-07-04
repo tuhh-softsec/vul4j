f423a1a912012f30a91b874e764f04c8bd9fb8e1
Raimund Renkert <rrenkert@intevation.de>
2013-07-04 11:41:26 +0200

    Added seqence to ort model.

--- src/main/java/de/intevation/lada/model/Ort.java
+++ src/main/java/de/intevation/lada/model/Ort.java
@@ -9,10 +9,13 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -69,6 +72,8 @@ public class Ort implements java.io.Serializable {
 
 	@Id
 	@Column(name = "ort_id", unique = true, nullable = false)
+	@SequenceGenerator(name = "ORT_ID_STORE", sequenceName = "ort_ort_id_seq", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ORT_ID_STORE")
 	public int getOrtId() {
 		return this.ortId;
 	}
