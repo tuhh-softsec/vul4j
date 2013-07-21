a2eb966172c0c3f1b04a57d78e8e9fb6e98880ab
Nicolas Richeton <nicolas.richeton@gmail.com>
2013-07-21 16:21:28 +0000

    Javadoc

--- esigate-core/src/main/java/org/esigate/events/impl/FetchEvent.java
+++ esigate-core/src/main/java/org/esigate/events/impl/FetchEvent.java
@@ -6,12 +6,20 @@ import org.apache.http.protocol.HttpContext;
 import org.esigate.events.Event;
 
 /**
- * Fetch event : when a new HTTP call is made to get a new block/template (Cache miss).
+ * Fetch event : when a new HTTP call is made to get a new block/template (Cache
+ * miss).
  * 
  * @author Nicolas Richeton
  * 
  */
 public class FetchEvent extends Event {
+	/**
+	 * The response returned by the remote server.
+	 * <p>
+	 * May be null if the request has not been executed yet. If this case,
+	 * setting a response cancels the HTTP call and use the given object
+	 * instead.
+	 */
 	public HttpResponse httpResponse;
 	/**
 	 * The request context
@@ -20,5 +28,5 @@ public class FetchEvent extends Event {
 	/**
 	 * The new HTTP call details.
 	 */
-	public HttpRequest httpRequest;
+	public HttpRequest httpRequest = null;
 }
