6cac14d6704eb05b23e8470fba574ea9a7d73918
Nicolas Richeton <nicolas.richeton@gmail.com>
2013-07-21 16:20:07 +0000

    Javadoc

--- esigate-core/src/main/java/org/esigate/events/impl/ReadEntityEvent.java
+++ esigate-core/src/main/java/org/esigate/events/impl/ReadEntityEvent.java
@@ -12,11 +12,35 @@ import org.esigate.events.Event;
  * 
  */
 public class ReadEntityEvent extends Event {
+	/**
+	 * The response returned by the remote server or by a cache subsystem.
+	 */
 	public HttpResponse httpResponse;
+	/**
+	 * Response mime type
+	 */
 	public String mimeType;
+
+	/**
+	 * Declared or detected charset.
+	 * <p>
+	 * The charset can be modified by an extension.
+	 * <p>
+	 * Note : if charset is modified, entityContent should be updated too.
+	 */
 	public Charset charset;
+
+	/**
+	 * The raw entity content, without any character set applied. It can be used
+	 * to re-decode the entity content if the default charset was incorrect.
+	 */
 	public byte[] rawEntityContent;
+	/**
+	 * The current, decoded entity content.
+	 * <p>
+	 * An extension can update this content if is incorrect.
+	 * <p>
+	 * Note : if entityContent is modified, charset should be updated too.
+	 */
 	public String entityContent;
 }
-
-
