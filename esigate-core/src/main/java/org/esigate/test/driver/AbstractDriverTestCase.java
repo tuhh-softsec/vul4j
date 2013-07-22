1d1e3f3c7ee3769fc79cf054ba6c3a7a279c36af
Nicolas Richeton <nicolas.richeton@gmail.com>
2013-07-22 14:03:12 +0000

    TestCase improvements

--- esigate-core/src/main/java/org/esigate/test/driver/AbstractDriverTestCase.java
+++ esigate-core/src/main/java/org/esigate/test/driver/AbstractDriverTestCase.java
@@ -39,12 +39,11 @@ public abstract class AbstractDriverTestCase extends TestCase {
 	/**
 	 * Create a Driver instance with a custom response handler.
 	 * 
-	 * @param properties 
+	 * @param properties
 	 * @param responseHandler
 	 * @return
 	 */
-	protected static Driver createMockDriver(Properties properties,
-			IResponseHandler responseHandler) {
+	protected static Driver createMockDriver(Properties properties, IResponseHandler responseHandler) {
 		MockConnectionManager connManager = new MockConnectionManager();
 		connManager.setResponseHandler(responseHandler);
 		return createMockDriver(properties, connManager);
@@ -58,8 +57,7 @@ public abstract class AbstractDriverTestCase extends TestCase {
 	 * @param response
 	 * @return
 	 */
-	protected static Driver createMockDriver(Properties properties,
-			HttpResponse response) {
+	protected static Driver createMockDriver(Properties properties, HttpResponse response) {
 		MockConnectionManager connManager = new MockConnectionManager();
 		connManager.setResponse(response);
 		return createMockDriver(properties, connManager);
@@ -72,8 +70,7 @@ public abstract class AbstractDriverTestCase extends TestCase {
 	 * @param connectionManager
 	 * @return
 	 */
-	protected static Driver createMockDriver(Properties properties,
-			HttpClientConnectionManager connectionManager) {
+	protected static Driver createMockDriver(Properties properties, HttpClientConnectionManager connectionManager) {
 		return createMockDriver(properties, connectionManager, "tested");
 	}
 
@@ -86,13 +83,11 @@ public abstract class AbstractDriverTestCase extends TestCase {
 	 *            name of the Driver instance
 	 * @return
 	 */
-	protected static Driver createMockDriver(Properties properties,
-			HttpClientConnectionManager connectionManager, String name) {
-		CookieManager cookieManager = ExtensionFactory.getExtension(properties,
-				Parameters.COOKIE_MANAGER, null);
+	protected static Driver createMockDriver(Properties properties, HttpClientConnectionManager connectionManager,
+			String name) {
+		CookieManager cookieManager = ExtensionFactory.getExtension(properties, Parameters.COOKIE_MANAGER, null);
 
-		HttpClientHelper httpClientHelper = new HttpClientHelper(
-				new EventManager(name), cookieManager, properties,
+		HttpClientHelper httpClientHelper = new HttpClientHelper(new EventManager(name), cookieManager, properties,
 				connectionManager);
 		Driver driver = new Driver(name, properties, httpClientHelper);
 		DriverFactory.put(name, driver);
@@ -131,11 +126,10 @@ public abstract class AbstractDriverTestCase extends TestCase {
 	 * @throws HttpErrorPage
 	 * @throws URISyntaxException
 	 */
-	public static HttpResponse driverProxy(Driver d,
-			HttpEntityEnclosingRequest request) throws IOException,
-			HttpErrorPage, URISyntaxException {
+	public static HttpResponse driverProxy(Driver d, HttpEntityEnclosingRequest request, Renderer... renderers)
+			throws IOException, HttpErrorPage, URISyntaxException {
 		String uri = request.getRequestLine().getUri();
-		d.proxy(new URI(uri).getPath(), request);
+		d.proxy(new URI(uri).getPath(), request, renderers);
 
 		return TestUtils.getResponse(request);
 
