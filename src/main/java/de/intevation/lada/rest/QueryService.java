0bb8957c2b9ae9fae1692498c3a0d664ab1d4447
Torsten Irländer <torsten.irlaender@intevation.de>
2013-07-24 09:09:57 +0200

    Added service to fetch sql query configurations.

---
+++ src/main/java/de/intevation/lada/rest/QueryService.java
@@ -0,0 +1,68 @@
+package de.intevation.lada.rest;
+
+import java.util.ArrayList;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.UriInfo;
+
+import de.intevation.lada.auth.Authentication;
+import de.intevation.lada.auth.AuthenticationException;
+import de.intevation.lada.model.LOrt;
+
+/**
+ * This class produces a RESTful service to read, write and update
+ * LOrt objects.
+ *
+ * @author <a href="mailto:torsten.irlaender@intevation.de">Torsten Irländer</a>
+ */
+@Path("/query")
+@RequestScoped
+public class QueryService
+{
+    /**
+     * The authorization module.
+     */
+    @Inject
+    @Named("ldapauth")
+    private Authentication authentication;
+
+    /**
+     * Request SQL-Queries
+     *
+     * Query parameters are used for the filter in form of key-value pairs.
+     *
+     * @param info      The URL query parameters.
+     * @param headers   The HTTP header containing authorization information.
+     * @return Response object.
+     */
+    @GET
+    @Produces("text/json")
+    public Response filter(
+        @Context UriInfo info,
+        @Context HttpHeaders headers
+    ) {
+        try {
+            if (!authentication.isAuthorizedUser(headers)) {
+                return new Response(false, 699, new ArrayList<LOrt>());
+            }
+            String queries = this.loadQueryConfig();
+            Response response = new Response(true, 200, queries);
+            return response;
+        }
+        catch(AuthenticationException ae) {
+            return new Response(false, 699, new ArrayList<LOrt>());
+        }
+    }
+
+	private String loadQueryConfig() {
+		String query = "[]";
+		return query;
+	}
+}
