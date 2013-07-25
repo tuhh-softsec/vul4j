4526aa5f169c607b9b8699ff0125a2c7b4083728
Torsten Irländer <torsten.irlaender@intevation.de>
2013-07-25 14:23:44 +0200

    Added Info class to serve general information on the server application.

--- src/main/java/de/intevation/lada/rest/InfoService.java
+++ src/main/java/de/intevation/lada/rest/InfoService.java
@@ -13,6 +13,35 @@ import javax.ws.rs.core.UriInfo;
 import de.intevation.lada.auth.Authentication;
 import de.intevation.lada.auth.AuthenticationException;
 
+class Info {
+    String user;
+    String groups;
+    String version;
+    public Info(String user, String groups, String version) {
+        this.user = user;
+        this.groups = groups;
+        this.version = version;
+    }
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+    public String getGroups() {
+        return groups;
+    }
+    public void setGroups(String groups) {
+        this.groups = groups;
+    }
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
+
 /**
  * This class produces a RESTful service to read, write and update
  * LOrt objects.
@@ -49,7 +78,7 @@ public class InfoService
             if (!authentication.isAuthorizedUser(headers)) {
                 return new Response(false, 699, null);
             }
-            Response response = new Response(true, 200, null);
+            Response response = new Response(true, 200, new Info("-/-", "-/-", "-/-"));
             return response;
         }
         catch(AuthenticationException ae) {
