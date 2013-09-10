98478429f4772780def6e73b7f17f73a5d5f7250
Pankaj Berde <pankaj@onlab.us>
2013-09-10 13:53:26 -0700

    move up to Netty 4

--- src/main/java/net/onrc/onos/ofcontroller/linkdiscovery/ILinkDiscovery.java
+++ src/main/java/net/onrc/onos/ofcontroller/linkdiscovery/ILinkDiscovery.java
@@ -3,7 +3,7 @@ package net.onrc.onos.ofcontroller.linkdiscovery;
 import net.floodlightcontroller.core.IUpdate;
 
 import org.codehaus.jackson.map.annotate.JsonSerialize;
-import org.codehaus.jackson.map.ser.std.ToStringSerializer;
+import org.codehaus.jackson.map.ser.ToStringSerializer;
 import org.openflow.util.HexString;
 
 public interface ILinkDiscovery {
