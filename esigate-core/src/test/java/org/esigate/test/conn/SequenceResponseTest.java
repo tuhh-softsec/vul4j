329dd8657456ecb4eb960d32da5c06c84f188f20
Nicolas Richeton <nicolas.richeton@gmail.com>
2013-10-20 17:06:34 +0000

    Unit tests on Response handlers

---
+++ esigate-core/src/test/java/org/esigate/test/conn/SequenceResponseTest.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.esigate.test.conn;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.apache.http.ParseException;
+import org.apache.http.util.EntityUtils;
+import org.esigate.test.http.HttpRequestBuilder;
+import org.esigate.test.http.HttpResponseBuilder;
+import org.junit.Test;
+
+/**
+ * Tests on SequenceResponse
+ * 
+ * @author Nicolas Richeton
+ * 
+ */
+public class SequenceResponseTest {
+
+	@Test
+	public void testSequence() throws ParseException, IOException {
+		SequenceResponse seq = new SequenceResponse().reponse(
+				new HttpResponseBuilder().status(200).entity("OK 1").build()).reponse(
+				new HttpResponseBuilder().status(200).entity("OK 2").build());
+
+		assertEquals("OK 1", EntityUtils.toString(seq.execute(new HttpRequestBuilder().build()).getEntity()));
+		assertEquals("OK 2", EntityUtils.toString(seq.execute(new HttpRequestBuilder().build()).getEntity()));
+
+		try {
+			seq.execute(new HttpRequestBuilder().build());
+			fail("Should send an exception");
+		} catch (IllegalStateException e) {
+			// OK
+		}
+	}
+}
---
+++ esigate-core/src/test/java/org/esigate/test/conn/UriResponseTest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.esigate.test.conn;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.apache.http.ParseException;
+import org.apache.http.util.EntityUtils;
+import org.esigate.test.http.HttpRequestBuilder;
+import org.esigate.test.http.HttpResponseBuilder;
+import org.junit.Test;
+
+/**
+ * Tests on UriResponse
+ * 
+ * @author Nicolas Richeton
+ * 
+ */
+public class UriResponseTest {
+
+	@Test
+	public void testUri() throws ParseException, IOException {
+		UriResponse seq = new UriResponse().reponse("http://test/path1",
+				new HttpResponseBuilder().status(200).entity("OK 1").build()).reponse("http://test/path2",
+				new HttpResponseBuilder().status(200).entity("OK 2").build());
+
+		assertEquals("OK 1", EntityUtils.toString(seq
+				.execute(new HttpRequestBuilder().uri("http://test/path1").build()).getEntity()));
+		assertEquals("OK 2", EntityUtils.toString(seq
+				.execute(new HttpRequestBuilder().uri("http://test/path2").build()).getEntity()));
+
+		try {
+			seq.execute(new HttpRequestBuilder().uri("http://test/path3").build());
+			fail("Should send an exception");
+		} catch (IllegalStateException e) {
+			// OK
+		}
+	}
+}
