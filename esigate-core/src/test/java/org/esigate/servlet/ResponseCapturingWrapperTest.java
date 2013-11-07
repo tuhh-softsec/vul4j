62216eaed49fc71e2557447bbc8c4e2a2d290c13
Francois-Xavier <francois-xavier.bonnet@centraliens.net>
2013-11-07 10:40:10 +0000

    0000074: Use ESIGate as Servlet filter (embedded in Java application) https://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=74 Added unit tests

---
+++ esigate-core/src/test/java/org/esigate/servlet/ResponseCapturingWrapperTest.java
@@ -0,0 +1,34 @@
+package org.esigate.servlet;
+
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.TestCase;
+
+import org.esigate.http.ContentTypeHelper;
+import org.esigate.http.HttpResponseUtils;
+
+public class ResponseCapturingWrapperTest extends TestCase {
+
+    public void testHtmlResponse() throws Exception {
+        Properties properties = new Properties();
+        ContentTypeHelper contentTypeHelper = new ContentTypeHelper(properties);
+        HttpServletResponse response = new MockHttpServletResponse();
+        ResponseCapturingWrapper tested = new ResponseCapturingWrapper(response, contentTypeHelper);
+        tested.setContentType("text/html;charset=UTF-8");
+        tested.getWriter().write("test");
+        assertEquals("test", HttpResponseUtils.toString(tested.getResponse(), null));
+    }
+
+    public void testNonParseableResponse() throws Exception {
+        Properties properties = new Properties();
+        ContentTypeHelper contentTypeHelper = new ContentTypeHelper(properties);
+        HttpServletResponse response = new MockHttpServletResponse();
+        ResponseCapturingWrapper tested = new ResponseCapturingWrapper(response, contentTypeHelper);
+        tested.setContentType("binary/octet-stream");
+        tested.getOutputStream().print("test");
+        assertNull(tested.getResponse().getEntity());
+    }
+
+}
