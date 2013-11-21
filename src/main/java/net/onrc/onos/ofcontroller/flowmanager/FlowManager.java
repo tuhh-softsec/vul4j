ff43b5635b2936e013c6594a7900e0f20d178e93
Pavlin Radoslavov <pavlin@onlab.us>
2013-11-20 17:13:48 -0800

    Redirect deleteFlow() to use clearFlow().
    In the notification-based implementation,
    deleteFlow() is implemented by using clearFlow()

--- src/main/java/net/onrc/onos/ofcontroller/flowmanager/FlowManager.java
+++ src/main/java/net/onrc/onos/ofcontroller/flowmanager/FlowManager.java
@@ -593,11 +593,19 @@ public class FlowManager implements IFloodlightModule, IFlowService, INetMapStor
      */
     @Override
     public boolean deleteAllFlows() {
+	//
+	// TODO: In the notification-based implementation,
+	// deleteFlow() is implemented by using clearFlow()
+	//
+	return clearAllFlows();
+
+	/*
 	if (FlowDatabaseOperation.deleteAllFlows(dbHandlerApi)) {
 	    datagridService.notificationSendAllFlowsRemoved();
 	    return true;
 	}
 	return false;
+	*/
     }
 
     /**
@@ -608,11 +616,18 @@ public class FlowManager implements IFloodlightModule, IFlowService, INetMapStor
      */
     @Override
     public boolean deleteFlow(FlowId flowId) {
+	//
+	// TODO: In the notification-based implementation,
+	// deleteFlow() is implemented by using clearFlow()
+	//
+	return clearFlow(flowId);
+	/*
 	if (FlowDatabaseOperation.deleteFlow(dbHandlerApi, flowId)) {
 	    datagridService.notificationSendFlowRemoved(flowId);
 	    return true;
 	}
 	return false;
+	*/
     }
 
     /**
