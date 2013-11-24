3084ff1d242317a301d87e0f0e8ef5a4d6090212
Francois-Xavier <francois-xavier.bonnet@centraliens.net>
2013-11-24 14:20:53 +0000

    0000074: Use ESIGate as Servlet filter (embedded in Java application) https://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=74 Removed references to deleted libraries

---
+++ esigate-core/src/main/java/org/esigate/servlet/user/ConnectorFilter.java
@@ -0,0 +1,50 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.esigate.servlet.user;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * This handles the user name and locale passed as parameters by the driver and
+ * overrides getRemoteUser and getLocale methods from the request in order for
+ * the application to retrieve them.
+ * 
+ * @author Francois-Xavier Bonnet
+ */
+public class ConnectorFilter implements Filter {
+    public void destroy() {
+ // Nothing to do
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+     FilterChain filterChain) throws IOException, ServletException {
+        FilteredRequest filteredRequest = new FilteredRequest(
+  (HttpServletRequest) request);
+ filterChain.doFilter(filteredRequest, response);
+    }
+
+    public void init(FilterConfig arg0) throws ServletException {
+ // Nothing to do
+    }
+}
