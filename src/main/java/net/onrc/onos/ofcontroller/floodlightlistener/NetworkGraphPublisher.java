f96c5855a4b438b64eacbb6fd0f7772542acc85c
yoshi <yoshi@onlab.us>
2013-12-06 09:23:58 -0800

    Added get DB object

--- src/main/java/net/onrc/onos/ofcontroller/floodlightlistener/NetworkGraphPublisher.java
+++ src/main/java/net/onrc/onos/ofcontroller/floodlightlistener/NetworkGraphPublisher.java
@@ -152,6 +152,7 @@ public class NetworkGraphPublisher implements IDeviceListener,
 
     protected void switchCleanup() {
     	//op.close();
+	op = GraphDBManager.getDBOperation("ramcloud", "/tmp/ramcloudconf");
     	Iterable<ISwitchObject> switches = op.getActiveSwitches();
     	
     	log.debug("Checking for inactive switches");
