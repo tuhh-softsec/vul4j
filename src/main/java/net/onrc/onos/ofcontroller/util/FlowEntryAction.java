020e96ccfbbc387ae554792dd015bb7c960aae87
Pavlin Radoslavov <pavlin@onlab.us>
2013-12-11 12:48:04 -0800

    * Renamed method ActionValues.toShort() to ActionValues.getValue() * Replaced "default" switch case statements with explicit case value   statements.

--- src/main/java/net/onrc/onos/ofcontroller/util/FlowEntryAction.java
+++ src/main/java/net/onrc/onos/ofcontroller/util/FlowEntryAction.java
@@ -43,10 +43,13 @@ public class FlowEntryAction {
 	private ActionValues(short value) {
 	    this.value = value;
 	}
-	
-	public short toShort() {
-		return value;
-	}
+
+	/**
+	 * Get the value.
+	 *
+	 * @return the value.
+	 */
+	public short getValue() { return value; }
     }
 
     /**
@@ -1568,9 +1571,9 @@ public class FlowEntryAction {
 	case ACTION_ENQUEUE:
 	    ret += " action=" + actionEnqueue.toString();
 	    break;
-	default:
-	    ret += " action=UNDEFINED";
-		break;
+	case ACTION_VENDOR:
+	    ret += " action=VENDOR";
+	    break;
 	}
 	ret += "]";
 
@@ -1663,8 +1666,9 @@ public class FlowEntryAction {
 	    case ACTION_ENQUEUE:
 		actionEnqueue = new ActionEnqueue(decode);
 		break;
-		default:
-			throw new IllegalArgumentException();
+	    case ACTION_VENDOR:
+		// TODO: Handle it as appropriate
+		break;
 	    }
 	} catch (IllegalArgumentException e) {
 	    throw new IllegalArgumentException("Invalid action string");
