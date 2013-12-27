37000dc7ac848291dd93eb069081092f01dc5308
Francois-Xavier <francois-xavier.bonnet@centraliens.net>
2013-12-27 09:34:05 +0000

    0000215: Do not use java.net.URI https://sourceforge.net/apps/mantisbt/webassembletool/view.php?id=215 java.net.URI is now only used in UriUtils class but still necessary for HttpClient

--- esigate-core/src/main/java/org/esigate/http/RedirectStrategy.java
+++ esigate-core/src/main/java/org/esigate/http/RedirectStrategy.java
@@ -15,8 +15,6 @@
 
 package org.esigate.http;
 
-import java.net.URI;
-
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.ProtocolException;
@@ -32,21 +30,14 @@ public class RedirectStrategy extends DefaultRedirectStrategy {
     private static final String LAST_REQUEST = "LAST_REQUEST";
 
     @Override
-    public URI getLocationURI(HttpRequest request, HttpResponse response, HttpContext context)
-            throws ProtocolException {
-        URI redirectLocation = super.getLocationURI(request, response, context);
-        if (LOG.isInfoEnabled()) {
-            LOG.info(request.getRequestLine() + " -> " + response.getStatusLine()
-                    + " -> automaticaly following redirect to " + redirectLocation.toString());
-        }
-        return redirectLocation;
-    }
-
-    @Override
     public HttpUriRequest getRedirect(HttpRequest request, HttpResponse response, HttpContext context)
             throws ProtocolException {
         HttpUriRequest newRequest = super.getRedirect(request, response, context);
         context.setAttribute(LAST_REQUEST, newRequest);
+        if (LOG.isInfoEnabled()) {
+            LOG.info(request.getRequestLine() + " -> " + response.getStatusLine()
+                    + " -> automaticaly following redirect to " + newRequest.getRequestLine());
+        }
         return newRequest;
     }
 
