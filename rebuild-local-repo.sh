3e8830d3dd9144ae9b674843c56a78914f5420c1
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-01-09 14:42:52 -0800

    Cleaned shaded kryo2 jar. Avoid re-shading already shaded package path

--- rebuild-local-repo.sh
+++ rebuild-local-repo.sh
@@ -18,7 +18,7 @@ fi
 # Install Kryo2 workaround to local repo
 # - Shaded(rename package name to allow mixing 2 different Kryo version)
 # - Install created sharded jar to local repo
-${MVN} -f kryo2/pom.xml package exec:exec
+${MVN} -f kryo2/pom.xml clean package exec:exec
 
 # Install modified curators to local repo
 ${MVN} install:install-file -Dfile=./curator/curator-framework-1.3.5-SNAPSHOT.jar -DgroupId=com.netflix.curator -DartifactId=curator-framework -Dversion=1.3.5-SNAPSHOT -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=./repo -DcreateChecksum=true
