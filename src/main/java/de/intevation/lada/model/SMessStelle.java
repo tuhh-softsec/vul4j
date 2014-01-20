10a23994be0cdbb4bf4c234466c7f0eba7daf134
Raimund Renkert <rrenkert@intevation.de>
2014-01-20 12:27:00 +0100

    Added license header to source files.

--- src/main/java/de/intevation/lada/model/SMessStelle.java
+++ src/main/java/de/intevation/lada/model/SMessStelle.java
@@ -1,3 +1,10 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3) 
+ * and comes with ABSOLUTELY NO WARRANTY! Check out 
+ * the documentation coming with IMIS-Labordaten-Application for details. 
+ */
 package de.intevation.lada.model;
 
 // Generated 21.05.2013 16:58:30 by Hibernate Tools 3.4.0.CR1
--- src/main/java/de/intevation/lada/utils/Resources.java
+++ src/main/java/de/intevation/lada/utils/Resources.java
@@ -1,3 +1,10 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3) 
+ * and comes with ABSOLUTELY NO WARRANTY! Check out 
+ * the documentation coming with IMIS-Labordaten-Application for details. 
+ */
 /*
  * JBoss, Home of Professional Open Source
  * Copyright 2012, Red Hat, Inc. and/or its affiliates, and individual
