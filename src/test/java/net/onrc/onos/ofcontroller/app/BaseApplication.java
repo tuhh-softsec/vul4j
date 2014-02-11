9edde9883c9de13e71444c737eddc23c162ad56b
Toshio Koide <t-koide@onlab.us>
2014-02-11 11:36:02 -0800

    Remove old intent prototype codes
    Change-Id: I1e2c175793b7e4ee6b7eda0a5444ed47e13947ac

--- src/test/java/net/onrc/onos/ofcontroller/app/BaseApplication.java
+++
@@ -1,8 +0,0 @@
-package net.onrc.onos.ofcontroller.app;
-
-/**
- * This code is valid for the architectural study purpose only.
- * @author Toshio Koide (t-koide@onlab.us)
- */
-public interface BaseApplication {
-}
