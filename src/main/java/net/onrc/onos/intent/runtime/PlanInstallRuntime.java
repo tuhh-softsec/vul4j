9b712f6e160e68ed2bc3e1f5ad5f81b94a54c9c7
Brian O'Connor <bocon@onlab.us>
2014-02-20 14:22:20 -0800

    Adding logging for PlanCalc and Install
    Change-Id: Id69e7893f3e6dd2ad1bd695476ecdbc346f0ea12

--- src/main/java/net/onrc/onos/intent/runtime/PlanInstallRuntime.java
+++ src/main/java/net/onrc/onos/intent/runtime/PlanInstallRuntime.java
@@ -12,6 +12,9 @@ import net.onrc.onos.ofcontroller.flowprogrammer.IFlowPusherService;
 import net.onrc.onos.ofcontroller.networkgraph.NetworkGraph;
 import net.onrc.onos.ofcontroller.util.Pair;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 
  * @author Brian O'Connor <bocon@onlab.us>
@@ -22,6 +25,7 @@ public class PlanInstallRuntime {
     NetworkGraph graph;
     IFlowPusherService pusher;
     IFloodlightProviderService provider;
+    private final static Logger log = LoggerFactory.getLogger(PlanInstallRuntime.class);
 
     public PlanInstallRuntime(NetworkGraph graph, 
 	    		      IFloodlightProviderService provider,
@@ -33,6 +37,7 @@ public class PlanInstallRuntime {
 
     public void installPlan(List<Set<FlowEntry>> plan) {
 	Map<Long,IOFSwitch> switches = provider.getSwitches();
+	log.debug("IOFSwitches: {}", switches);
 	for(Set<FlowEntry> phase : plan) {
 	    Set<Pair<IOFSwitch, net.onrc.onos.ofcontroller.util.FlowEntry>> entries = new HashSet<>();
 	    // convert flow entries and create pairs
@@ -40,6 +45,7 @@ public class PlanInstallRuntime {
 		entries.add(new Pair<>(switches.get(entry.getSwitch().getDpid()), 
 			entry.getFlowEntry()));
 	    }
+	    log.debug("Pushing flow entries: {}", entries);
 	    // push flow entries to switches
 	    pusher.pushFlowEntries(entries);
 	    // TODO: wait for confirmation messages before proceeding
