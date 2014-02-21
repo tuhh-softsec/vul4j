247a23c7eae4c7062f98e2f16ac7535bb1e06efa
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-02-21 09:21:14 -0800

    Respect environment variables, if set.
    Change-Id: I98f36c7aaab5e38147d5c3f3c50b387e130b246e

--- build-ramcloud-java-bindings.sh
+++ build-ramcloud-java-bindings.sh
@@ -4,13 +4,13 @@
 
 set -x
 
-ONOS_HOME=~/ONOS
-RAMCLOUD_HOME=~/ramcloud
+ONOS_HOME=${ONOS_HOME:-~/ONOS}
+RAMCLOUD_HOME=${RAMCLOUD_HOME:-~/ramcloud}
 
 # create ramcloud lib
-cp -p ${ONOS_HOME}/src/main/java/edu/stanford/ramcloud/JRamCloud.java ${HOME}/ramcloud/bindings/java/edu/stanford/ramcloud/
-cp -p ${ONOS_HOME}/src/main/cpp/edu_stanford_ramcloud_JRamCloud.cc ${HOME}/ramcloud/bindings/java/edu/stanford/ramcloud/
-cd ${HOME}/ramcloud/bindings/java/edu/stanford/ramcloud/
+cp -pvf ${ONOS_HOME}/src/main/java/edu/stanford/ramcloud/JRamCloud.java ${RAMCLOUD_HOME}/bindings/java/edu/stanford/ramcloud/
+cp -pvf ${ONOS_HOME}/src/main/cpp/edu_stanford_ramcloud_JRamCloud.cc ${RAMCLOUD_HOME}/bindings/java/edu/stanford/ramcloud/
+cd ${RAMCLOUD_HOME}/bindings/java/edu/stanford/ramcloud/
 javac JRamCloud.java
 ./build_so.sh
 jar cvf ${RAMCLOUD_HOME}/bindings/java/RamCloud.jar ${RAMCLOUD_HOME}/bindings/java/edu/stanford/ramcloud/*.class
