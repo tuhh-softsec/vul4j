eb2d9e5d7a354ec9ee3d63119d98e16127537e8b
Toshio Koide <t-koide@onlab.us>
2014-03-31 17:52:34 -0700

    Add unit test code for ErrorIntent and IntentOperation class.
    Change-Id: Id87e1b01b8aeed25c1e8e87e947d1cf864571388

---
+++ src/test/java/net/onrc/onos/intent/IntentOperationTest.java
@@ -0,0 +1,41 @@
+package net.onrc.onos.intent;
+
+import static org.junit.Assert.*;
+import net.onrc.onos.intent.IntentOperation.Operator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for the IntentOperation class.
+ * @author Toshio Koide (t-koide@onlab.us)
+ */
+public class IntentOperationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test the result of executing constructor of the IntentOperation class.
+     * This test checks the id field and the operator field.
+     */
+    @Test
+    public void testCreate() {
+        IntentOperation op1 = new IntentOperation(Operator.ADD, new Intent("1"));
+        IntentOperation op2 = new IntentOperation(Operator.REMOVE, new Intent("2"));
+        IntentOperation op3 = new IntentOperation(Operator.ERROR, new Intent("3"));
+
+        assertEquals("1", op1.intent.getId());
+        assertEquals(Operator.ADD, op1.operator);
+        assertEquals("2", op2.intent.getId());
+        assertEquals(Operator.REMOVE, op2.operator);
+        assertEquals("3", op3.intent.getId());
+        assertEquals(Operator.ERROR, op3.operator);
+    }
+}
