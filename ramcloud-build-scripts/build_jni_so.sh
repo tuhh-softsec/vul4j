c83a4fbc39416e2a5e4dc1e832893901e69e812b
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-04-07 15:16:06 -0700

    better heuristics to guess correct JAVA_HOME
    When JAVA_HOME is not defined,
    guess whether we're using OpenJDK or Oracle Java
    by using java -version string. ONOS-1159
    
    Change-Id: Ia2994722aec16c1f49cda05c51728854332ea815

--- ramcloud-build-scripts/build_jni_so.sh
+++ ramcloud-build-scripts/build_jni_so.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+if [ -z "${JAVA_HOME}" ]; then
+  echo "JAVA_HOME not set"
+  OPENJDK=`java -version 2>&1 | grep OpenJDK`
+  if [ -z "${OPENJDK}" -a -d "/usr/lib/jvm/java-7-oracle" ]; then
+    JAVA_HOME=/usr/lib/jvm/java-7-oracle
+  else
+    JAVA_HOME=/usr/lib/jvm/default-java
+  fi
+  echo "Assuming JAVA_HOME=${JAVA_HOME}"
+fi
+
 # fail on command error
 set -e
+
+# echo back each command
+set -x
 
 JAVA_HOME=${JAVA_HOME:-/usr/lib/jvm/java-7-oracle}
 ONOS_HOME=${ONOS_HOME:-~/ONOS}
