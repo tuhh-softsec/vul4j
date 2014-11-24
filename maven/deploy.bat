55aa86cdb40021a7f0e38577068a2c29d40cbc46
Jirka Kosek <318795+kosek@users.noreply.github.com>
2014-11-24 21:30:01 +0000

    Various changes and updates to make Maven build work after several years of being in limbo

--- maven/deploy.bat
+++ maven/deploy.bat
@@ -1,5 +1,5 @@
-set VERSION=2.0.1
-set REPO_URL=sftp://web.sourceforge.net/home/groups/x/xs/xslthl/htdocs/maven/repository
+set VERSION=2.1.3
+set REPO_URL=sftp://web.sourceforge.net/home/project-web/xslthl/htdocs/maven/repository
 set REPO_ID=xslthl-sourceforge
 
-call mvn deploy:deploy-file -Durl=%REPO_URL% -DrepositoryId=%REPO_ID% -DpomFile=pom.xml -Dfile=..\dist\xslthl-%VERSION%.jar
+call c:\maven\bin\mvn -X deploy:deploy-file -Durl=%REPO_URL% -DrepositoryId=%REPO_ID% -DpomFile=pom.xml -Dfile=..\dist\xslthl-%VERSION%.jar
