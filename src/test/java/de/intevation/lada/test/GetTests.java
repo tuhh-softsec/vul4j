450ca079892f7da7b7dec964732b5710f4c123d7
Raimund Renkert <raimund.renkert@intevation.de>
2015-02-17 13:44:19 +0100

    Added test for proben kommentar service.

--- src/test/java/de/intevation/lada/test/GetTests.java
+++ src/test/java/de/intevation/lada/test/GetTests.java
@@ -363,4 +363,87 @@ public class GetTests {
         prot.setPassed(true);
     }
 
+    /**
+     * Test the GET Service by requesting all KommentarP objects.
+     *
+     * @param baseUrl The url pointing to the test deployment.
+     */
+    private final void pkommentarGetAllService(URL baseUrl)
+    throws Exception {
+        System.out.print(".");
+        Protocol prot = new Protocol();
+        prot.setName("ProbeKommentarService");
+        prot.setType("get all");
+        prot.setPassed(false);
+        protocol.add(prot);
+        /* Create a client*/
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(baseUrl + "pkommentar");
+        /* Request all probe objects*/
+        Response response = target.request().get();
+        String entity = response.readEntity(String.class);
+        try{
+            /* Try to parse the response*/
+            JsonReader reader = Json.createReader(new StringReader(entity));
+            JsonObject content = reader.readObject();
+            /* Verify the response*/
+            Assert.assertTrue(content.getBoolean("success"));
+            prot.addInfo("success", content.getBoolean("success"));
+            Assert.assertEquals("200", content.getString("message"));
+            prot.addInfo("message", content.getString("message"));
+            Assert.assertNotNull(content.getJsonArray("data"));
+            prot.addInfo("objects", content.getJsonArray("data").size());
+        }
+        catch(JsonException je) {
+            prot.addInfo("exception", je.getMessage());
+            Assert.fail(je.getMessage());
+        }
+        prot.setPassed(true);
+    }
+
+    /**
+     * Test the GET Service by requesting a single KommentarP object by id.
+     *
+     * @param baseUrl The url pointing to the test deployment.
+     */
+    private final void pkommentarGetByIdService(URL baseUrl)
+    throws Exception {
+        System.out.print(".");
+        Protocol prot = new Protocol();
+        prot.setName("ProbeKommentarService");
+        prot.setType("get by Id");
+        prot.setPassed(false);
+        protocol.add(prot);
+        try {
+            /* Create a json object from static messung string*/
+            JsonReader fromStringRreader =
+                Json.createReader(new StringReader(COMPARE_KOMMENTARP));
+            JsonObject staticKommentar = fromStringRreader.readObject();
+            /* Create a client*/
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client.target(baseUrl + "pkommentar/1");
+            prot.addInfo("kommentarId", 1);
+            /* Request a probe object by id*/
+            Response response = target.request().get();
+            String entity = response.readEntity(String.class);
+            /* Try to parse the response*/
+            JsonReader fromServiceReader =
+                Json.createReader(new StringReader(entity));
+            JsonObject content = fromServiceReader.readObject();
+            /* Verify the response*/
+            Assert.assertTrue(content.getBoolean("success"));
+            prot.addInfo("success", content.getBoolean("success"));
+            Assert.assertEquals("200", content.getString("message"));
+            prot.addInfo("message", content.getString("message"));
+            Assert.assertEquals(staticKommentar,
+                content.getJsonObject("data"));
+            prot.addInfo("object", "equals");
+        }
+        catch(JsonException je) {
+            prot.addInfo("exception", je.getMessage());
+            Assert.fail(je.getMessage());
+        }
+        prot.setPassed(true);
+    }
+
 }
