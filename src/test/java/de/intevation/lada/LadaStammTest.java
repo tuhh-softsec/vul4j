f865aee0b265cb9c4b1d0a9f84dc8fae01c25833
Raimund Renkert <raimund.renkert@intevation.de>
2015-02-18 16:11:41 +0100

    Comments.

--- src/test/java/de/intevation/lada/LadaStammTest.java
+++ src/test/java/de/intevation/lada/LadaStammTest.java
@@ -1,3 +1,10 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3) 
+ * and comes with ABSOLUTELY NO WARRANTY! Check out 
+ * the documentation coming with IMIS-Labordaten-Application for details. 
+ */
 package de.intevation.lada;
 
 import java.net.URL;
@@ -15,6 +22,12 @@ import org.junit.runners.MethodSorters;
 
 import de.intevation.lada.test.stamm.Stammdaten;
 
+
+/**
+ * Class to test the Lada server stammdaten services.
+ *
+ * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
+ */
 @RunWith(Arquillian.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LadaStammTest extends BaseTest {
