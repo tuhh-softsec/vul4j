ac09b556efd6827817195c1e2bc1b3e728c188e5
Raimund Renkert <raimund.renkert@intevation.de>
2015-04-30 17:27:40 +0200

    Updated messprogramm model.

--- src/main/java/de/intevation/lada/model/land/Messprogramm.java
+++ src/main/java/de/intevation/lada/model/land/Messprogramm.java
@@ -12,6 +12,8 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -25,6 +27,8 @@ public class Messprogramm implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id", unique=true, nullable=false)
     private Integer id;
 
     @Column(name="ba_id")
