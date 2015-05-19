4d16ba16f695e589ca277117e761d61ea864a684
Boriss Redkins <Boriss.Redkins@NetBoss.Com>
2015-05-19 11:55:53 +0100

    Updated comments.

--- Java/exercises/src/main/java/com/mycompany/exercises/jdbc/MyDatabase.java
+++ Java/exercises/src/main/java/com/mycompany/exercises/jdbc/MyDatabase.java
@@ -38,13 +38,19 @@ public class MyDatabase {
     }
 
     /**
-     * Creates Database Connection Pool using Apache Commons DBCP. This
-     * minimizes the number of times you close and re-create Connection objects.
+     * Creates Database Connection Pool using Apache Commons DBCP.
+     *
+     * This minimizes the number of times you close and re-create Connection
+     * objects. Connections in try-with-resources statements are taken from and
+     * returned to the connection pool. When a connection is returned to the
+     * pool, setAutoCommit is set to true.
+     *
      * Note: DBCP 2 compiles and runs under Java 7 only (JDBC 4.1).
      */
     private void createPoolingDriver() {
         ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(url, username, password);
         PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory, null);
+        // using defaults from GenericObjectPoolConfig (DEFAULT_MAX_IDLE=8, DEFAULT_MAX_TOTAL=8, DEFAULT_MIN_IDLE=0)
         ObjectPool connectionPool = new GenericObjectPool(poolableConnectionFactory);
         poolableConnectionFactory.setPool(connectionPool);
         PoolingDriver driver = new PoolingDriver();
