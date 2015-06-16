92c0628f42e1ea6bb9611ca9181af38bf12d4d28
Mat Booth <mat.booth@redhat.com>
2015-06-16 15:39:18 +0100

    Test case that reproduces "too many open files" problem from #6.

--- src/test/java/org/codehaus/plexus/archiver/jar/JarArchiverTest.java
+++ src/test/java/org/codehaus/plexus/archiver/jar/JarArchiverTest.java
@@ -3,7 +3,9 @@ package org.codehaus.plexus.archiver.jar;
 import org.codehaus.plexus.archiver.ArchiverException;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
 
 import junit.framework.TestCase;
 
@@ -42,4 +44,32 @@ public class JarArchiverTest
         archiver.createArchive();
     }
 
+    public void testVeryLargeJar()
+        throws IOException, ManifestException, ArchiverException
+    {
+        // Generate some number of random files that is likely to be
+        // two or three times the number of available file handles
+        File tmpDir = File.createTempFile( "veryLargeJar", null );
+        tmpDir.delete();
+        tmpDir.mkdirs();
+        Random rand = new Random();
+        for ( int i = 0; i < 15000; i++ )
+        {
+           File f = new File( tmpDir, "file" + i );
+           f.deleteOnExit();
+           FileOutputStream out = new FileOutputStream(f);
+           byte[] data = new byte[10240]; // 10kb per file
+           rand.nextBytes( data );
+           out.write( data );
+           out.flush();
+           out.close();
+        }
+
+        File jarFile = new File( "target/output/veryLargeJar.jar" );
+
+        JarArchiver archiver = new JarArchiver();
+        archiver.setDestFile( jarFile );
+        archiver.addDirectory( tmpDir );
+        archiver.createArchive();
+    }
 }
