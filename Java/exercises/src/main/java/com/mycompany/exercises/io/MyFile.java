f249dd226bc76e506d35ada0e3d7c2f63c299d5d
“msultan2” <mohamed.sultan1581@gmail.com>
2015-09-04 16:15:17 +0100

    Added FileWriterEAM class which Implements the execute around method (EAM) pattern. Added examples of using lambda expressions.

--- Java/exercises/src/main/java/com/mycompany/exercises/io/MyFile.java
+++ Java/exercises/src/main/java/com/mycompany/exercises/io/MyFile.java
@@ -4,9 +4,20 @@
  */
 package com.mycompany.exercises.io;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
 
 public final class MyFile {
@@ -40,4 +51,100 @@ public final class MyFile {
     System.err.println("Message: " + ex.getMessage());
   }
 
+  public static List<Path> listFilesInCurrentDirectory() {
+    return listFilesInDirectory(Paths.get("."));
+  }
+
+  public static List<Path> listFilesInDirectory(final Path directory) {
+    try {
+      return Files.list(directory).collect(toList());
+    } catch (IOException ex) {
+      System.err.println("Failed to list the names of all the files in " + directory + ": "
+          + ex.getMessage());
+    }
+    return Collections.emptyList();
+  }
+
+  public static List<Path> listSubdirectoriesInCurrentDirectory() {
+    return listSubdirectoriesInDirectory(Paths.get("."));
+  }
+
+  public static List<Path> listSubdirectoriesInDirectory(final Path directory) {
+    try {
+      return Files.list(directory)
+        .filter(Files::isDirectory)
+        .collect(toList());
+    } catch (IOException ex) {
+      System.err.println("Failed to list the names of all the subdirectories in " + directory + ": "
+        + ex.getMessage());
+    }
+    return Collections.emptyList();
+  }
+
+  /**
+   * List all files which names end with the specified suffix in a directory.
+   */
+  public static List<Path> listSelectFilesInDirectory(final Path directory,
+      final String suffix) {
+    List<Path> files = new ArrayList<>();
+    DirectoryStream.Filter<Path> filter = path -> path.toString().endsWith(suffix);
+    try {
+      Files.newDirectoryStream(directory, filter).forEach(path -> files.add(path));
+    } catch (IOException ex) {
+      System.err.println("Failed to list the names of *" + suffix + " files in " + directory + ": "
+        + ex.getMessage());
+    }
+    return files;
+  }
+
+  public static List<File> listHiddenFilesInDirectory(final Path directory) {
+    return Stream.of(new File(directory.toString())
+      .listFiles(File::isHidden))
+      .collect(toList());
+  }
+
+  /**
+   * List the immediate (one level deep) subdirectories in a given directory.
+   */
+  public static List<File> listImmediateSubdirectoriesInDirectory(final Path directory) {
+    return Stream.of(new File(directory.toString())
+      .listFiles())
+      .flatMap(file -> file.listFiles() == null ? Stream.of(file) : Stream.of(file.listFiles()))
+      .collect(toList());
+  }
+
+  // TODO
+  // list all files -> use it in FTPServerMock
+
+  // TODO - investigate Optional
+  public static void watchForFileChangesInDirectory(final Path directory) {
+    WatchService watchService = registerWatchServiceToObserveAnyChangeToDirectory(directory);
+    pollWatchServiceForAnyChangeToFilesInDirectory(watchService);
+  }
+
+
+  private static WatchService registerWatchServiceToObserveAnyChangeToDirectory(final Path directory) {
+    try {
+      WatchService watchService = directory.getFileSystem().newWatchService();
+      directory.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+      System.out.println("Report any file changed within next 1 minute...");
+      return watchService;
+    } catch (IOException ex) {
+      System.err.println("ERROR! Failed to registers the directory " + directory
+          + " with a watch service: " + ex.getMessage());
+    }
+    return null;
+  }
+
+  private static void pollWatchServiceForAnyChangeToFilesInDirectory(final WatchService watchService) {
+    try {
+      WatchKey watchKey = watchService.poll(1, TimeUnit.MINUTES);
+      if (watchKey != null) {
+        watchKey.pollEvents().stream()
+          .forEach(event -> System.out.println(event.context()));
+      }
+    } catch (InterruptedException ex) {
+      System.err.println("ERROR! Watch service was interrupted while waiting: " + ex.getMessage());
+    }
+  }
 }
--- Java/exercises/src/main/java/com/mycompany/exercises/io/MyFileExecutor.java
+++ Java/exercises/src/main/java/com/mycompany/exercises/io/MyFileExecutor.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.exercises.io;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -15,6 +16,16 @@ public final class MyFileExecutor {
     Path file = Paths.get("/", "home", "bredkins", "Desktop", "test.txt");
     new FileGenerator().generateFile(file);
     MyFile.processStringsFromFile(file);
-  }
 
+    try {
+      FileWriterEAM.use("eam.txt", writerEAM -> writerEAM.writeStuff("sweet"));
+
+      FileWriterEAM.use("eam2.txt", writerEAM -> {
+          writerEAM.writeStuff("how");
+          writerEAM.writeStuff("sweet");
+        });
+    } catch (IOException ex) {
+      System.err.println("ERROR! Failed to write files.");
+    }
+  }
 }
