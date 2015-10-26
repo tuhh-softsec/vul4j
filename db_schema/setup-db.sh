abce088e96e63fac869e5d2bf0ecea7f57893cf7
Tom Gottfried <tom@intevation.de>
2015-10-26 15:47:51 +0100

    Add script to set up database as user postgres.

---
+++ db_schema/setup-db.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+DIR=`dirname $0`
+
+ROLE_NAME=lada
+ROLE_PW=lada
+DB_NAME=lada
+
+psql --command "CREATE USER $ROLE_NAME PASSWORD '$ROLE_PW';"
+createdb -E UTF-8 $DB_NAME
+psql -d $DB_NAME -f $DIR/lada_schema.sql
+psql -d $DB_NAME --command \
+     "GRANT USAGE ON SCHEMA stammdaten, bund, land TO $ROLE_NAME;
+      GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES
+            ON ALL TABLES IN SCHEMA stammdaten, bund, land TO $ROLE_NAME;"
