b1db3d6e247fa7c567d636761cda26c99b095a86
Raimund Renkert <raimund.renkert@intevation.de>
2015-10-30 15:07:22 +0100

    Added grant to all sequences.

--- db_schema/setup-db.sh
+++ db_schema/setup-db.sh
@@ -10,5 +10,7 @@ createdb -E UTF-8 $DB_NAME
 psql -d $DB_NAME -f $DIR/lada_schema.sql
 psql -d $DB_NAME --command \
      "GRANT USAGE ON SCHEMA stammdaten, bund, land TO $ROLE_NAME;
+      GRANT SELECT
+            ON ALL SEQUENCES IN SCHEMA stammdaten, bund, land TO $ROLE_NAME;
       GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES
             ON ALL TABLES IN SCHEMA stammdaten, bund, land TO $ROLE_NAME;"
