9091b21ddd88eb6afcc07286ddac6acf66f7d124
Raimund Renkert <raimund.renkert@intevation.de>
2015-11-18 15:58:11 +0100

    Added view for reachable status values.

--- db_schema/lada_schema.sql
+++ db_schema/lada_schema.sql
@@ -1836,6 +1836,24 @@ CREATE TABLE verwaltungseinheit (
     latitude double precision
 );
 
+--
+-- Name status_erreichbar; Type: VIEW; Schema: stammdaten; Owner: -;
+--
+CREATE VIEW status_erreichbar AS (
+    SELECT DISTINCT k.wert_id,
+        j.wert_id AS cur_wert,
+        j.stufe_id AS cur_stufe
+    FROM stammdaten.status_kombi k
+    JOIN (SELECT r.zu_id,
+              kom.wert_id,
+              kom.stufe_id
+          FROM stammdaten.status_reihenfolge r
+          JOIN stammdaten.status_kombi kom
+          ON kom.id = r.von_id) j
+    ON j.zu_id = k.id
+);
+
+
 
 SET search_path = bund, pg_catalog;
 
