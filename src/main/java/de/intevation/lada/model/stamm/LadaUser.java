9938dd9c10b9c843829474e3d3b4867909cfe4da
Raimund Renkert <raimund.renkert@intevation.de>
2016-01-27 15:33:57 +0100

    Fixed code style.

--- src/main/java/de/intevation/lada/model/stamm/LadaUser.java
+++ src/main/java/de/intevation/lada/model/stamm/LadaUser.java
@@ -1,8 +1,19 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
 package de.intevation.lada.model.stamm;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
 /**
@@ -11,84 +22,83 @@ import java.util.List;
  */
 @Entity
 @Table(name="lada_user")
-@NamedQuery(name="LadaUser.findAll", query="SELECT l FROM LadaUser l")
 public class LadaUser implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	private Integer id;
+    @Id
+    private Integer id;
 
-	private String name;
+    private String name;
 
-	//bi-directional many-to-one association to Favorite
-	@OneToMany(mappedBy="ladaUser")
-	private List<Favorite> favorites;
+    //bi-directional many-to-one association to Favorite
+    @OneToMany(mappedBy="ladaUser")
+    private List<Favorite> favorites;
 
-	//bi-directional many-to-one association to FilterValue
-	@OneToMany(mappedBy="ladaUser")
-	private List<FilterValue> filterValues;
+    //bi-directional many-to-one association to FilterValue
+    @OneToMany(mappedBy="ladaUser")
+    private List<FilterValue> filterValues;
 
-	public LadaUser() {
-	}
+    public LadaUser() {
+    }
 
-	public Integer getId() {
-		return this.id;
-	}
+    public Integer getId() {
+        return this.id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public List<Favorite> getFavorites() {
-		return this.favorites;
-	}
+    public List<Favorite> getFavorites() {
+        return this.favorites;
+    }
 
-	public void setFavorites(List<Favorite> favorites) {
-		this.favorites = favorites;
-	}
+    public void setFavorites(List<Favorite> favorites) {
+        this.favorites = favorites;
+    }
 
-	public Favorite addFavorite(Favorite favorite) {
-		getFavorites().add(favorite);
-		favorite.setLadaUser(this);
+    public Favorite addFavorite(Favorite favorite) {
+        getFavorites().add(favorite);
+        favorite.setLadaUser(this);
 
-		return favorite;
-	}
+        return favorite;
+    }
 
-	public Favorite removeFavorite(Favorite favorite) {
-		getFavorites().remove(favorite);
-		favorite.setLadaUser(null);
+    public Favorite removeFavorite(Favorite favorite) {
+        getFavorites().remove(favorite);
+        favorite.setLadaUser(null);
 
-		return favorite;
-	}
+        return favorite;
+    }
 
-	public List<FilterValue> getFilterValues() {
-		return this.filterValues;
-	}
+    public List<FilterValue> getFilterValues() {
+        return this.filterValues;
+    }
 
-	public void setFilterValues(List<FilterValue> filterValues) {
-		this.filterValues = filterValues;
-	}
+    public void setFilterValues(List<FilterValue> filterValues) {
+        this.filterValues = filterValues;
+    }
 
-	public FilterValue addFilterValue(FilterValue filterValue) {
-		getFilterValues().add(filterValue);
-		filterValue.setLadaUser(this);
+    public FilterValue addFilterValue(FilterValue filterValue) {
+        getFilterValues().add(filterValue);
+        filterValue.setLadaUser(this);
 
-		return filterValue;
-	}
+        return filterValue;
+    }
 
-	public FilterValue removeFilterValue(FilterValue filterValue) {
-		getFilterValues().remove(filterValue);
-		filterValue.setLadaUser(null);
+    public FilterValue removeFilterValue(FilterValue filterValue) {
+        getFilterValues().remove(filterValue);
+        filterValue.setLadaUser(null);
 
-		return filterValue;
-	}
+        return filterValue;
+    }
 
 }
