ce16809f3dc016e28a0bf13ae0740f03656409c9
lore <lore@omicrontheta>
2016-03-20 10:39:21 +0200

    Add xz components for tar operations

---
+++ src/main/java/org/codehaus/plexus/archiver/tar/XZTarFile.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Codehaus.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.plexus.archiver.tar;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import org.codehaus.plexus.archiver.xz.XZUnArchiver;
+
+/**
+ *
+ * @author philip.lourandos
+ */
+public class XZTarFile extends TarFile 
+{
+
+    public XZTarFile ( File file ) 
+    {
+        super(file);
+    }
+
+    @Override
+    protected InputStream getInputStream( File file ) throws IOException
+    {
+        return XZUnArchiver.getXZInputStream( super.getInputStream( file ) );
+    }
+}
