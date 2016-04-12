2bb6dace64b62ba00b69ceac70fe0363b0c8cc5c
Raphael Pionke <Raphael.Pionke@t-systems.com>
2016-04-12 14:28:22 +0200

    use less files  in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 var builder = require('jenkins-js-builder');
 
-var gulp = require('gulp');
-gulp.task('lint', function () {
-});
-
 //
 // Bundle the modules.
 // See https://github.com/jenkinsci/js-builder
 //
 builder.bundle('src/main/js/buildactionresultsdisplay.js')
-    .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
-    .inDir('src/main/webapp/jsbundles');
+    .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3')
+    .less('src/main/less/bootstrapprefix.less')
+    .inDir('src/main/webapp/jsbundles').minify();
 
 builder.bundle('src/main/js/floatingBox.js')
-    .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
-    .inDir('src/main/webapp/jsbundles');
+    .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3')
+    .inDir('src/main/webapp/jsbundles').minify();
 
 /*builder.bundle('src/main/js/projectaction.js')
     .withExternalModuleMapping('bootstrap-detached', 'bootstrap:bootstrap3', {addDefaultCSS: true})
     .withExternalModuleMapping('jquery', 'bootstrap:bootstrap3')
  .inDir('src/main/webapp/jsbundles');*/
+
+builder.defineTasks(['test', 'bundle', 'rebundle']);
+builder.defineTask('lint', function () {
+});
