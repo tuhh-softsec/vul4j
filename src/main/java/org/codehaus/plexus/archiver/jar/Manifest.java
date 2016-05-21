a01612f7ffadb1c50469c5cfab556dd45325a15f
Michael Osipov <1983-01-06@gmx.net>
2016-05-21 02:19:42 +0200

    Remove obsolete import

--- src/main/java/org/codehaus/plexus/archiver/jar/Manifest.java
+++ src/main/java/org/codehaus/plexus/archiver/jar/Manifest.java
@@ -21,7 +21,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
