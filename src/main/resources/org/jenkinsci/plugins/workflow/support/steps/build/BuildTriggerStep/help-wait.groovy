707268d6b5fcb3e1d8b6f70302642df22cb914d1
Jesse Glick <jglick@cloudbees.com>
2016-09-22 12:38:15 -0400

    Oops, needed raw(…) to avoid escaping HTML tags.

--- src/main/resources/org/jenkinsci/plugins/workflow/support/steps/build/BuildTriggerStep/help-wait.groovy
+++ src/main/resources/org/jenkinsci/plugins/workflow/support/steps/build/BuildTriggerStep/help-wait.groovy
@@ -1,8 +1,8 @@
-p('''
+p(raw('''
     You may ask that this Pipeline build wait for completion of the downstream build.
     In that case the return value of the step is an object on which you can obtain the following read-only properties:
-    so you can inspect its <code>result</code> and so on.
-''')
+    so you can inspect its <code>.result</code> and so on.
+'''))
 raw(org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper.class.getResource("RunWrapper/help.html").text)
 p('''
     If you do not wait, this step succeeds so long as the downstream build can be added to the queue (it will not even have been started).
