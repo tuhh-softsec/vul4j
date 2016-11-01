a96db1030c8cb94f863c261e8a0b586426a72547
Tom Gottfried <tom@intevation.de>
2016-11-01 13:16:43 +0100

    Output more comprehensive message instead of token number.

--- src/main/java/de/intevation/lada/importer/laf/LafErrorListener.java
+++ src/main/java/de/intevation/lada/importer/laf/LafErrorListener.java
@@ -10,30 +10,31 @@ import org.antlr.v4.runtime.Recognizer;
 import de.intevation.lada.importer.ReportItem;
 
 public class LafErrorListener extends BaseErrorListener {
+
     public static LafErrorListener INSTANCE =
         new LafErrorListener();
 
     private List<ReportItem> errors = new ArrayList<ReportItem>();
 
     @Override
-    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
-                            int line, int charPositionInLine,
-                            String msg, RecognitionException e)
-    {
+    public void syntaxError(
+        Recognizer<?, ?> recognizer,
+        Object offendingSymbol,
+        int line,
+        int charPositionInLine,
+        String msg,
+        RecognitionException e
+    ) {
         String sourceName = "Parser";
         if (e != null && e.getCtx() != null) {
             sourceName = e.getCtx().getText();
         }
-        String token = "Token";
-        if (e != null && e.getOffendingToken() != null) {
-            e.getOffendingToken().getText();
-        }
+
         ReportItem err = new ReportItem();
         err.setKey(sourceName);
-        err.setValue(line + ":" + charPositionInLine + " - " + token);
+        err.setValue("line " + line + ": " + msg);
         err.setCode(670);
         this.errors.add(err);
-        System.err.println(err.getKey() + " - " +err.getValue() + " - " + err.getCode());
     }
 
     public void reset() {
