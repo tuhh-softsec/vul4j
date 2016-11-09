54bdaf6a4b0c68f5dd8a24295cea61a586ac46c6
Raimund Renkert <raimund.renkert@intevation.de>
2016-11-09 12:40:42 +0100

    Added validation to make sure a probe has only one 'entnahme ort'.

---
+++ src/main/java/de/intevation/lada/validation/rules/ort/HasEntnahmeOrt.java
@@ -0,0 +1,54 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.rules.ort;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import de.intevation.lada.model.land.Ortszuordnung;
+import de.intevation.lada.util.annotation.RepositoryConfig;
+import de.intevation.lada.util.data.QueryBuilder;
+import de.intevation.lada.util.data.Repository;
+import de.intevation.lada.util.data.RepositoryType;
+import de.intevation.lada.validation.Violation;
+import de.intevation.lada.validation.annotation.ValidationRule;
+import de.intevation.lada.validation.rules.Rule;
+
+@ValidationRule("Ort")
+public class HasEntnahmeOrt implements Rule {
+
+    @Inject
+    @RepositoryConfig(type=RepositoryType.RO)
+    private Repository repository;
+
+    @Override
+    public Violation execute(Object object) {
+        Ortszuordnung ort = (Ortszuordnung)object;
+        if (!"E".equals(ort.getOrtszuordnungTyp())) {
+            return null;
+        }
+
+        QueryBuilder<Ortszuordnung> builder = new QueryBuilder<Ortszuordnung>(
+            repository.entityManager("land"),
+            Ortszuordnung.class);
+        builder.and("probeId", ort.getProbeId());
+        List<Ortszuordnung> orte = repository.filterPlain(
+            builder.getQuery(),
+            "land");
+        for (Ortszuordnung o : orte) {
+            if ("E".equals(o.getOrtszuordnungTyp())) {
+                Violation violation = new Violation();
+                violation.addError("ortszuordnungsTyp", 611);
+                return violation;
+            }
+        }
+
+        return null;
+    }
+}
