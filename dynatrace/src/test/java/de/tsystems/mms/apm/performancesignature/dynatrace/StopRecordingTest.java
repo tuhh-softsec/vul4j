b2f7978d58317a5369bf70e82834ea7381f57065
Raphael Pionke <Raphael.Pionke@t-systems.com>
2016-12-22 14:27:16 +0100

    add stop recording tests

--- dynatrace/src/test/java/de/tsystems/mms/apm/performancesignature/dynatrace/StopRecordingTest.java
+++ dynatrace/src/test/java/de/tsystems/mms/apm/performancesignature/dynatrace/StopRecordingTest.java
@@ -21,7 +21,6 @@ import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 import de.tsystems.mms.apm.performancesignature.dynatrace.configuration.CredProfilePair;
 import de.tsystems.mms.apm.performancesignature.dynatrace.configuration.DynatraceServerConfiguration;
-import de.tsystems.mms.apm.performancesignature.dynatrace.configuration.DynatraceServerConfiguration.DescriptorImpl;
 import de.tsystems.mms.apm.performancesignature.dynatrace.rest.CommandExecutionException;
 import de.tsystems.mms.apm.performancesignature.dynatrace.rest.DTServerConnection;
 import de.tsystems.mms.apm.performancesignature.dynatrace.util.TestUtils;
@@ -29,11 +28,15 @@ import de.tsystems.mms.apm.performancesignature.util.PerfSigUtils;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
+import hudson.tasks.BatchFile;
+import hudson.tasks.Shell;
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import java.io.IOException;
@@ -43,11 +46,13 @@ import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.*;
 
-public class StartRecordingTest {
+public class StopRecordingTest {
 
     @ClassRule
     public static JenkinsRule j = new JenkinsRule();
     private static ListBoxModel dynatraceConfigurations;
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -60,11 +65,11 @@ public class StartRecordingTest {
         credProfilePairs.add(credProfilePair);
 
         configurations.add(new DynatraceServerConfiguration("PoC PerfSig", "https", "192.168.192.202", 8021, credProfilePairs,
-                false, DescriptorImpl.defaultDelay, DescriptorImpl.defaultRetryCount,
+                false, DynatraceServerConfiguration.DescriptorImpl.defaultDelay, DynatraceServerConfiguration.DescriptorImpl.defaultRetryCount,
                 false, 0, null, 0, null, null));
 
         configurations.add(new DynatraceServerConfiguration("PoC mobile Apps", "https", "192.168.194.209", 8021, credProfilePairs,
-                false, DescriptorImpl.defaultDelay, DescriptorImpl.defaultRetryCount,
+                false, DynatraceServerConfiguration.DescriptorImpl.defaultDelay, DynatraceServerConfiguration.DescriptorImpl.defaultRetryCount,
                 false, 0, null, 0, null, null));
 
         Jenkins.getInstance().save();
@@ -79,29 +84,34 @@ public class StartRecordingTest {
     }
 
     @Test
-    public void testContinuousSessionRecording() throws IOException {
+    public void testStopContinuousSessionRecording() throws IOException {
         DTServerConnection connection = TestUtils.createDTServerConnection(dynatraceConfigurations.get(0).name);
 
-        String result = null;
-        try {
-            result = connection.startRecording("testContinuousSessionRecording", "triggered by UnitTest",
-                    PerfSigStartRecording.DescriptorImpl.defaultRecordingOption, false, false);
-        } catch (CommandExecutionException e) {
-            assertTrue(e.getMessage().contains("continuous"));
-        } finally {
-            System.out.println("Result: " + result);
-        }
+        exception.expect(CommandExecutionException.class);
+        exception.expectMessage("error stop recording session:");
+        connection.stopRecording();
     }
 
     @Test
-    public void testDisabledContinuousSessionRecording() throws IOException {
+    public void testStopDisabledContinuousSessionRecording1() throws IOException {
+        DTServerConnection connection = TestUtils.createDTServerConnection(dynatraceConfigurations.get(1).name);
+
+        exception.expect(CommandExecutionException.class);
+        exception.expectMessage("error stop recording session: Failed to stop session recording");
+        connection.stopRecording();
+    }
+
+    @Test
+    public void testStopDisabledContinuousSessionRecording2() throws IOException {
         DTServerConnection connection = TestUtils.createDTServerConnection(dynatraceConfigurations.get(1).name);
 
         String result = connection.startRecording("testDisabledContinuousSessionRecording", "triggered by UnitTest",
                 PerfSigStartRecording.DescriptorImpl.defaultRecordingOption, false, true);
 
+        String result2 = connection.stopRecording();
+
+        assertEquals(result, result2);
         assertTrue(result.contains("testDisabledContinuousSessionRecording"));
-        assertFalse(connection.stopRecording().isEmpty());
     }
 
     @Test
@@ -110,8 +120,16 @@ public class StartRecordingTest {
 
         FreeStyleProject project = j.createFreeStyleProject();
         project.getBuildersList().add(new PerfSigStartRecording(dynatraceConfigurations.get(0).name, testCase));
+        //wait some time to get some data into the session
+        if (isWindows()) {
+            project.getBuildersList().add(new BatchFile("ping -n 10 127.0.0.1 > NUL"));
+        } else {
+            project.getBuildersList().add(new Shell("sleep 10"));
+        }
+        project.getBuildersList().add(new PerfSigStopRecording(dynatraceConfigurations.get(0).name));
         FreeStyleBuild build = project.scheduleBuild2(0).get();
 
+        Thread.sleep(100000);
         PerfSigEnvInvisAction invisAction = build.getAction(PerfSigEnvInvisAction.class);
 
         assertEquals(build.getResult(), Result.SUCCESS);
@@ -120,5 +138,12 @@ public class StartRecordingTest {
         assertTrue(invisAction.getTestCase().equals(testCase));
         assertFalse(invisAction.getTestRunID().isEmpty());
         assertTrue(invisAction.getTimeframeStart() != null);
+
+        DTServerConnection connection = TestUtils.createDTServerConnection(dynatraceConfigurations.get(0).name);
+        assertTrue(connection.getSessions().contains(invisAction.getSessionName()));
+    }
+
+    private boolean isWindows() {
+        return System.getProperty("os.name").startsWith("Windows");
     }
 }
