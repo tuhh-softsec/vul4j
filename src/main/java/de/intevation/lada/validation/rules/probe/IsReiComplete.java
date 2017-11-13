f0784d305ac8a209b912d142da5a245d3c67b4dd
Raimund Renkert <Raimund Renkert raimund.renkert@intevation.de>
2017-11-13 14:59:53 +0100

    Added new validation rules for REI attributes.

---
+++ src/main/java/de/intevation/lada/validation/rules/probe/IsReiComplete.java
@@ -0,0 +1,63 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.rules.probe;
+
+import javax.inject.Inject;
+
+import de.intevation.lada.model.land.Probe;
+import de.intevation.lada.util.annotation.RepositoryConfig;
+import de.intevation.lada.util.data.Repository;
+import de.intevation.lada.util.data.RepositoryType;
+import de.intevation.lada.validation.Violation;
+import de.intevation.lada.validation.annotation.ValidationRule;
+import de.intevation.lada.validation.rules.Rule;
+
+/**
+ * Validation rule for probe.
+ * Validates if the probe has valid REI attributes.
+ *
+ * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
+ */
+@ValidationRule("Probe")
+public class IsReiComplete implements Rule {
+
+    @Inject
+    @RepositoryConfig(type=RepositoryType.RO)
+    private Repository repository;
+
+    @Override
+    public Violation execute(Object object) {
+        Probe probe = (Probe)object;
+        Violation violation = new Violation();
+        System.out.println("null? : " + probe == null);
+        if (probe.getDatenbasisId() != null &&
+            probe.getDatenbasisId() != 3 &&
+            probe.getDatenbasisId() != 4) {
+            if (probe.getReiProgpunktGrpId() != null) {
+                violation.addError("reiProgpunktGruppe", 631);
+            }
+            if (probe.getKtaGruppeId() != null) {
+                violation.addError("ktaGruppe", 631);
+            }
+            if (violation.hasErrors()) {
+                return violation;
+            }
+            return null;
+        }
+        if (probe.getReiProgpunktGrpId() == null) {
+            violation.addWarning("reiProgpunktGruppe", 631);
+        }
+        if (probe.getKtaGruppeId() == null) {
+            violation.addWarning("ktaGruppe", 631);
+        }
+        if (violation.hasWarnings()) {
+            return violation;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/de/intevation/lada/validation/rules/status/IsReiComplete.java
@@ -0,0 +1,60 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.rules.status;
+
+import javax.inject.Inject;
+
+import org.apache.log4j.Logger;
+
+import de.intevation.lada.model.land.Messung;
+import de.intevation.lada.model.land.Probe;
+import de.intevation.lada.model.land.StatusProtokoll;
+import de.intevation.lada.util.annotation.RepositoryConfig;
+import de.intevation.lada.util.data.Repository;
+import de.intevation.lada.util.data.RepositoryType;
+import de.intevation.lada.validation.Violation;
+import de.intevation.lada.validation.annotation.ValidationRule;
+import de.intevation.lada.validation.rules.Rule;
+
+/**
+ * Validation rule for status.
+ *
+ * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
+ */
+@ValidationRule("Status")
+public class IsReiComplete implements Rule {
+
+    @Inject Logger logger;
+
+    @Inject
+    @RepositoryConfig(type=RepositoryType.RO)
+    private Repository repository;
+
+    @Override
+    public Violation execute(Object object) {
+        StatusProtokoll status = (StatusProtokoll)object;
+
+        Messung messung = repository.getByIdPlain(Messung.class, status.getMessungsId(), "land");
+        Probe probe = repository.getByIdPlain(Probe.class, messung.getProbeId(), "land");
+        if (probe.getDatenbasisId() != 3 &&
+            probe.getDatenbasisId() != 4) {
+            return null;
+        }
+        Violation violation = new Violation();
+        if (probe.getReiProgpunktGrpId() == null) {
+            violation.addError("probe#rei", 631);
+        }
+        if (probe.getKtaGruppeId() == null) {
+            violation.addError("probe#rei", 631);
+        }
+        if (violation.hasErrors()) {
+            return violation;
+        }
+        return null;
+    }
+}
