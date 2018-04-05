c82c65c9cb6b147b16721353c18501bdbcd32bdd
Nicolas Richeton <nicolas.richeton@gmail.com>
2018-04-05 21:58:05 +0200

    Build / test issues with Java 9 #184
    https://github.com/esigate/esigate/issues/184
    Fix Cas tests

--- esigate-cas/src/test/java/org/esigate/cas/CasAuthenticationHandlerTest.java
+++ esigate-cas/src/test/java/org/esigate/cas/CasAuthenticationHandlerTest.java
@@ -33,118 +33,121 @@ import junit.framework.TestCase;
  * CasAuthenticationHandlerTest
  */
 public class CasAuthenticationHandlerTest extends TestCase {
-	private Driver driver1;
-	private CasAuthenticationHandler handler;
-	private HttpClientRequestExecutor httpClientRequestExecutor;
-	private MockConnectionManager mockConnectionManager;
+    private Driver driver1;
+    private CasAuthenticationHandler handler;
+    private HttpClientRequestExecutor httpClientRequestExecutor;
+    private MockConnectionManager mockConnectionManager;
 
-	private HttpResponse createMockResponse(String entity) throws Exception {
-		HttpResponse response = new BasicHttpResponse(new ProtocolVersion("HTTP", 1, 1), HttpStatus.SC_OK, "OK");
-		HttpEntity httpEntity = new StringEntity(entity);
-		response.setEntity(httpEntity);
-		return response;
-	}
+    private HttpResponse createMockResponse(String entity) throws Exception {
+        HttpResponse response = new BasicHttpResponse(new ProtocolVersion("HTTP", 1, 1), HttpStatus.SC_OK, "OK");
+        HttpEntity httpEntity = new StringEntity(entity);
+        response.setEntity(httpEntity);
+        return response;
+    }
 
-	@Override
-	public void setUp() {
-		// Conf
-		Properties properties = new PropertiesBuilder() //
-				.set(Parameters.REMOTE_URL_BASE, "http://localhost:8080") //
-				.set(CasAuthenticationHandler.CAS_LOGIN_URL, "/loginurl") //
-				.build();
+    @Override
+    public void setUp() {
+        // Conf
+        Properties properties = new PropertiesBuilder() //
+                .set(Parameters.REMOTE_URL_BASE, "http://localhost:8080") //
+                .set(CasAuthenticationHandler.CAS_LOGIN_URL, "/loginurl") //
+                .build();
 
-		mockConnectionManager = new MockConnectionManager();
+        mockConnectionManager = new MockConnectionManager();
 
-		driver1 = Driver
-				.builder().setName(
-						"driver1")
-				.setProperties(properties)
-				.setRequestExecutorBuilder(HttpClientRequestExecutor.builder()
-						.setConnectionManager(mockConnectionManager).setCookieManager((CookieManager) ExtensionFactory
-								.getExtension(properties, Parameters.COOKIE_MANAGER, null)))
-				.build();
+        driver1 =
+                Driver.builder()
+                        .setName("driver1")
+                        .setProperties(properties)
+                        .setRequestExecutorBuilder(
+                                HttpClientRequestExecutor
+                                        .builder()
+                                        .setConnectionManager(mockConnectionManager)
+                                        .setCookieManager(
+                                                (CookieManager) ExtensionFactory.getExtension(properties,
+                                                        Parameters.COOKIE_MANAGER, null))).build();
 
-		httpClientRequestExecutor = (HttpClientRequestExecutor) driver1.getRequestExecutor();
-		handler = new CasAuthenticationHandler();
+        httpClientRequestExecutor = (HttpClientRequestExecutor) driver1.getRequestExecutor();
+        handler = new CasAuthenticationHandler();
 
-		handler.init(driver1, properties);
-	}
+        handler.init(driver1, properties);
+    }
 
-	public void testCasAuthenticationKo() throws Exception {
-		DriverRequest driverRequest = TestUtils.createDriverRequest(driver1);
-		OutgoingRequest outgoingRequest = httpClientRequestExecutor.createOutgoingRequest(driverRequest,
-				"http://localhost:8080", true);
-		FragmentEvent event = new FragmentEvent(driverRequest.getOriginalRequest(), outgoingRequest,
-				outgoingRequest.getContext());
-		CloseableHttpResponse httpResponse = BasicCloseableHttpResponse.adapt(createMockResponse("0"));
-		httpResponse.setHeader("Location", "http://localhost/loginurl?service=http");
-		event.setHttpResponse(httpResponse);
+    public void testCasAuthenticationKo() throws Exception {
+        DriverRequest driverRequest = TestUtils.createDriverRequest(driver1);
+        OutgoingRequest outgoingRequest =
+                httpClientRequestExecutor.createOutgoingRequest(driverRequest, "http://localhost:8080", true);
+        FragmentEvent event =
+                new FragmentEvent(driverRequest.getOriginalRequest(), outgoingRequest, outgoingRequest.getContext());
+        CloseableHttpResponse httpResponse = BasicCloseableHttpResponse.adapt(createMockResponse("0"));
+        httpResponse.setHeader("Location", "http://localhost/loginurl?service=http");
+        event.setHttpResponse(httpResponse);
 
-		HttpResponse responseOnceAuthenticated = createMockResponse("1");
-		mockConnectionManager.setResponse(responseOnceAuthenticated);
+        HttpResponse responseOnceAuthenticated = createMockResponse("1");
+        mockConnectionManager.setResponse(responseOnceAuthenticated);
 
-		handler.event(EventManager.EVENT_FRAGMENT_POST, event);
+        handler.event(EventManager.EVENT_FRAGMENT_POST, event);
 
-		// No extra request should be sent
-		assertNull(mockConnectionManager.getSentRequest());
-		// The response should be "unauthorized" as we cannot send the CAS ticket
-		assertEquals(401, event.getHttpResponse().getStatusLine().getStatusCode());
-	}
+        // No extra request should be sent
+        assertNull(mockConnectionManager.getSentRequest());
+        // The response should be "unauthorized" as we cannot send the CAS ticket
+        assertEquals(401, event.getHttpResponse().getStatusLine().getStatusCode());
+    }
 
-	public void testCasAuthenticationOk() throws Exception {
-		AttributePrincipal userPrincipal = new AttributePrincipal() {
-			private static final long serialVersionUID = 1L;
+    public void testCasAuthenticationOk() throws Exception {
+        AttributePrincipal userPrincipal = new AttributePrincipal() {
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public Map<String, Object> getAttributes() {
-				return null;
-			}
+            @Override
+            public Map<String, Object> getAttributes() {
+                return null;
+            }
 
-			@Override
-			public String getName() {
-				return "test";
-			}
+            @Override
+            public String getName() {
+                return "test";
+            }
 
-			@Override
-			public String getProxyTicketFor(String arg0) {
-				return "proxy_ticket";
-			}
-		};
-		IncomingRequest incomingRequest = TestUtils.createIncomingRequest().setUserPrincipal(userPrincipal).build();
-		DriverRequest driverRequest = new DriverRequest(incomingRequest, driver1, "/");
-		OutgoingRequest outgoingRequest = httpClientRequestExecutor.createOutgoingRequest(driverRequest,
-				"http://localhost:8080", true);
-		FragmentEvent event = new FragmentEvent(driverRequest.getOriginalRequest(), outgoingRequest,
-				outgoingRequest.getContext());
-		CloseableHttpResponse httpResponse = BasicCloseableHttpResponse.adapt(createMockResponse("0"));
-		httpResponse.setHeader("Location", "http://localhost/loginurl?service=http");
-		event.setHttpResponse(httpResponse);
+            @Override
+            public String getProxyTicketFor(String arg0) {
+                return "proxy_ticket";
+            }
+        };
+        IncomingRequest incomingRequest = TestUtils.createIncomingRequest().setUserPrincipal(userPrincipal).build();
+        DriverRequest driverRequest = new DriverRequest(incomingRequest, driver1, "/");
+        OutgoingRequest outgoingRequest =
+                httpClientRequestExecutor.createOutgoingRequest(driverRequest, "http://localhost:8080", true);
+        FragmentEvent event =
+                new FragmentEvent(driverRequest.getOriginalRequest(), outgoingRequest, outgoingRequest.getContext());
+        CloseableHttpResponse httpResponse = BasicCloseableHttpResponse.adapt(createMockResponse("0"));
+        httpResponse.setHeader("Location", "http://localhost/loginurl?service=http");
+        event.setHttpResponse(httpResponse);
 
-		HttpResponse responseOnceAuthenticated = createMockResponse("1");
-		mockConnectionManager.setResponse(responseOnceAuthenticated);
+        HttpResponse responseOnceAuthenticated = createMockResponse("1");
+        mockConnectionManager.setResponse(responseOnceAuthenticated);
 
-		handler.event(EventManager.EVENT_FRAGMENT_POST, event);
+        handler.event(EventManager.EVENT_FRAGMENT_POST, event);
 
-		// A new request should have been sent with the proxy ticket
-		assertNotNull(mockConnectionManager.getSentRequest());
-		assertEquals("/?ticket=proxy_ticket", mockConnectionManager.getSentRequest().getRequestLine().getUri());
-		assertEquals(200, event.getHttpResponse().getStatusLine().getStatusCode());
-		assertEquals("1", EntityUtils.toString(event.getHttpResponse().getEntity()));
-	}
+        // A new request should have been sent with the proxy ticket
+        assertNotNull(mockConnectionManager.getSentRequest());
+        assertEquals("/?ticket=proxy_ticket", mockConnectionManager.getSentRequest().getRequestLine().getUri());
+        assertEquals(200, event.getHttpResponse().getStatusLine().getStatusCode());
+        assertEquals("1", EntityUtils.toString(event.getHttpResponse().getEntity()));
+    }
 
-	public void testNoCasAuthenticationRequired() throws Exception {
-		DriverRequest driverRequest = TestUtils.createDriverRequest(driver1);
-		OutgoingRequest outgoingRequest = httpClientRequestExecutor.createOutgoingRequest(driverRequest,
-				"http://localhost:8080", true);
-		FragmentEvent event = new FragmentEvent(driverRequest.getOriginalRequest(), outgoingRequest,
-				outgoingRequest.getContext());
-		CloseableHttpResponse httpResponse = BasicCloseableHttpResponse.adapt(createMockResponse("0"));
-		event.setHttpResponse(httpResponse);
+    public void testNoCasAuthenticationRequired() throws Exception {
+        DriverRequest driverRequest = TestUtils.createDriverRequest(driver1);
+        OutgoingRequest outgoingRequest =
+                httpClientRequestExecutor.createOutgoingRequest(driverRequest, "http://localhost:8080", true);
+        FragmentEvent event =
+                new FragmentEvent(driverRequest.getOriginalRequest(), outgoingRequest, outgoingRequest.getContext());
+        CloseableHttpResponse httpResponse = BasicCloseableHttpResponse.adapt(createMockResponse("0"));
+        event.setHttpResponse(httpResponse);
 
-		handler.event(EventManager.EVENT_FRAGMENT_POST, event);
+        handler.event(EventManager.EVENT_FRAGMENT_POST, event);
 
-		// No extra request should be sent
-		assertNull(mockConnectionManager.getSentRequest());
-	}
+        // No extra request should be sent
+        assertNull(mockConnectionManager.getSentRequest());
+    }
 
 }
