02d33e5fd402019094a0b00ffd99c551bf971fec
Alexander Woestmann <awoestmann@intevation.de>
2018-11-23 14:12:37 +0100

    Add tag filter to database and query tools
     * Adds entries in the filter and filter_type table to filter probe entries
       for tags.
     * Adds support for multiselect tag filters to the query tools
     * TODO: Currently, only filtering for a single tag returns a result.
       Filtering for multiple tags just returns an empty data object.

--- src/main/java/de/intevation/lada/query/QueryTools.java
+++ src/main/java/de/intevation/lada/query/QueryTools.java
@@ -100,6 +100,7 @@ public class QueryTools
         String genericFilterSql = "";
         String sortSql = "";
         boolean subquery = false;
+        String tagFilterSql = "";
 
         for (GridColumnValue customColumn : customColumns) {
             boolean generic = false;
@@ -135,6 +136,18 @@ public class QueryTools
                     currentFilterString = currentFilterString.replace(":" + genTextValue, ":" + currentFilterParam);
                     subquery = true;
                     generic = true;
+                }
+
+                if (filterType.equals("tag")) {
+                    String[] values = filterValue.split(",");
+
+                    for (int i = 0; i < values.length; i++) {
+                        String s = values[i];
+                        filterValues.add(filter.getParameter(), s);
+                    }
+                    subquery = true;
+                    tagFilterSql = filter.getSql();
+                    continue;
                 }
 
                 //Check if Filter is an in filter
@@ -236,11 +249,23 @@ public class QueryTools
         }
         sql += sortSql;
         //TODO: Avoid using subqueries to use aliases in the where clause
-        //Append generic filter sql seperated from non-generic filters
+        //Append generic and/or tag filter sql seperated from other filters
         if (subquery) {
-            sql = "SELECT * FROM ( " + sql + " ) AS inner_query " + genericFilterSql;
+            sql = "SELECT * FROM ( " + sql + " ) AS inner_query " ;
+            boolean tagFilter = tagFilterSql.length() > 0;
+            boolean genericFilter = genericFilterSql.length() > 0;
+
+            if (tagFilter) {
+                sql += " WHERE " + tagFilterSql + " ";
+            }
+
+            if (genericFilter) {
+                sql = tagFilter == true ? sql + " AND " : sql + " WHERE ";
+                sql += genericFilterSql;
+            }
         }
         sql += " ;";
+        System.out.println(sql);
         javax.persistence.Query q = prepareQuery(
             sql,
             filterValues,
