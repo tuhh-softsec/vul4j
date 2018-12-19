7939b77f78bc7cb5699b56552b3f33fb607ef7d2
Michael Stanko <mstanko@bfs.de>
2018-12-19 15:16:58 +0100

    fix laf export
    - add translation of MESSPROGRAMM_S
    - add missing ort attributes
    - fix messwertexport if only NWG exists

--- src/main/java/de/intevation/lada/exporter/laf/LafCreator.java
+++ src/main/java/de/intevation/lada/exporter/laf/LafCreator.java
@@ -30,8 +30,12 @@ import de.intevation.lada.model.stammdaten.MessEinheit;
 import de.intevation.lada.model.stammdaten.MessStelle;
 import de.intevation.lada.model.stammdaten.Messgroesse;
 import de.intevation.lada.model.stammdaten.Ort;
+import de.intevation.lada.model.stammdaten.GemeindeUntergliederung;
 import de.intevation.lada.model.stammdaten.ProbenZusatz;
 import de.intevation.lada.model.stammdaten.Probenart;
+import de.intevation.lada.model.stammdaten.DatensatzErzeuger;
+import de.intevation.lada.model.stammdaten.MessprogrammKategorie;
+import de.intevation.lada.model.stammdaten.Probenehmer;
 import de.intevation.lada.model.stammdaten.ReiProgpunktGruppe;
 import de.intevation.lada.model.stammdaten.StatusKombi;
 import de.intevation.lada.util.annotation.AuthorizationConfig;
@@ -45,6 +49,8 @@ import de.intevation.lada.util.data.RepositoryType;
 import de.intevation.lada.util.data.Strings;
 import de.intevation.lada.util.rest.Response;
 
+//import org.apache.log4j.Logger;
+
 /**
  * This creator produces a LAF conform String containing all information about
  * a single {@link LProbe} object including subobjects like
@@ -56,6 +62,8 @@ import de.intevation.lada.util.rest.Response;
 public class LafCreator
 implements Creator
 {
+//    @Inject private Logger logger;
+
     // Some format strings corresponding to LAF notation
     private static final String KEY_FORMAT = "%-30s";
     private static final String DEFAULT_FORMAT = "%s";
@@ -166,10 +174,32 @@ implements Creator
         laf += lafLine("PROBE_ID", probe.getExterneProbeId(), CN);
         laf += probe.getHauptprobenNr() == null ?
             "" : lafLine("HAUPTPROBENNUMMER", probe.getHauptprobenNr(), CN);
-        laf += probe.getBaId() == null ?
-            "" : lafLine("MESSPROGRAMM_S", probe.getBaId(), CN);
+        if (probe.getBaId() != null && probe.getDatenbasisId() != null) {
+            switch (probe.getDatenbasisId()) {
+            case 4: 
+                if (probe.getBaId() == 1 ) {
+                    laf += lafLine("MESSPROGRAMM_S", 4, CN);
+                } else if (probe.getBaId() == 2 ) {
+                    laf += lafLine("MESSPROGRAMM_S", 5, CN);
+                } else {
+                    laf += lafLine("MESSPROGRAMM_S", 6, CN);
+                }
+                break;
+            case 7:
+                if (probe.getBaId() > 3 ) {
+                    laf += lafLine("MESSPROGRAMM_S", "\""+(char)probe.getBaId().intValue()+"\"");
+                } else {
+                    laf += lafLine("MESSPROGRAMM_S", probe.getBaId(), CN);
+                }
+                break;
+            default:
+                laf += lafLine("MESSPROGRAMM_S", probe.getBaId(), CN);
+                break;
+            }
+        }
         laf += probe.getProbenartId() == null ?
             "" : lafLine("PROBENART", probenart, CN);
+        laf += lafLine("ZEITBASIS_S", "2");
         laf += probe.getSolldatumBeginn() == null ?
             "" : lafLine("SOLL_DATUM_UHRZEIT_A",
                 toUTCString(probe.getSolldatumBeginn()));
@@ -189,19 +219,33 @@ implements Creator
                 probe.getMediaDesk().replaceAll(" ", "").substring(2), CN);
         laf += probe.getTest() == Boolean.TRUE ?
             lafLine("TESTDATEN", "1") : lafLine("TESTDATEN", "0");
+        if (probe.getErzeugerId() != null) {
+            DatensatzErzeuger erz = repository.getByIdPlain(
+                DatensatzErzeuger.class, probe.getErzeugerId(), "stamm");
+            laf += lafLine("ERZEUGER", erz.getDatensatzErzeugerId(), CN);
+        }
+        if (probe.getMplId() != null) {
+            MessprogrammKategorie mpkat = repository.getByIdPlain(
+                MessprogrammKategorie.class, probe.getMplId(), "stamm");
+            laf += lafLine("MESSPROGRAMM_LAND", mpkat.getCode(), CN);
+        }
+        if (probe.getProbeNehmerId() != null) {
+            Probenehmer prn = repository.getByIdPlain(
+                Probenehmer.class, probe.getProbeNehmerId(), "stamm");
+            laf += lafLine("PROBENAHMEINSTITUTION", prn.getPrnId(), CN);
+        }
         if (probe.getReiProgpunktGrpId() != null) {
             ReiProgpunktGruppe rpg = repository.getByIdPlain(
                 ReiProgpunktGruppe.class, probe.getReiProgpunktGrpId(), "stamm");
             laf += lafLine("REI_PROGRAMMPUNKTGRUPPE", rpg.getReiProgPunktGruppe(), CN);
         }
-        laf += lafLine("ZEITBASIS_S", "2");
-        laf += writeOrt(probe);
         for (ZusatzWert zw : zusatzwerte) {
             laf += writeZusatzwert(zw);
         }
         for (KommentarP kp : kommentare) {
             laf += writeKommentar(kp);
         }
+        laf += writeOrt(probe);
         laf += writeMessung(probe, messungen);
         return laf;
     }
@@ -251,62 +295,88 @@ implements Creator
         String laf = "";
         for(Ortszuordnung o : orte) {
             String type = "";
-            if ("E".equals(o.getOrtszuordnungTyp())) {
-                type = "P_";
-            }
-            else if ("U".equals(o.getOrtszuordnungTyp()) ||
+            if ("E".equals(o.getOrtszuordnungTyp()) ||
                 "R".equals(o.getOrtszuordnungTyp())) {
-                type = "U_";
+                laf += writeOrtData(o,"E_");
+            }
+        }
+        for(Ortszuordnung o : orte) {
+            String type = "";
+            if ("U".equals(o.getOrtszuordnungTyp()) ||
+                "R".equals(o.getOrtszuordnungTyp())) {
                 laf += "%URSPRUNGSORT%\n";
+                laf += writeOrtData(o,"U_");
             }
-            else {
-                continue;
-            }
-            if (o.getOrtszusatztext() != null &&
+        }
+        return laf;
+    }
+
+    /**
+     * Write {@link LOrt} attributes.
+     *
+     * @param Ortszuordnung
+     * @return LAF conform string
+     */
+    @SuppressWarnings("unchecked")
+    private String writeOrtData(Ortszuordnung o, String typePrefix) {
+        String laf = "";
+        if (o.getOrtszusatztext() != null &&
                 o.getOrtszusatztext().length() > 0) {
-                laf += lafLine(type + "ORTS_ZUSATZTEXT",
+                laf += lafLine(typePrefix + "ORTS_ZUSATZTEXT",
                     o.getOrtszusatztext(), CN);
-            }
-            QueryBuilder<Ort> oBuilder =
-                new QueryBuilder<Ort>(
-                    repository.entityManager(Strings.STAMM),
-                    Ort.class);
-            oBuilder.and("id", o.getOrtId());
-            List<Ort> sOrte=
-                (List<Ort>)repository.filter(
-                    oBuilder.getQuery(),
-                    Strings.STAMM).getData();
+        }
+        QueryBuilder<Ort> oBuilder =
+            new QueryBuilder<Ort>(
+                repository.entityManager(Strings.STAMM),
+                Ort.class);
+        oBuilder.and("id", o.getOrtId());
+        List<Ort> sOrte=
+            (List<Ort>)repository.filter(
+                oBuilder.getQuery(),
+                Strings.STAMM).getData();
 
-            if (sOrte.get(0).getStaatId() != null) {
-                laf += lafLine(type + "HERKUNFTSLAND_S",
-                    String.format("%08d", sOrte.get(0).getStaatId()));
-            }
+        if (sOrte.get(0).getStaatId() != null) {
+            laf += lafLine(typePrefix + "HERKUNFTSLAND_S",
+                String.format("%08d", sOrte.get(0).getStaatId()));
+        }
 
-            if (sOrte.get(0).getGemId() != null &&
-                sOrte.get(0).getGemId().length() > 0) {
-                laf += lafLine(type + "GEMEINDESCHLUESSEL",
-                    sOrte.get(0).getGemId());
-            }
+        if (sOrte.get(0).getGemId() != null && 
+            sOrte.get(0).getGemId().length() > 0) {
+            laf += lafLine(typePrefix + "GEMEINDESCHLUESSEL",
+                sOrte.get(0).getGemId());
+        }
 
-            String koord = String.format("%02d", sOrte.get(0).getKdaId());
-            koord += " \"";
-            koord += sOrte.get(0).getKoordXExtern() + "\" \"";
-            koord += sOrte.get(0).getKoordYExtern() + "\"";
-            laf += lafLine(type + "KOORDINATEN_S", koord);
+        if (sOrte.get(0).getNutsCode() != null && 
+            sOrte.get(0).getNutsCode().length() > 0) {
+            laf += lafLine(typePrefix + "NUTS_CODE",
+                sOrte.get(0).getNutsCode());
+        }
 
-            if (probe.getReiProgpunktGrpId() != null) {
-                lafLine(type + "ORTS_ZUSATZCODE",
-                    sOrte.get(0).getKtaGruppeId() + sOrte.get(0).getGemId());
-            }
-            else if (sOrte.get(0).getOzId() != null &&
-                sOrte.get(0).getOzId().length() > 0) {
-                laf += lafLine(type + "ORTS_ZUSATZCODE",
-                    sOrte.get(0).getOzId(), CN);
-            }
-            if (sOrte.get(0).getHoeheUeberNn() != null) {
-                laf += lafLine(type + "HOEHE_NN",
-                    String.format("%f", sOrte.get(0).getHoeheUeberNn()));
-            }
+        if (sOrte.get(0).getGemUntId() != null) {
+            GemeindeUntergliederung gu = repository.getByIdPlain(
+                GemeindeUntergliederung.class, sOrte.get(0).getGemUntId(), "stamm");
+            laf += lafLine(typePrefix + "ORTS_ZUSATZKENNZAHL",
+                gu.getOzkId(), CN);
+        }
+
+        String koord = String.format("%02d", sOrte.get(0).getKdaId());
+        koord += " \"";
+        koord += sOrte.get(0).getKoordXExtern() + "\" \"";
+        koord += sOrte.get(0).getKoordYExtern() + "\"";
+        laf += lafLine(typePrefix + "KOORDINATEN_S", koord);
+
+        if ("E_".equals(typePrefix) && sOrte.get(0).getOzId() != null) {
+            lafLine(typePrefix + "ORTS_ZUSATZCODE", sOrte.get(0).getOzId(), CN);
+        }
+        else if ("U_".equals(typePrefix) && "R".equals(o.getOrtszuordnungTyp())) {
+            laf += lafLine(typePrefix + "ORTS_ZUSATZCODE", sOrte.get(0).getOrtId(), CN);
+        }
+        else if ("U_".equals(typePrefix) && sOrte.get(0).getOzId() != null) {
+            laf += lafLine(typePrefix + "ORTS_ZUSATZCODE", sOrte.get(0).getOzId(), CN);
+        } 
+        if (sOrte.get(0).getHoeheUeberNn() != null) {
+            laf += lafLine(typePrefix + "HOEHE_NN",
+                String.format("%f", sOrte.get(0).getHoeheUeberNn()));
         }
         return laf;
     }
@@ -362,11 +432,10 @@ implements Creator
             Response kommentar = repository.filter(kommBuilder.getQuery(), Strings.LAND);
             List<KommentarM> kommentare = (List<KommentarM>)kommentar.getData();
             laf += lafLine("MESSUNGS_ID", m.getExterneMessungsId().toString());
-            laf += lafLine("NEBENPROBENNUMMER", m.getNebenprobenNr(), CN);
+            laf += m.getNebenprobenNr() == null ?
+                "" : lafLine("NEBENPROBENNUMMER", m.getNebenprobenNr(), CN);
             laf += m.getMesszeitpunkt() == null ?
-                "" : lafLine(
-                    "MESS_DATUM_UHRZEIT",
-                    toUTCString(m.getMesszeitpunkt()));
+                "" : lafLine("MESS_DATUM_UHRZEIT", toUTCString(m.getMesszeitpunkt()));
             laf += m.getMessdauer() == null ?
                 "" : lafLine("MESSZEIT_SEKUNDEN", m.getMessdauer().toString());
             laf += m.getMmtId() == null ?
@@ -482,7 +551,7 @@ implements Creator
         String value = "\"" + groessen.get(0).getMessgroesse() + "\"";
         value += " ";
         value += mw.getMesswertNwg() == null ? " " : mw.getMesswertNwg();
-        value += mw.getMesswert();
+        value += mw.getMesswertNwg() == null ? mw.getMesswert() : mw.getNwgZuMesswert();
         value += " \"" + einheiten.get(0).getEinheit() + "\"";
         value += mw.getMessfehler() == null ? " 0.0" : " " + mw.getMessfehler();
         if (mw.getGrenzwertueberschreitung() == null ||
