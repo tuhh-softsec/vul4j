9f0572a4a2f01d015df6c739726234a85bfcbbed
Michael Stanko <mstanko@bfs.de>
2019-06-21 13:48:37 +0200

    fix MesswertNormalizer
      - enable null values
      - includ nwgZuMesswert in normalisation

--- src/main/java/de/intevation/lada/util/data/MesswertNormalizer.java
+++ src/main/java/de/intevation/lada/util/data/MesswertNormalizer.java
@@ -6,7 +6,7 @@ import javax.inject.Inject;
 
 import de.intevation.lada.model.land.Messwert;
 import de.intevation.lada.model.stammdaten.MassEinheitUmrechnung;
-import de.intevation.lada.model.stammdaten.MessEinheit;
+
 import de.intevation.lada.model.stammdaten.Umwelt;
 import de.intevation.lada.util.annotation.RepositoryConfig;
 
@@ -33,25 +33,32 @@ public class MesswertNormalizer {
         Integer mehIdToConvertTo = umwelt.getMehId();
 
         for (Messwert messwert: messwerte) {
-            MessEinheit einheit = defaultRepo.getByIdPlain(MessEinheit.class, messwert.getMehId(), Strings.STAMM);
+            if (mehIdToConvertTo != messwert.getMehId()) {
+                //Get the conversion factor
+                QueryBuilder<MassEinheitUmrechnung> builder = new QueryBuilder<>(
+                    defaultRepo.entityManager(Strings.STAMM),
+                    MassEinheitUmrechnung.class
+                );
+                builder.and("mehIdZu", mehIdToConvertTo);
+                builder.and("mehVon", messwert.getMehId());
+                List<MassEinheitUmrechnung> meu = defaultRepo.filterPlain(builder.getQuery(), Strings.STAMM);
+                if (meu.size() == 0) {
+                    //No suitable conversion found: continue
+                    continue;
+                }
+                Float factor = meu.get(0).getFaktor();
 
-            //Get the conversion factor
-            QueryBuilder<MassEinheitUmrechnung> builder = new QueryBuilder<>(
-                defaultRepo.entityManager(Strings.STAMM),
-                MassEinheitUmrechnung.class
-            );
-            builder.and("mehIdZu", mehIdToConvertTo);
-            builder.and("mehVon", einheit);
-            List<MassEinheitUmrechnung> meu = defaultRepo.filterPlain(builder.getQuery(), Strings.STAMM);
-            if (meu.size() == 0) {
-                //No suitable conversion found: continue
-                continue;
+                //Update einheit
+                messwert.setMehId(mehIdToConvertTo);
+                //Update messwert
+                if (messwert.getMesswert() != null) {
+                    messwert.setMesswert(messwert.getMesswert() * factor);
+                }
+                //update nwgZuMesswert
+                if (messwert.getNwgZuMesswert() != null) {
+                    messwert.setNwgZuMesswert(messwert.getNwgZuMesswert() * factor);
+                }
             }
-            Float factor = meu.get(0).getFaktor();
-
-            //Update messwert
-            messwert.setMehId(mehIdToConvertTo);
-            messwert.setMesswert(messwert.getMesswert() * factor);
         }
         return messwerte;
     }
