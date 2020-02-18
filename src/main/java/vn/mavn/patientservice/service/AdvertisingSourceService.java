b2de248c352b6a0ce00da864170af9f4eb6f4cda
Nghiem Nguyen <kukull.ncn@gmail.com>
2020-02-18 23:05:42 +0700

    add logic edit advert

--- src/main/java/vn/mavn/patientservice/service/AdvertisingSourceService.java
+++ src/main/java/vn/mavn/patientservice/service/AdvertisingSourceService.java
@@ -1,9 +1,13 @@
 package vn.mavn.patientservice.service;
 
 import vn.mavn.patientservice.dto.AdvertisingSourceAddDto;
+import vn.mavn.patientservice.dto.AdvertisingSourceEditDto;
 import vn.mavn.patientservice.entity.AdvertisingSource;
 
 public interface AdvertisingSourceService {
 
   AdvertisingSource addNew(AdvertisingSourceAddDto advertisingSourceAddDto);
+
+  AdvertisingSource editAdvertSource(AdvertisingSourceEditDto advertisingSourceEditDto);
+
 }
--- src/main/java/vn/mavn/patientservice/service/impl/AdvertisingSourceServiceImpl.java
+++ src/main/java/vn/mavn/patientservice/service/impl/AdvertisingSourceServiceImpl.java
@@ -1,11 +1,14 @@
 package vn.mavn.patientservice.service.impl;
 
 import java.util.Collections;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import vn.mavn.patientservice.dto.AdvertisingSourceAddDto;
+import vn.mavn.patientservice.dto.AdvertisingSourceEditDto;
 import vn.mavn.patientservice.entity.AdvertisingSource;
 import vn.mavn.patientservice.exception.ConflictException;
+import vn.mavn.patientservice.exception.NotFoundException;
 import vn.mavn.patientservice.repository.AdvertisingSourceRepository;
 import vn.mavn.patientservice.service.AdvertisingSourceService;
 
@@ -27,6 +30,23 @@ public class AdvertisingSourceServiceImpl implements AdvertisingSourceService {
         .name(advertisingSourceAddDto.getName().trim()).build();
     advertisingSource.setCreatedBy(advertisingSourceAddDto.getCreatedBy());
     advertisingSource.setUpdatedBy(advertisingSourceAddDto.getCreatedBy());
+    return advertisingSourceRepository.save(advertisingSource);
+  }
+
+  @Override
+  public AdvertisingSource editAdvertSource(AdvertisingSourceEditDto advertisingSourceEditDto) {
+    //TODO: check exist advertising_source
+    AdvertisingSource advertisingSource = advertisingSourceRepository
+        .findById(advertisingSourceEditDto.getId()).orElseThrow(() -> new NotFoundException(
+            Collections.singletonList("err-advertising-not-found")));
+    //TODO: valid name duplicate
+    advertisingSourceRepository
+        .findByNameNotEqualId(advertisingSourceEditDto.getName().trim().toUpperCase(),
+            advertisingSourceEditDto.getId())
+        .ifPresent(advert -> {
+          throw new ConflictException(Collections.singletonList("err-advertising-duplicate-name"));
+        });
+    BeanUtils.copyProperties(advertisingSourceEditDto, advertisingSource);
     return advertisingSourceRepository.save(advertisingSource);
   }
 }
