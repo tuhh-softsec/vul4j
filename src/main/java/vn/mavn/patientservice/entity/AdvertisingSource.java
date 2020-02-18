3b1e1db12ddba167fdc379b57bfcbc543f69ed94
Nghiem Nguyen <kukull.ncn@gmail.com>
2020-02-18 22:47:46 +0700

    add repo, entity, dto

---
+++ src/main/java/vn/mavn/patientservice/entity/AdvertisingSource.java
@@ -0,0 +1,26 @@
+package vn.mavn.patientservice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import vn.mavn.patientservice.entity.listener.EntityListener;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pm_advertising_source")
+@Builder
+@EntityListeners(EntityListener.class)
+public class AdvertisingSource extends BaseIdEntity {
+
+  private String name;
+  private String description;
+
+}
---
+++ src/main/java/vn/mavn/patientservice/repository/AdvertisingSourceRepository.java
@@ -0,0 +1,13 @@
+package vn.mavn.patientservice.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import vn.mavn.patientservice.entity.AdvertisingSource;
+
+public interface AdvertisingSourceRepository extends JpaRepository<AdvertisingSource, Long> {
+
+  @Query("select a from AdvertisingSource a where upper(a.name) =:name ")
+  Optional<AdvertisingSource> findByName(String name);
+
+}
