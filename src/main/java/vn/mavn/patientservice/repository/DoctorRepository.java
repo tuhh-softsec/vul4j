bfccfc4de6da96f9db2cfad653748e83cf37a947
PMTuan <phamminhtuan@unimarmores.com>
2020-02-19 20:01:03 +0700

    update code

--- src/main/java/vn/mavn/patientservice/repository/DoctorRepository.java
+++ src/main/java/vn/mavn/patientservice/repository/DoctorRepository.java
@@ -12,12 +12,16 @@ import vn.mavn.patientservice.entity.Doctor;
 public interface DoctorRepository extends JpaRepository<Doctor, Long>,
     JpaSpecificationExecutor<Doctor> {
 
+  @Query("select d from Doctor d where unaccent(d.name) = unaccent(:name)")
   Optional<Doctor> findByName(String name);
 
+  @Query("select d from Doctor d where unaccent(d.name) = unaccent(:name) and d.id <> :id")
   Optional<Doctor> findByNameAndIdNot(String name, Long id);
 
+  @Query("select d from Doctor d where d.phone = :phone")
   Optional<Doctor> findByPhone(String phone);
 
+  @Query("select d from Doctor d where d.phone = :phone and d.id <> :id")
   Optional<Doctor> findByPhoneAndIdNot(String phone, Long id);
 
   @Modifying
