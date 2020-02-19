5fc27dd71ab25edbbf1d361c35f475d14a179782
Nghiem Nguyen <kukull.ncn@gmail.com>
2020-02-19 22:56:21 +0700

    add new patient

--- src/main/java/vn/mavn/patientservice/dto/PatientAddDto.java
+++ src/main/java/vn/mavn/patientservice/dto/PatientAddDto.java
@@ -1,6 +1,8 @@
 package vn.mavn.patientservice.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,12 +18,18 @@ public class PatientAddDto {
 
   @NotBlank(message = "err-patient-name-is-mandatory")
   private String name;
-  @NotBlank(message = "err-patient-age-is-mandatory")
+  @NotNull(message = "err-patient-age-is-mandatory")
   private Integer age;
   @NotBlank(message = "err-patient-address-is-mandatory")
   private String address;
+  @Pattern(regexp = "^(\\+84|0)((2[0-9]{9})|((3|5|7|8|9){1}([0-9]{8})))$",
+      message = "err-patient-phone-invalid")
   private String phone;
+  @Pattern(regexp = "^(\\+84|0)((2[0-9]{9})|((3|5|7|8|9){1}([0-9]{8})))$",
+      message = "err-patient-za-lo-phone-invalid")
   private String zaLoPhone;
+  @Pattern(regexp = "^(\\+84|0)((2[0-9]{9})|((3|5|7|8|9){1}([0-9]{8})))$",
+      message = "err-patient-other-phone-invalid")
   private String otherPhone;
 
 }
