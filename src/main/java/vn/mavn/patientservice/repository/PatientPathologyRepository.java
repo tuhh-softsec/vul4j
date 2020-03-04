1eed023b1a423c52fa7a166cf9f3578f0b19ee21
minhlee <lengocminh@unimarmores.com>
2020-03-04 17:51:11 +0700

    update detail patient includes pathology information

--- src/main/java/vn/mavn/patientservice/repository/PatientPathologyRepository.java
+++ src/main/java/vn/mavn/patientservice/repository/PatientPathologyRepository.java
@@ -1,8 +1,12 @@
 package vn.mavn.patientservice.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import vn.mavn.patientservice.entity.PatientPathology;
 
 public interface PatientPathologyRepository extends CrudRepository<PatientPathology, Long> {
 
+  @Query("select pp from PatientPathology pp where pp.patientId = :patientId")
+  List<PatientPathology> findAllByPatientId(Long patientId);
 }
