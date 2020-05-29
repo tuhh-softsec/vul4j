39a53a830d8a2a6c1250d80b36657a4ec5134b8d
Mark McKelvy <mckelvym@usgs.gov>
2020-05-29 08:56:29 -0400

    Address: May expose internal representation by returning reference to mutable object.
    Returning a reference to a mutable object value stored in one of the object's fields exposes the internal representation of the object.  If instances are accessed by untrusted code, and unchecked changes to the mutable object would compromise security or other important properties, you will need to do something different. Returning a new copy of the object is better approach in many situations.
    
    Bug kind and pattern: EI - EI_EXPOSE_REP

--- gov.usgs.warc.iridium.sbd.decoder/src/main/java/gov/usgs/warc/iridium/sbd/decoder/parser/elements/Payload.java
+++ gov.usgs.warc.iridium.sbd.decoder/src/main/java/gov/usgs/warc/iridium/sbd/decoder/parser/elements/Payload.java
@@ -81,4 +81,26 @@ public class Payload
 		return getPayloadType().getPayloadDecoder().decode(this, p_DataTypes,
 				p_DecodeOrder);
 	}
+
+	/**
+	 * Method added to address SpotBugs-reported issue: May expose internal
+	 * representation by returning reference to mutable object.
+	 *
+	 * Returning a reference to a mutable object value stored in one of the
+	 * object's fields exposes the internal representation of the object.  If
+	 * instances are accessed by untrusted code, and unchecked changes to the
+	 * mutable object would compromise security or other important properties,
+	 * you will need to do something different. Returning a new copy of the
+	 * object is better approach in many situations.
+	 *
+	 * Bug kind and pattern: EI - EI_EXPOSE_REP
+	 *
+	 * @return clone of {@link #payload}
+	 * @author mckelvym
+	 * @since May 29, 2020
+	 */
+	public byte[] getPayload()
+	{
+		return payload.clone();
+	}
 }
