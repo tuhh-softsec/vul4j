f2cdcd5b4e4c69a455dff4eb0077141ba5ef6231
Jonas Buermeyer <jbuermeyer@bfs.de>
2020-06-10 08:04:09 +0200

    Added validation rules for probe object: HasBetriebsart and HasDatenbasis

---
+++ src/main/java/de/intevation/lada/validation/rules/probe/HasBetriebsart.java
@@ -0,0 +1,34 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+package de.intevation.lada.validation.rules.probe;
+
+import de.intevation.lada.model.land.Probe;
+import de.intevation.lada.validation.Violation;
+import de.intevation.lada.validation.annotation.ValidationRule;
+import de.intevation.lada.validation.rules.Rule;
+
+/**
+ * Validation rule for probe.
+ * Validates if the probe has a "probeart".
+ *
+ * @author <a href="mailto:rrenkert@intevation.de">Raimund Renkert</a>
+ */
+@ValidationRule("Probe")
+public class HasBetriebsart implements Rule {
+
+    @Override
+    public Violation execute(Object object) {
+        Probe probe = (Probe)object;
+        if (probe.getBaId() == null) {
+            Violation violation = new Violation();
+            violation.addError("betriebsart", 631);
+            return violation;
+        }
+        return null;
+    }
+}
