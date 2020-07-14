88979c26f75ad8f83d5b4b40f73b8f53e3b3401a
Jonas Buermeyer <jbuermeyer@bfs.de>
2020-07-14 14:12:59 +0200

    Added notifications to MessungValidator and ProbeValidator

--- src/main/java/de/intevation/lada/validation/MessungValidator.java
+++ src/main/java/de/intevation/lada/validation/MessungValidator.java
@@ -48,6 +48,9 @@ public class MessungValidator implements Validator {
                 if (result.hasErrors()) {
                     violations.addErrors(result.getErrors());
                 }
+                if (result.hasNotifications()){
+                  violations.addNotifications(result.getNotifications());
+                }
             }
         }
         return violations;
