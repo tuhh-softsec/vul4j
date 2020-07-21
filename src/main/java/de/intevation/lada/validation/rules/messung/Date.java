005b31f46ad8f6474b60635920dd51e39876093b
Jonas Buermeyer <jbuermeyer@bfs.de>
2020-07-21 14:14:59 +0200

    Fixed Nullpointer-Exceptions in validation rules

--- src/main/java/de/intevation/lada/validation/rules/messung/Date.java
+++ src/main/java/de/intevation/lada/validation/rules/messung/Date.java
@@ -57,7 +57,7 @@ public class Date implements Rule {
         if (probe.getProbeentnahmeBeginn() != null && probe.getProbeentnahmeBeginn().after(messung.getMesszeitpunkt()) ||
             probe.getProbeentnahmeEnde() != null
               && probe.getProbeentnahmeEnde().after(messung.getMesszeitpunkt()) 
-              && ( probe.getProbenartId() == 3 || probe.getProbenartId() == 9)
+              && probe.getProbenartId()!=null && ( probe.getProbenartId() == 3 || probe.getProbenartId() == 9)
         ) {
             Violation violation = new Violation();
             violation.addWarning(
