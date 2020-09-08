85c6ff29f22bc072096997a01c2250f74d39b8d2
Jonas Buermeyer <jbuermeyer@bfs.de>
2020-09-08 14:07:49 +0200

    Fix Nullpointer on ProbeartId in HasProbenahmeEnde rule

--- src/main/java/de/intevation/lada/validation/rules/probe/HasProbeentnahmeEnde.java
+++ src/main/java/de/intevation/lada/validation/rules/probe/HasProbeentnahmeEnde.java
@@ -27,7 +27,8 @@ public class HasProbeentnahmeEnde implements Rule {
     public Violation execute(Object object) {
         Probe probe = (Probe)object;
         Timestamp ende = probe.getProbeentnahmeEnde();
-        if (probe.getDatenbasisId() != null && (probe.getDatenbasisId() == 4 && probe.getProbenartId()==9 && ende == null) || ((probe.getProbenartId()==9 || probe.getProbenartId()==3) && probe.getDatenbasisId()!=4 && ende == null)){
+        if ( probe.getDatenbasisId() != null && probe.getProbenartId()!= null && ((probe.getDatenbasisId() == 4 && probe.getProbenartId()==9 && ende == null) 
+             || ((probe.getProbenartId()==9 || probe.getProbenartId()==3) && probe.getDatenbasisId()!=4 && ende == null)) ){
             Violation violation = new Violation();
             violation.addWarning("probeentnahmeEnde", 631);
             return violation;
