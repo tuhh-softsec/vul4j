64f8e4832e185f959503939430aa4b273cc8eb0f
bqcuong <cuong.bui@tuhh.de>
2021-04-28 10:44:00 +0200

    reproduce

--- src/main/java/org/apache/commons/compress/archivers/examples/Expander.java
+++ src/main/java/org/apache/commons/compress/archivers/examples/Expander.java
@@ -238,7 +238,7 @@ public class Expander {
 
     private void expand(ArchiveEntrySupplier supplier, EntryWriter writer, File targetDirectory)
         throws IOException {
-        String targetDirPath = targetDirectory.getCanonicalPath() + File.separatorChar;
+        String targetDirPath = targetDirectory.getCanonicalPath();
         ArchiveEntry nextEntry = supplier.getNextReadableEntry();
         while (nextEntry != null) {
             File f = new File(targetDirectory, nextEntry.getName());
