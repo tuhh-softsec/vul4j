ed8ceac95011f2c2604ad7738f1edc256b9fd1ca
bqcuong <bqcuong2212@gmail.com>
2021-06-11 15:31:55 +0200

    reproduce

--- src/main/java/org/apache/sling/xss/impl/XSSAPIImpl.java
+++ src/main/java/org/apache/sling/xss/impl/XSSAPIImpl.java
@@ -18,6 +18,9 @@ package org.apache.sling.xss.impl;
 
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -29,6 +32,7 @@ import javax.json.JsonReaderFactory;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.xss.ProtectionContext;
 import org.apache.sling.xss.XSSAPI;
@@ -224,22 +228,33 @@ public class XSSAPIImpl implements XSSAPI {
     @Nonnull
     public String getValidHref(final String url) {
         if (StringUtils.isNotEmpty(url)) {
-            // Percent-encode characters that are not allowed in unquoted
-            // HTML attributes: ", ', >, <, ` and space. We don't encode =
-            // since this would break links with query parameters.
-            String encodedUrl = url.replaceAll("\"", "%22")
-                    .replaceAll("'", "%27")
-                    .replaceAll(">", "%3E")
-                    .replaceAll("<", "%3C")
-                    .replaceAll("`", "%60")
-                    .replaceAll(" ", "%20");
-            int qMarkIx = encodedUrl.indexOf('?');
-            if (qMarkIx > 0) {
-                encodedUrl = encodedUrl.substring(0, qMarkIx) + encodedUrl.substring(qMarkIx).replaceAll(":", "%3A");
-            }
-            encodedUrl = mangleNamespaces(encodedUrl);
-            if (xssFilter.isValidHref(encodedUrl)) {
-                return encodedUrl;
+            try {
+                String unescapedURL = URLDecoder.decode(url, StandardCharsets.UTF_8.name());
+                /*
+                    StringEscapeUtils is deprecated starting with version 3.6 of commons-lang3, however the indicated replacement comes from
+                    commons-text, which is not an OSGi bundle
+                */
+                unescapedURL = StringEscapeUtils.unescapeXml(unescapedURL);
+                // Percent-encode characters that are not allowed in unquoted
+                // HTML attributes: ", ', >, <, ` and space. We don't encode =
+                // since this would break links with query parameters.
+                String encodedUrl = unescapedURL.replaceAll("\"", "%22")
+                        .replaceAll("'", "%27")
+                        .replaceAll(">", "%3E")
+                        .replaceAll("<", "%3C")
+                        .replaceAll("`", "%60")
+                        .replaceAll(" ", "%20");
+                int qMarkIx = encodedUrl.indexOf('?');
+                if (qMarkIx > 0) {
+                    encodedUrl = encodedUrl.substring(0, qMarkIx) + encodedUrl.substring(qMarkIx).replaceAll(":", "%3A");
+                }
+
+                encodedUrl = mangleNamespaces(encodedUrl);
+                if (xssFilter.isValidHref(encodedUrl)) {
+                    return encodedUrl;
+                }
+            } catch (UnsupportedEncodingException e) {
+                LOGGER.error("Unable to decode url: {}.", url);
             }
         }
         // fall through to empty string
