422f250d1498fee697a4bcf1076224e9fff76901
bqcuong <bqcuong2212@gmail.com>
2021-07-15 16:55:48 +0200

    reproduce

--- spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/SpelView.java
+++ spring-security-oauth2/src/main/java/org/springframework/security/oauth2/provider/endpoint/SpelView.java
@@ -23,7 +23,6 @@ import org.springframework.context.expression.MapAccessor;
 import org.springframework.expression.Expression;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
-import org.springframework.security.oauth2.common.util.RandomValueStringGenerator;
 import org.springframework.util.PropertyPlaceholderHelper;
 import org.springframework.util.PropertyPlaceholderHelper.PlaceholderResolver;
 import org.springframework.web.servlet.View;
@@ -36,19 +35,19 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 class SpelView implements View {
 
 	private final String template;
-	
-	private final String prefix;
 
 	private final SpelExpressionParser parser = new SpelExpressionParser();
 
 	private final StandardEvaluationContext context = new StandardEvaluationContext();
 
+	private PropertyPlaceholderHelper helper;
+
 	private PlaceholderResolver resolver;
 
 	public SpelView(String template) {
 		this.template = template;
-		this.prefix = new RandomValueStringGenerator().generate() + "{";
 		this.context.addPropertyAccessor(new MapAccessor());
+		this.helper = new PropertyPlaceholderHelper("${", "}");
 		this.resolver = new PlaceholderResolver() {
 			public String resolvePlaceholder(String name) {
 				Expression expression = parser.parseExpression(name);
@@ -69,10 +68,7 @@ class SpelView implements View {
 				.getPath();
 		map.put("path", (Object) path==null ? "" : path);
 		context.setRootObject(map);
-		String maskedTemplate = template.replace("${", prefix);
-		PropertyPlaceholderHelper helper = new PropertyPlaceholderHelper(prefix, "}");
-		String result = helper.replacePlaceholders(maskedTemplate, resolver);
-		result = result.replace(prefix, "${");
+		String result = helper.replacePlaceholders(template, resolver);
 		response.setContentType(getContentType());
 		response.getWriter().append(result);
 	}
