b23dcc439c269d1ee8f134ac8059ca9dd68c68f4
bqcuong <bqcuong2212@gmail.com>
2021-07-28 14:49:41 +0200

    update test

--- src/test/java/com/fasterxml/jackson/dataformat/xml/TestCVE_2016_3720.java
+++ src/test/java/com/fasterxml/jackson/dataformat/xml/TestCVE_2016_3720.java
@@ -1,4 +1,4 @@
-package com.fasterxml.jackson.dataformat.xml.failing;
+package com.fasterxml.jackson.dataformat.xml;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlTestBase;
