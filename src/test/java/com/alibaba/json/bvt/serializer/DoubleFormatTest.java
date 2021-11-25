690e12797b7848d70cba9dcf84da156bba718a9d
Your Name <you@example.com>
2021-11-25 19:50:12 +0000

    update

--- src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest.java
@@ -2,7 +2,6 @@ package com.alibaba.json.bvt.serializer;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
-import com.sun.javafx.sg.prism.NGShape;
 import junit.framework.TestCase;
 
 import java.text.DecimalFormat;
