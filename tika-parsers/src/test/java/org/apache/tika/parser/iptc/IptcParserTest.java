26c27ff355896ad1bbf69557af368a0fa42502ca
bqcuong <bqcuong2212@gmail.com>
2021-12-16 23:04:43 +0100

    reproduce

---
+++ tika-parsers/src/test/java/org/apache/tika/parser/iptc/IptcParserTest.java
@@ -0,0 +1,21 @@
+package org.apache.tika.parser.iptc;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.junit.Test;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.InputStream;
+
+public class IptcParserTest {
+
+    private final Parser parser = new IptcAnpaParser();
+
+    @Test(timeout = 2000)
+    public void testCVE() throws Exception {
+        Metadata metadata = new Metadata();
+        InputStream stream = getClass().getResourceAsStream("/test-documents/12_hang_tika_iptc.iptc");
+        parser.parse(stream, new DefaultHandler(), metadata, new ParseContext());
+    }
+}
