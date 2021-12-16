2138cd634412ca69fd1ba33804ed4687ccb958d0
bqcuong <bqcuong2212@gmail.com>
2021-12-16 14:49:33 +0100

    reproduce

--- src/main/java/org/apache/commons/configuration2/YAMLConfiguration.java
+++ src/main/java/org/apache/commons/configuration2/YAMLConfiguration.java
@@ -18,14 +18,11 @@
 package org.apache.commons.configuration2;
 
 import org.apache.commons.configuration2.ex.ConfigurationException;
-import org.apache.commons.configuration2.ex.ConfigurationRuntimeException;
 import org.apache.commons.configuration2.io.InputStreamSupport;
 import org.apache.commons.configuration2.tree.ImmutableNode;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
-import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -68,7 +65,7 @@ public class YAMLConfiguration extends AbstractYAMLBasedConfiguration
     {
         try
         {
-            final Yaml yaml = createYamlForReading(new LoaderOptions());
+            final Yaml yaml = new Yaml();
             final Map<String, Object> map = (Map) yaml.load(in);
             load(map);
         }
@@ -83,7 +80,7 @@ public class YAMLConfiguration extends AbstractYAMLBasedConfiguration
     {
         try
         {
-            final Yaml yaml = createYamlForReading(options);
+            final Yaml yaml = new Yaml(options);
             final Map<String, Object> map = (Map) yaml.load(in);
             load(map);
         }
@@ -120,7 +117,7 @@ public class YAMLConfiguration extends AbstractYAMLBasedConfiguration
     {
         try
         {
-            final Yaml yaml = createYamlForReading(new LoaderOptions());
+            final Yaml yaml = new Yaml();
             final Map<String, Object> map = (Map) yaml.load(in);
             load(map);
         }
@@ -135,7 +132,7 @@ public class YAMLConfiguration extends AbstractYAMLBasedConfiguration
     {
         try
         {
-            final Yaml yaml = createYamlForReading(options);
+            final Yaml yaml = new Yaml(options);
             final Map<String, Object> map = (Map) yaml.load(in);
             load(map);
         }
@@ -145,34 +142,4 @@ public class YAMLConfiguration extends AbstractYAMLBasedConfiguration
         }
     }
 
-    /**
-     * Creates a {@code Yaml} object for reading a Yaml file. The object is
-     * configured with some default settings.
-     *
-     * @param options options for loading the file
-     * @return the {@code Yaml} instance for loading a file
-     */
-    private static Yaml createYamlForReading(LoaderOptions options)
-    {
-        return new Yaml(createClassLoadingDisablingConstructor(), new Representer(), new DumperOptions(), options);
-    }
-
-    /**
-     * Returns a {@code Constructor} object for the YAML parser that prevents
-     * all classes from being loaded. This effectively disables the dynamic
-     * creation of Java objects that are declared in YAML files to be loaded.
-     *
-     * @return the {@code Constructor} preventing object creation
-     */
-    private static Constructor createClassLoadingDisablingConstructor()
-    {
-        return new Constructor()
-        {
-            @Override
-            protected Class<?> getClassForName(String name)
-            {
-                throw new ConfigurationRuntimeException("Class loading is disabled.");
-            }
-        };
-    }
 }
