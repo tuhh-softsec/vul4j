45ddad15edd0f56a38d1d10e1ac6e1c2de9de881
Bui Quang Cuong <bqcuong@users.noreply.github.com>
2023-01-12 10:44:58 +0100

    Reduce the number of inner loop iterations because maven takes more than 2s to execute

--- src/test/java/org/apache/commons/compress/Test_CVE_2018_1324.java
+++ src/test/java/org/apache/commons/compress/Test_CVE_2018_1324.java
@@ -1,7 +1,12 @@
 package org.apache.commons.compress;
-
+  
 import org.apache.commons.compress.archivers.zip.ZipFile;
+import org.apache.commons.compress.archivers.zip.ZipLong;
+import org.apache.commons.compress.archivers.zip.ZipShort;
+import org.apache.commons.compress.archivers.zip.X0017_StrongEncryptionHeader;
+
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 
@@ -9,11 +14,20 @@ import static org.apache.commons.compress.AbstractTestCase.getFile;
 
 public class Test_CVE_2018_1324 {
 
-    @Test(timeout = 2000, expected = ArrayIndexOutOfBoundsException.class)
+    @Test(timeout = 6000)
     // Running test should not be in an infinite loop
     public void testCVE_2018_1324() throws IOException {
-        ZipFile zf = new ZipFile(getFile("difflist_fsbwserver.f-secure.com_80_583109529_2.zip"));
-        zf.close();
+        // the data is crafted based on the file difflist_fsbwserver.f-secure.com_80_583109529_2.zip
+        byte[] data = {23, 0, 0, 0, 1, 40, 0, 0, 0, 62, 55, 15, 87, 121, -27, -23, -5, 9, -118, -2, -45, -115, -63, -110, -24};
+        int offset = 4;
+        int length = 0;
+
+        ZipShort.putShort(1, data, offset + 14); // reduce the number of second loops from 65152 -> 1 to save testing time
+
+        assertEquals(3924130135L, ZipLong.getValue(data, offset + 8));
+        assertEquals(1, ZipShort.getValue(data, offset + 14));
+
+        X0017_StrongEncryptionHeader x0017 = new X0017_StrongEncryptionHeader();
+        x0017.parseCentralDirectoryFormat(data, offset, length);
     }
 }
-
